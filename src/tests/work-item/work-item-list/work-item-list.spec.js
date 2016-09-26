155486a016e086e0b1fe8ab2cd49adcfb909d5f2
Michael Kleinhenz <kleinhenz@redhat.com>
2016-09-26 12:26:12 +0200

    Added tests for work list component.

--- src/tests/work-item/work-item-list/work-item-list.spec.js
+++ src/tests/work-item/work-item-list/work-item-list.spec.js
@@ -1,15 +1,40 @@
-describe('Work item list', function() {
+var WorkItemListPage = require('./work-item-list.page');
 
-  beforeEach(function() {
-    browser.get('http://localhost:8088/');
+describe('Work item list', function () {
+
+  var page;
+
+  beforeEach(function () {
+    page = new WorkItemListPage();
   });
 
-  it('should contain 14 items', function() {
-
-      // doesn't work as expected - results in 0
-      //expect(element.all(by.repeater('workItem of workItems')).count()).toEqual(3);
-
-      //var list = element.all(by.id('work-item-list'));
-      //expect(list.count()).toBe(14);
+  it('should contain 14 items.', function() {
+    expect(page.allWorkItems.count()).toBe(14);
   });
+
+  it('should have the right mock data in the first entry.', function() {
+    expect(page.workItemDescription(page.firstWorkItem)).toBe('No description available for this work item.');
+    expect(page.workItemTitle(page.firstWorkItem)).toBe('');
+  });
+
+  it('should create a new workitem.', function () {
+    page.typeQuickAddWorkItemTitle('Some Title');
+    page.typeQuickAddWorkItemDescription('Some Description');
+    page.clickQuickAddSave().then(function() {
+      expect(page.allWorkItems.count()).toBe(15);
+      expect(page.workItemDescription(page.firstWorkItem)).toBe('Some Description');
+      expect(page.workItemTitle(page.firstWorkItem)).toBe('Some Title');
+    });
+  });
+
+  it('should reset the quick add form when the reset button is clicked.', function () {
+    page.typeQuickAddWorkItemTitle('Some Other Title');
+    page.typeQuickAddWorkItemDescription('Some Other Description');
+    page.clickQuickAddCancel().then(function() {
+      expect(page.allWorkItems.count()).toBe(14);
+      expect(page.workItemQuickAddTitle.getText()).toBe('');
+      expect(page.workItemQuickAddDescription.getText()).toBe('');
+    });
+  });
+
 });
