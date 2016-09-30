9039b89c8a36f83e13d9df86cbae8a5de5ef4b88
Sudipta Sen <sanborn.sen@gmail.com>
2016-09-30 22:46:26 +0530

    Check title for only white space in quick add component (#108)
    * Had to bring back the checkTitle function and the flag
    * Angular's default keyupevent handler has slight delay and
      that was making the add button visible for a moment.
    * This solution works good.

--- src/app/work-item/work-item-quick-add/work-item-quick-add.component.ts
+++ src/app/work-item/work-item-quick-add/work-item-quick-add.component.ts
@@ -4,6 +4,7 @@ import { Logger } from '../../shared/logger.service';
 import { WorkItem } from '../work-item';
 import { WorkItemService } from '../work-item.service';
 
+
 @Component({
   selector: 'work-item-quick-add',
   templateUrl: '/work-item-quick-add.component.html',
@@ -13,6 +14,7 @@ export class WorkItemQuickAddComponent implements OnInit {
   @Output() close = new EventEmitter();
   error: any = false;
   workItem: WorkItem;
+  validTitle: Boolean;
 
   constructor(
     private workItemService: WorkItemService,
@@ -34,15 +36,34 @@ export class WorkItemQuickAddComponent implements OnInit {
   }
 
   save(): void {
+    if (this.workItem.fields["system.title"] != null) {
+      this.workItem.fields["system.title"] = this.workItem.fields["system.title"].trim();
+    }
+    if (this.workItem.fields["system.description"] != null) {
+      this.workItem.fields["system.description"] = this.workItem.fields["system.description"].trim();
+    }
     if (this.workItem.fields["system.title"]) {
       this.workItemService
         .create(this.workItem)
         .then(workItem => {
           this.workItem = workItem; // saved workItem, w/ id if new
           this.logger.log(`created and returned this workitem: ${workItem}`);
+          this.workItem.fields["system.description"] = '';
+          this.workItem.fields["system.title"] = '';
+          this.validTitle = false;
           this.goBack(workItem);
         })
         .catch(error => this.error = error); // TODO: Display error message
+    } else {
+      this.error = "Title can not be empty.";
+    }
+  }
+
+  checkTitle(): void {
+    if(this.workItem.fields["system.title"] && this.workItem.fields["system.title"].trim()) {
+      this.validTitle = true;
+    } else {
+      this.validTitle = false;
     }
   }
 
