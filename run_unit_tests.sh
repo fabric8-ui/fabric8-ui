866a8de7c3db9d95338558e153f77a25c88f103c
Joshua Wilson <joshuawilson@users.noreply.github.com>
2016-10-13 01:35:47 -0400

    Fixes #134 - Make it easier to run the test. Update the README with instructions. (#176)

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -5,7 +5,7 @@ echo Using logfile $LOGFILE
 
 # Running npm test
 echo Running unit tests...
-npm test | tee $LOGFILE ; UNIT_TEST_RESULT=${PIPESTATUS[0]}
+npm run test:unit | tee $LOGFILE ; UNIT_TEST_RESULT=${PIPESTATUS[0]}
 
 if [ $UNIT_TEST_RESULT -eq 0 ]; then
   echo 'Unit tests OK'
