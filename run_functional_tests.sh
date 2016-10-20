280372680dd49990e00e1782ce3e8f91063fe510
Nimisha Mukherjee <nimishamukerjee@gmail.com>
2016-10-20 18:53:24 +0530

    Fix for timeout during functional test execution (#214) (#215)

--- run_functional_tests.sh
+++ run_functional_tests.sh
@@ -23,6 +23,12 @@ echo -n Starting Almighty development server...
 while ! (ncat -w 1 127.0.0.1 8088 </dev/null >/dev/null 2>&1); do sleep 1; done
 echo done.
 
+# Retrieve index.html to trigger webpack to build the source
+echo -n Building source...
+# Wait for the build to finish (index.html is delivered)
+curl http://localhost:8088/ -o /dev/null -s
+echo done.
+
 # Finally run protractor
 echo Running tests...
 node_modules/protractor/bin/protractor protractor.config.js
