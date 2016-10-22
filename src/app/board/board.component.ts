905409032b7b3f4c65123f9bb2424d114ab2eab8
harrybabu <ibuddy@ibuddy.local>
2016-10-22 15:59:24 +0530

    Implemented Initial version of board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 import { WorkItem } from '../work-item/work-item';
 import { WorkItemService } from '../work-item/work-item.service';
@@ -11,6 +12,8 @@ import { WorkItemService } from '../work-item/work-item.service';
 })
 export class BoardComponent implements OnInit {
   workItems: WorkItem[] = [];
+  //Added mock data here
+  lanes:Array<any> = [];
 
   constructor(
     private router: Router,
@@ -19,9 +22,16 @@ export class BoardComponent implements OnInit {
 
   ngOnInit() {
     this.workItemService.getWorkItems()
-      .then(workItems => this.workItems = workItems);
+      .then(workItems => {
+        this.workItems = workItems;
+        //need push lane data here once the backend data is available
+        this.lanes.push({title:'Backlog', cards: workItems});
+        this.lanes.push({title: 'In-Progress',cards: [{'id': 211,fields:{'system.title': 'Item A'}}, {'id': 212,fields:{'system.title': 'Item B'}}, {'id': 213,fields:{'system.title':'Item C'}}, {'id': 214,fields:{'system.title': 'Item D'}}]});
+        this.lanes.push({title: 'Done',cards: [{'id': 311,fields:{'system.title': 'Item 1'}}, {'id': 312,fields:{'system.title':'Item 2'}}, {'id': 313,fields:{'system.title': 'Item 3'}}, {'id': 314,fields:{'system.title': 'Item 4'}}]});
+      });
+    
   }
-
+  
   gotoDetail(workItem: WorkItem) {
     let link = ['/detail', workItem.id];
     this.router.navigate(link);
--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }           from '@angular/core';
 import { CommonModule }       from '@angular/common';
+import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 
 import { BoardComponent }   from './board.component';
 import { BoardRoutingModule }   from './board-routing.module';
 
 @NgModule({
-  imports:      [ CommonModule, BoardRoutingModule ],
+  imports:      [ CommonModule, BoardRoutingModule, DragulaModule ],
   declarations: [ BoardComponent ],
   exports: [ BoardComponent ]
 })
