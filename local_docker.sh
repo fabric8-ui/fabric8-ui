8d331fb65bb65a9e044ac7050caed128ba58e8bb
Sudipta Sen <sanborn.sen@gmail.com>
2016-10-28 19:57:01 +0530

    Nginx configuration to support pushState URL pattern (#250)

---
+++ local_docker.sh
@@ -0,0 +1,7 @@
+docker build -t almighty-ui-builder -f Dockerfile.builder .
+mkdir -p dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd)/dist:/dist:Z almighty-ui-builder
+docker exec almighty-ui-builder npm install
+docker exec almighty-ui-builder ./run_unit_tests.sh
+docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
+docker build -t almighty-ui-deploy -f Dockerfile.deploy .
+docker tag almighty-ui-deploy registry.ci.centos.org:5000/almighty/almighty-ui:latest
