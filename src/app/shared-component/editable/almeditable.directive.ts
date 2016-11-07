53ca1400117ce414553121c61ba824af9bcd84d4
Sanborn <sanborn.sen@gmail.com>
2016-11-07 17:58:10 +0530

    ContentEditable directive created
    - It's better if we use Contenteditable feature instead of regular
    input fields, that way it's more customizable.
    - This directive will make any element editable.

---
+++ src/app/shared-component/editable/almeditable.directive.ts
@@ -0,0 +1,68 @@
+import {
+  AfterContentInit, 
+  Directive, 
+  EventEmitter, 
+  ElementRef, 
+  Input, 
+  Output,
+  OnInit,
+  OnChanges,
+  HostListener
+} from '@angular/core';
+
+@Directive({
+    selector: '[almEditable]',
+    exportAs: 'almEditable'
+})
+export class AlmEditableDirective implements OnInit, OnChanges {
+
+  @Output('onUpdate') onUpdate = new EventEmitter();
+  @Input() editable = true;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  private content: any = '';
+  private element: HTMLElement = this.elementRef.nativeElement;
+
+  ngOnInit() {
+    this.element.style.whiteSpace = 'pre-line';
+    if (this.editable) {
+      this.makeEditable();
+    }
+  }
+
+  ngOnChanges() {
+    if (this.editable) {
+      this.makeEditable();
+    } else {
+      this.makeNonEditable();
+    }
+  }
+
+  onEdit() {
+    // alert(this.element.innerText);
+    // alert(this.element.textContent);
+    let newContent = this.element.innerText;
+    if (this.content != newContent) {
+      this.content = newContent;
+      this.onUpdate.emit(this.content);
+    }
+
+  }
+
+  makeEditable() {
+    this.element.setAttribute('contenteditable', 'true');
+  }
+
+  makeNonEditable() {
+    this.element.setAttribute('contenteditable', 'null');
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  listenToKeypress(event: any) {
+    if (this.editable) {
+      this.onEdit();
+    }
+  }
+}
