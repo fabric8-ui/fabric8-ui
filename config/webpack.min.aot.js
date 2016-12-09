aa546429f4b7577c1ae8b92fc797e72e3ad0c8b2
joshuawilson <javajoshw@gmail.com>
2016-12-09 15:52:39 -0500

    Update to Angular 2.2.3. Add Chunk support to AoT, though it doesn't work so it is currently commented out.

--- config/webpack.min.aot.js
+++ config/webpack.min.aot.js
@@ -1,21 +1,38 @@
 const path = require('path');
+var webpack = require("webpack");
 const ngToolsWebpack = require('@ngtools/webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.common.js');
+var WebpackMd5Hash = require('webpack-md5-hash');
+var ChunkManifestPlugin = require('chunk-manifest-webpack2-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = {
   resolve: {
     extensions: ['.ts', '.js']
   },
+
+  /* Uncomment below to use Chunks.*/
+  // entry: {
+  //   'polyfills': './src/polyfills.browser.ts',
+  //   'vendor': './src/vendor.browser.ts',
+  //   'main': './src/main.browser.ts'
+  // },
+  /* Comment below to use Chunks.*/
   entry: './src/main.browser.ts',
+  
   output: {
     path: path.join(process.cwd(), 'dist'),
     publicPath: 'dist/',
-    filename: 'app.main.js'
+    filename: './aot/[name].[chunkhash].js',
+    chunkFilename: './aot/[id].[chunkhash].js'
   },
   plugins: [
     new ngToolsWebpack.AotPlugin({
       "tsConfigPath": "./tsconfig-aot.json",
-      // "mainPath": "src/main.browser.ts"
     }),
     /**
      * Webpack plugin and CLI utility that represents bundle content as convenient interactive zoomable treemap
@@ -23,6 +40,33 @@ module.exports = {
     new BundleAnalyzerPlugin({
       generateStatsFile: true
     }),
+
+    /* Uncomment below to use Chunks.*/
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: ['main', 'vendor', 'polyfills']
+    // }),
+
+    /* The following works with Chunks and should be uncommented when Chunks are working, but needed for Chunks.*/
+    // new webpack.NoErrorsPlugin(),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   beautify: false,
+    //   mangle: {screw_ie8: true, keep_fnames: true},
+    //   compress: {screw_ie8: true},
+    //   comments: false,
+    //   compressor: {
+    //     warnings: false
+    //   }
+    // }),
+    // new WebpackMd5Hash(),
+    // new ManifestPlugin(),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     'ENV': JSON.stringify(ENV)
+    //   }
+    // })
+
+
   ],
   module: {
     loaders: [
--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,6 +1,7 @@
 /*
  * Angular bootstraping
  */
+import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { decorateModuleRef } from './app/environment';
 import { bootloader } from '@angularclass/hmr';
