cf4d244b3f443034d1314e3ba742d3163f98cb5b
joshuawilson <javajoshw@gmail.com>
2016-12-10 15:03:05 -0500

    change the title to fabric8

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,7 +25,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'Fabric8',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
