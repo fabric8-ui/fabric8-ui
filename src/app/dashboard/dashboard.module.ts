158b487141c78425edeefe2991dcf7a7270e2cdd
joshuawilson <javajoshw@gmail.com>
2016-12-17 00:59:07 -0500

    update the dashboard and project-dialog

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,11 @@ import { HttpModule, Http } from '@angular/http';
 
 import { DashboardComponent }     from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ProjectDialogComponent } from '../project-dialog/project-dialog.component';
 
 @NgModule({
   imports:      [ CommonModule, DashboardRoutingModule, HttpModule ],
-  declarations: [ DashboardComponent ],
+  declarations: [ DashboardComponent, ProjectDialogComponent ],
 })
 export class DashboardModule {
   constructor(http: Http) {}
