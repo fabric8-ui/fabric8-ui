64a8184dc513f9beb2d0f05937186a988f2978f0
Pete Muir <pmuir@bleepbleep.org.uk>
2016-12-18 10:18:04 -0500

    Create team membership dialog

--- src/app/analyze/analyze-overview/analyze-overview.module.ts
+++ src/app/analyze/analyze-overview/analyze-overview.module.ts
@@ -1,13 +1,17 @@
 import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
+import { ModalModule } from 'ng2-modal';
 
 import { AnalyzeOverviewComponent }     from './analyze-overview.component';
 import { AnalyzeOverviewRoutingModule } from './analyze-overview-routing.module';
+import { TeamMembershipDialogComponent }
+      from '../../team-membership-dialog/team-membership-dialog.component';
+
 
 @NgModule({
-  imports:      [ CommonModule, AnalyzeOverviewRoutingModule, HttpModule ],
-  declarations: [ AnalyzeOverviewComponent ],
+  imports:      [ CommonModule, AnalyzeOverviewRoutingModule, HttpModule, ModalModule ],
+  declarations: [ AnalyzeOverviewComponent, TeamMembershipDialogComponent ],
 })
 export class AnalyzeOverviewModule {
   constructor(http: Http) {}
---
+++ src/app/team-membership-dialog/team-membership-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+
+@Component({
+  selector: 'team-membership-dialog',
+  templateUrl: './team-membership-dialog.component.html',
+  styleUrls: ['./team-membership-dialog.component.scss']
+})
+export class TeamMembershipDialogComponent {
+ }
