4aa9ffca3dad3a15a72ae94b2f4f56bdb4c70c29
Pete Muir <pmuir@bleepbleep.org.uk>
2016-12-17 22:38:15 -0500

    adip - backlog

--- src/app/plan/backlog/backlog.component.ts
+++ src/app/plan/backlog/backlog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }            from '@angular/router';
+import { Router } from '@angular/router';
 
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'alm-backlog',
@@ -9,12 +10,34 @@ import { Router }            from '@angular/router';
 })
 export class BacklogComponent implements OnInit {
 
+  public isCollapsed: boolean;
+  public currentList: string;
+  private space: string = 'BalloonPopGame';
+  private keyPrefix = 'plan.backlog.sidebar';
+  private isCollapsedKey = this.keyPrefix + '.' + this.space + '.collapsed';
+  private currentListKey = this.keyPrefix + '.' + this.space + '.currentList';
+
   constructor(
-    private router: Router) {
+    private router: Router,
+    private localStorageService: LocalStorageService) {
+  }
+
+  public collapsed(event: any): void {
+    this.localStorageService.set(this.isCollapsedKey, this.isCollapsed);
+  }
+
+  public selectList(list: string): void {
+    this.currentList = list;
+    this.localStorageService.set(this.currentListKey, this.currentList);
   }
 
   ngOnInit() {
-    
+    this.isCollapsed = this.localStorageService.get<boolean>(this.isCollapsedKey) || false;
+    this.currentList = this.localStorageService.get<string>(this.currentListKey) || 'backlog';
   }
 
+
+
 }
+
+
--- src/app/plan/backlog/backlog.module.ts
+++ src/app/plan/backlog/backlog.module.ts
@@ -2,12 +2,24 @@ import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
 
+import { CollapseDirective } from 'ng2-bootstrap/ng2-bootstrap';
+import { LocalStorageModule } from 'angular-2-local-storage';
+
+
 import { BacklogComponent }     from './backlog.component';
 import { BacklogRoutingModule } from './backlog-routing.module';
 
+
 @NgModule({
-  imports:      [ CommonModule, BacklogRoutingModule, HttpModule ],
-  declarations: [ BacklogComponent ],
+  imports:      [ CommonModule, 
+                  BacklogRoutingModule, 
+                  HttpModule, 
+                  LocalStorageModule.withConfig({
+                    prefix: 'fabric8',
+                    storageType: 'localStorage'
+                  })
+                ],
+  declarations: [ BacklogComponent, CollapseDirective ],
 })
 export class BacklogModule {
   constructor(http: Http) {}
