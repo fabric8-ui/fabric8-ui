794ac7bc5a25f0e2e7a726d7bb8161c42408007e
Pete Muir <pmuir@bleepbleep.org.uk>
2016-12-19 15:07:00 -0500

    Front end for github login (#22)

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 
+import { LoginService } from '../login/login.service';
+
 
 @Component({
   selector: 'alm-public',
@@ -10,11 +12,16 @@ import { Router }            from '@angular/router';
 export class PublicComponent implements OnInit {
 
   constructor(
-    private router: Router) {
+    private router: Router,
+    private loginService: LoginService) {
   }
 
   ngOnInit() {
     
+  }
+
+  gitSignin() {
+    this.loginService.gitHubSignIn();
   }
 
 }
