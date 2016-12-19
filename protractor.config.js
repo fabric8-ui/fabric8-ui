3e178c7f64a1feb7d11f718dd3986745d754d7c5
Michael Kleinhenz <kleinhenz@redhat.com>
2016-12-19 20:05:11 +0100

    Rebased.

--- protractor.config.js
+++ protractor.config.js
@@ -2,7 +2,7 @@ exports.config = {
     useAllAngular2AppRoots: true,
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['src/tests/**/*.spec.js'],
+    specs: ['src/tests/**/displayAndUpdateWorkItemDetails.spec.js'],
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
     },
