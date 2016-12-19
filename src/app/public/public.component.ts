cfadc0a462f17aaa8fd17afe6b592966765e9d01
joshuawilson <javajoshw@gmail.com>
2016-12-19 15:28:44 -0500

    connect the github login feature to the backend to enable system login

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 
+import { LoginItem } from '../login/login-item';
 import { LoginService } from '../login/login.service';
+import { AuthenticationService } from '../auth/authentication.service';
 
 
 @Component({
@@ -12,16 +14,38 @@ import { LoginService } from '../login/login.service';
 export class PublicComponent implements OnInit {
 
   constructor(
+    private auth: AuthenticationService,
     private router: Router,
     private loginService: LoginService) {
   }
 
-  ngOnInit() {
-    
+  loginItem: LoginItem;
+  showError: boolean = false;
+  feedbackMessage: string = '';
+  statusCode: number = 0;
+
+
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['home'], {});
+    }
   }
 
   gitSignin() {
     this.loginService.gitHubSignIn();
   }
 
+  checkStatus(loginStatus: any){
+    if (loginStatus.token) {
+      this.router.navigate(['home'], {});
+    } else {
+      this.statusCode = loginStatus.status;
+      this.feedbackMessage = loginStatus.responseText;
+      this.showError = true;
+    }
+  }
+
+  closeAlert(){
+    this.showError = false;
+  }
 }
