7d858dba991cb443a39a8be9cede2b7deb54c31c
joshuawilson <javajoshw@gmail.com>
2016-12-20 15:44:35 -0500

    add protractor.config

---
+++ protractor.config.js
@@ -0,0 +1,16 @@
+exports.config = {
+    useAllAngular2AppRoots: true,
+    getPageTimeout: 30000,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['src/tests/**/*.spec.js'],
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    },
+
+    capabilities: {
+         'browserName': 'phantomjs',
+         'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+         'phantomjs.cli.args': ['--webdriver-loglevel=ERROR', '--local-storage-path=/tmp/phantom_' + Math.random()]
+
+    }
+};
