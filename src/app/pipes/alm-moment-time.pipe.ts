708069377a3823cd1abcd329e0eafbeb3908974f
Vikram Raj <vikramraj1991@gmail.com>
2016-12-20 16:37:52 +0530

    Place create time left to close icon. (fixes #524).
      -show date if workitem is created at more then 1 month ago.

--- src/app/pipes/alm-moment-time.pipe.ts
+++ src/app/pipes/alm-moment-time.pipe.ts
@@ -3,19 +3,28 @@ import * as moment from 'moment';
 
 /*
  * Usage:
- *   timestamp | almMomentTime
- * Example:
- *   {{'2016-12-12T06:57:05.612922Z' |  almMomentTime}} //4 hours ago
+ *   <span [innerHTML]="timestamp | almMomentTime"></span>
 */
 
 @Pipe({ name: 'almMomentTime'})
 export class AlmMomentTime implements PipeTransform {
-  transform(val: string): any {    
+  transform(val: string): any {
     if (!val) {
       return '';
     }
     else {
-      return moment(val, [moment.ISO_8601]).fromNow();
+      let dt = new Date();
+      dt.setMonth(dt.getMonth() - 1);
+      let aMonthAgo = new Date(dt);
+      let dateValue = new Date(val);
+      if (dateValue < dt) {
+        return `<span title="${moment(val).format('lll')}">
+                  ${moment(val).format('D MMM YYYY')}
+                </span>`;
+      }
+      return `<span title="${moment(val).format('lll')}">
+                ${moment(val, [moment.ISO_8601]).fromNow()}
+              </span>`;
     }
   }
 }
