86b2502bfb0ba6e1ac36294f3da8bdb91a0ddb0b
Sanborn <sanborn.sen@gmail.com>
2016-12-21 12:59:05 +0530

    Updating link type list on change wi type
    - Re arrange the pipe inputs to keep the variable in main and constant as regular input.
    - fixes #555

--- src/app/pipes/alm-valid-link-types.pipe.ts
+++ src/app/pipes/alm-valid-link-types.pipe.ts
@@ -3,16 +3,16 @@ import { WorkItem } from '../models/work-item';
 
 @Pipe({ name: 'almValidLinkTypes', pure: true })
 export class AlmValidLinkTypes implements PipeTransform {
-  transform(arr: any[], workItem: WorkItem): any {
+  transform(workItemType: string, arr: any[]): any {
     let opLinkTypes: any[] = [];
     arr.forEach((item) => {
-      if (item.relationships.source_type.data.id == workItem.relationships.baseType.data.id) {
+      if (item.relationships.source_type.data.id == workItemType) {
         opLinkTypes.push({
           name: item.attributes['forward_name'],
           linkId: item.id,
           linkType: 'forward'
         });
-      } else if (item.relationships.target_type.data.id == workItem.relationships.baseType.data.id){
+      } else if (item.relationships.target_type.data.id == workItemType){
         opLinkTypes.push({
           name: item.attributes['reverse_name'],
           linkId: item.id,
