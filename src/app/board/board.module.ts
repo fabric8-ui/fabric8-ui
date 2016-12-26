b389cc2a3be98bdb6d90830611c16b3de84f9499
SMahil <mahilsukh11@gmail.com>
2016-12-26 11:42:33 +0530

    Create Board View base on the WI states

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,12 +2,18 @@ import { NgModule }           from '@angular/core';
 import { CommonModule }       from '@angular/common';
 import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 
+import { AlmIconModule } from './../shared-component/icon/almicon.module';
 import { BoardComponent }   from './board.component';
 import { BoardRoutingModule }   from './board-routing.module';
+//pipes
+
+import { AlmFilterBoardList } from '../pipes/alm-board-filter.pipe';
+import { CountPipe } from 'ng2bln-count-pipe';
+
 
 @NgModule({
-  imports:      [ CommonModule, BoardRoutingModule, DragulaModule ],
-  declarations: [ BoardComponent ],
+  imports:      [ CommonModule, BoardRoutingModule, DragulaModule, AlmIconModule ],
+  declarations: [ BoardComponent, CountPipe , AlmFilterBoardList ],
   exports: [ BoardComponent ]
 })
 export class BoardModule { }
