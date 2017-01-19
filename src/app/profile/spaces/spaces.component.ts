ff4a22c0f2b183329c20b2054db9061a0dba7ae5
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2017-01-19 18:50:49 +0530

    Initial working version of space listing (#66) (#62)

--- src/app/profile/spaces/spaces.component.ts
+++ src/app/profile/spaces/spaces.component.ts
@@ -2,20 +2,35 @@ import { DummyService } from './../../dummy/dummy.service';
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 
+import { Logger } from "../../shared/logger.service";
+import { SpaceService } from './space.service';
+import { Space } from "../../models/space";
+
 
 @Component({
   selector: 'alm-spaces',
   templateUrl: 'spaces.component.html',
-  styleUrls: ['./spaces.component.scss']
+  styleUrls: ['./spaces.component.scss'],
+  providers: [SpaceService]
 })
 export class SpacesComponent implements OnInit {
 
+  _spaces: Space[];
+  pageSize: number = 20;
+
   constructor(
-    private router: Router, public dummy: DummyService) {
+    private router: Router, public dummy: DummyService, private spaceService: SpaceService, private logger: Logger) {
   }
 
   ngOnInit() {
-    
+    this.spaceService.getSpaces(this.pageSize)
+      .then((spaces) => {
+        this._spaces = spaces;
+      })
+      .catch((e) => this.logger.error(e));
   }
 
+  get spaces(): Space[] {
+    return this._spaces;
+  }
 }
