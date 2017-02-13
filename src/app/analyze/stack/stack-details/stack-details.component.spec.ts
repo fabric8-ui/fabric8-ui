d4dd48613efca34f122fc07d2db391fad5a2c7a0
Arunkumar Srisailapathi <asrisail@redhat.com>
2017-02-13 18:14:32 +0530

    Fixed the failing test cases. This effectively runs and checks for the basic ones

--- src/app/analyze/stack/stack-details/stack-details.component.spec.ts
+++ src/app/analyze/stack/stack-details/stack-details.component.spec.ts
@@ -2,6 +2,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, NgForm } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
+import { ModalModule } from 'ngx-modal';
+import { DataTableModule } from 'angular2-datatable';
+
+import { Stack } from './../../../models/stack';
+import { StackRecommendationModule } from '../stack-recommendation/stack-recommendation.module';
+import
+  { ContainerTogglerModule }
+from '../../../shared-component/container-toggler/container-toggler.module';
 
 import { StackDetailsComponent } from './stack-details.component';
 
@@ -11,19 +22,28 @@ describe('StackDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [StackDetailsComponent]
-    })
-      .compileComponents();
+        imports: [ContainerTogglerModule,
+              DataTableModule,
+              ModalModule,
+              StackRecommendationModule,
+              HttpModule,
+              ReactiveFormsModule
+              ],
+        declarations: [StackDetailsComponent, NgForm],
+        providers: [FormBuilder]
+      })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(StackDetailsComponent);
+        component = fixture.componentInstance;
+        let stack: Stack = new Stack();
+        stack.uuid = '2ec2749ef0711bad2112ef45c2a5ee47bd32a6e4';
+        component.stack = stack;
+        fixture.detectChanges();
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StackDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('Component has to be present', () => {
+    expect(component).toBeTruthy();
   });
-
-  // TODO: fix failing test and uncomment
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
 });
