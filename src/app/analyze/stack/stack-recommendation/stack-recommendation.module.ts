c1ae1810e1eb7b5a398040ba1ec0089cd5fb807e
Jaivardhan <jaivardhan@dhcp35-152.lab.eng.blr.redhat.com>
2017-02-13 14:04:56 +0530

    changed class name as file name

---
+++ src/app/analyze/stack/stack-recommendation/stack-recommendation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StackRecommendationComponent } from './stack-recommendation.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [StackRecommendationComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [StackRecommendationComponent]
+})
+
+export class StackRecommendationModule { }
