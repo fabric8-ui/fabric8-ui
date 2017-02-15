a52ddb2883bde6f2c8f1ff470340a68b5032275e
joshuawilson <javajoshw@gmail.com>
2017-02-15 00:14:02 -0500

    add exports to the index

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 export { WidgetsModule } from './widgets.module';
 
-// export { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
-// export { DialogModule } from './src/app/dialog/dialog.module';
-// export { DropdownModule } from './src/app/dropdown/dropdown.module';
-// export { AlmEditableModule } from './src/app/editable/almeditable.module';
-// export { AlmIconModule } from './src/app/icon/almicon.module';
-// export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
-//
-//
-// export { AlmArrayFilter } from './src/app/pipes/alm-array-filter.pipe';
-// export { AlmAvatarSize } from './src/app/pipes/alm-avatar-size.pipe';
-// export { AlmLinkTarget } from './src/app/pipes/alm-link-target.pipe';
-// // export { AlmMomentTime } from './src/app/pipes/alm-moment-time.pipe';
-// export { AlmSearchHighlight } from './src/app/pipes/alm-search-highlight.pipe';
-// export { AlmTrim } from './src/app/pipes/alm-trim';
+export { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
+export { DialogModule } from './src/app/dialog/dialog.module';
+export { DropdownModule } from './src/app/dropdown/dropdown.module';
+export { AlmEditableModule } from './src/app/editable/almeditable.module';
+export { AlmIconModule } from './src/app/icon/almicon.module';
+export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
+
+
+export { AlmArrayFilter } from './src/app/pipes/alm-array-filter.pipe';
+export { AlmAvatarSize } from './src/app/pipes/alm-avatar-size.pipe';
+export { AlmLinkTarget } from './src/app/pipes/alm-link-target.pipe';
+// export { AlmMomentTime } from './src/app/pipes/alm-moment-time.pipe';
+export { AlmSearchHighlight } from './src/app/pipes/alm-search-highlight.pipe';
+export { AlmTrim } from './src/app/pipes/alm-trim';
