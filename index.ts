71256434bf9b714c5f0fa8a12a1f263071387e60
joshuawilson <javajoshw@gmail.com>
2017-02-18 16:53:21 -0500

    add missing exports

--- index.ts
+++ index.ts
@@ -3,20 +3,16 @@ export { WidgetsModule } from './widgets.module';
 export { AlmArrayFilter } from './src/app/pipes/alm-array-filter.pipe';
 export { AlmAvatarSize } from './src/app/pipes/alm-avatar-size.pipe';
 export { AlmLinkTarget } from './src/app/pipes/alm-link-target.pipe';
+export { AlmMomentTime } from './src/app/pipes/alm-moment-time.pipe';
 export { AlmSearchHighlight } from './src/app/pipes/alm-search-highlight.pipe';
 export { AlmTrim } from './src/app/pipes/alm-trim';
 
 export { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
 export { DialogModule } from './src/app/dialog/dialog.module';
+export { Dialog } from './src/app/dialog/dialog';
 export { DropdownModule } from './src/app/dropdown/dropdown.module';
+export { DropdownOption } from './src/app/dropdown/dropdown-option';
 export { AlmEditableModule } from './src/app/editable/almeditable.module';
 export { AlmIconModule } from './src/app/icon/almicon.module';
 export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
 export { TreeListModule } from './src/app/treelist/treelist.module';
-
-// export { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
-// export { DialogModule } from './src/app/dialog/dialog.module';
-// export { DropdownModule } from './src/app/dropdown/dropdown.module';
-// export { AlmEditableModule } from './src/app/editable/almeditable.module';
-// export { AlmIconModule } from './src/app/icon/almicon.module';
-// export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
