3a8a4bc3dca68b64aed58e8d5f69799d1e47c9b3
joshuawilson <javajoshw@gmail.com>
2017-02-21 22:26:42 -0500

    remove reference to treelist component from widgets module and clean up commented code

--- widgets.module.ts
+++ widgets.module.ts
@@ -1,20 +1,13 @@
-// import './rxjs-extensions';
-
-import { NgModule, Pipe } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-// import { DropdownModule } from 'ngx-dropdown';
 
 import { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
 import { DialogModule } from './src/app/dialog/dialog.module';
-// import { Dialog } from './src/app/dialog/dialog';
 import { DropdownModule } from './src/app/dropdown/dropdown.module';
-// import { DropdownOption } from './src/app/dropdown/dropdown-option';
 import { AlmEditableModule } from './src/app/editable/almeditable.module';
 import { AlmIconModule } from './src/app/icon/almicon.module';
 import { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
 import { TreeListModule } from './src/app/treelist/treelist.module';
-import { TreeListComponent } from './src/app/treelist/treelist.component';
 
 import { AlmArrayFilter } from './src/app/pipes/alm-array-filter.pipe';
 import { AlmAvatarSize } from './src/app/pipes/alm-avatar-size.pipe';
@@ -22,20 +15,6 @@ import { AlmLinkTarget } from './src/app/pipes/alm-link-target.pipe';
 import { AlmMomentTime } from './src/app/pipes/alm-moment-time.pipe';
 import { AlmSearchHighlight } from './src/app/pipes/alm-search-highlight.pipe';
 import { AlmTrim } from './src/app/pipes/alm-trim';
-
-
-// export { ContainerTogglerModule } from './src/app/container-toggler/container-toggler.module';
-// export { DialogModule } from './src/app/dialog/dialog.module';
-// export { DropdownModule } from './src/app/dropdown/dropdown.module';
-// export { AlmEditableModule } from './src/app/editable/almeditable.module';
-// export { AlmIconModule } from './src/app/icon/almicon.module';
-// export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
-
-// export { AlmArrayFilter } from './src/app/pipes/alm-array-filter.pipe';
-// export { AlmAvatarSize } from './src/app/pipes/alm-avatar-size.pipe';
-// export { AlmLinkTarget } from './src/app/pipes/alm-link-target.pipe';
-// export { AlmSearchHighlight } from './src/app/pipes/alm-search-highlight.pipe';
-// export { AlmTrim } from './src/app/pipes/alm-trim';
 
 
 @NgModule({
@@ -48,10 +27,7 @@ import { AlmTrim } from './src/app/pipes/alm-trim';
     AlmLinkTarget,
     AlmMomentTime,
     AlmSearchHighlight,
-    AlmTrim,
-    TreeListComponent
-    // Dialog,
-    // DropdownOption
+    AlmTrim
   ],
   exports: [
     AlmArrayFilter,
@@ -64,12 +40,9 @@ import { AlmTrim } from './src/app/pipes/alm-trim';
     AlmIconModule,
     ContainerTogglerModule,
     DialogModule,
-    // Dialog,
     DropdownModule,
-    // DropdownOption,
     InfiniteScrollModule,
-    TreeListModule,
-    TreeListComponent
+    TreeListModule
   ]
 })
 export class WidgetsModule {
