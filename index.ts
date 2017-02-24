01389279c7bb5473fcc721e7242d6c5c78d339ec
Dan <dlabrecq@redhat.com>
2017-02-24 09:21:25 -0500

    Added exports to index.ts and modified package versions per code review comments.

--- index.ts
+++ index.ts
@@ -16,5 +16,31 @@ export { DropdownOption } from './src/app/dropdown/dropdown-option';
 export { AlmEditableModule } from './src/app/editable/almeditable.module';
 export { AlmIconModule } from './src/app/icon/almicon.module';
 export { InfiniteScrollModule } from './src/app/infinitescroll/infinitescroll.module';
+
+// Filter
+export { Filter } from './src/app/filters/filter';
+export { FilterComponent } from './src/app/filters/filter.component';
+export { FilterConfig } from './src/app/filters/filter-config';
+export { FilterEvent } from './src/app/filters/filter-event';
+export { FilterField } from './src/app/filters/filter-field';
+export { FilterFieldsComponent } from './src/app/filters/filter-fields.component';
+export { FilterResultsComponent } from './src/app/filters/filter-results.component';
+
+// Sort
+export { SortComponent } from './src/app/sort/sort.component';
+export { SortConfig } from './src/app/sort/sort-config';
+export { SortEvent } from './src/app/sort/sort-event';
+export { SortField } from './src/app/sort/sort-field';
+export { SortModule } from './src/app/sort/sort.module';
+
+// TreeList
 export { TreeListComponent } from './src/app/treelist/treelist.component';
 export { TreeListModule } from './src/app/treelist/treelist.module';
+
+// Toolbar
+export { Action } from './src/app/toolbar/action';
+export { ActionsConfig } from './src/app/toolbar/actions-config';
+export { ToolbarComponent } from './src/app/toolbar/toolbar.component';
+export { ToolbarModule } from './src/app/toolbar/toolbar.module';
+export { View } from './src/app/toolbar/view';
+export { ViewsConfig } from './src/app/toolbar/views-config';
