765271a99d39e5835679b5044d65f0fc9ab972b8
Pete Muir <pmuir@bleepbleep.org.uk>
2017-02-25 20:31:25 -0500

    fix(profile): Import Modal in to Spaces (#200)

--- src/app/profile/spaces/spaces.module.ts
+++ src/app/profile/spaces/spaces.module.ts
@@ -1,3 +1,4 @@
+import { ModalModule } from 'ngx-modal';
 import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
@@ -7,7 +8,7 @@ import { SpacesRoutingModule } from './spaces-routing.module';
 import { SpaceWizardModule } from './../../space-wizard/space-wizard.module';
 
 @NgModule({
-  imports:      [ CommonModule, SpacesRoutingModule, HttpModule, SpaceWizardModule ],
+  imports:      [ CommonModule, SpacesRoutingModule, HttpModule, ModalModule, SpaceWizardModule ],
   declarations: [ SpacesComponent ]
 })
 export class SpacesModule {
