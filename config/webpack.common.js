981a69d3dec7c2f3f59690556cb4d512c2164709
Pete Muir <pmuir@bleepbleep.org.uk>
2017-02-26 09:36:59 -0500

    chore(build): only hash filenames in prod

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -210,7 +210,7 @@ module.exports = function (options) {
               loader: "url-loader",
               query: {
                 limit: 3000,
-                name: 'assets/fonts/[name].[hash].[ext]'
+                name: 'assets/fonts/[name].' + (isProd ? '[hash]' : '') + '[ext]'
               }
             }
           ]
@@ -221,7 +221,7 @@ module.exports = function (options) {
               loader: "url-loader",
               query: {
                 limit: 3000,
-                name: 'assets/images/[name].[hash].[ext]'
+                name: 'assets/images/[name].' + (isProd ? '[hash]' : '') + '[ext]'
               }
             }
           ]
