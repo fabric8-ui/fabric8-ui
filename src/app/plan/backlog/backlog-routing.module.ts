25988dd7ee7ce2fdf8097cc876de6dfe998a504b
Dana Gutride <dgutride@redhat.com>
2017-03-01 14:10:59 -0500

    feat(boardlink): adds board component to plan section of ui

--- src/app/plan/backlog/backlog-routing.module.ts
+++ src/app/plan/backlog/backlog-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule }  from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BacklogComponent } from './backlog.component';
-import {WorkItemListComponent} from 'fabric8-planner';
-import {AuthUserResolve} from '../../shared/common.resolver';
+import { WorkItemListComponent, WorkItemBoardComponent } from 'fabric8-planner';
+import { AuthUserResolve } from '../../shared/common.resolver';
+import { WorkItemDetailComponent } from 'fabric8-planner/src/app/work-item/work-item-detail/work-item-detail.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,31 @@ const routes: Routes = [
         resolve: {
           authuser: AuthUserResolve
         },
+        children: [
+          {
+            path: 'detail/:id',
+            component: WorkItemDetailComponent,
+            resolve: {
+              authuser: AuthUserResolve
+            }
+          }
+        ]
+      },
+      {
+        path: 'board',
+        component: WorkItemBoardComponent,
+        resolve: {
+          authuser: AuthUserResolve
+        },
+        children: [
+          {
+            path: 'detail/:id',
+            component: WorkItemDetailComponent,
+            resolve: {
+              authuser: AuthUserResolve
+            }
+          }
+        ]
       }
     ]
   }
