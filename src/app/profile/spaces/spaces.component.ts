322aee43df622b3a3fbb53fee128ded57c660861
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2017-03-02 17:20:39 +0530

    feat(space): Utilize SpaceService for searching spaces.
    Sets up a RxJS subject to receive key input events,
    and acts on distinct inputs. The input text is fed to SpaceService
    to execute search queries in the REST API.
    
    Fixes #70

--- src/app/profile/spaces/spaces.component.ts
+++ src/app/profile/spaces/spaces.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
+import { Subject } from "rxjs";
 
 import { Logger } from 'ngx-login-client';
 import { Space, SpaceService } from 'ngx-fabric8-wit';
@@ -17,6 +18,7 @@ export class SpacesComponent implements OnInit {
 
   _spaces: Space[];
   pageSize: number = 20;
+  searchTermStream = new Subject<string>();
 
   constructor(
     private router: Router, public dummy: DummyService, private spaceService: SpaceService, private logger: Logger) {
@@ -24,13 +26,26 @@ export class SpacesComponent implements OnInit {
 
   ngOnInit() {
     this.spaceService.getSpaces(this.pageSize)
-      .then((spaces) => {
+      .then(spaces => {
         this._spaces = spaces;
       })
       .catch((e) => this.logger.error(e));
+    this.searchTermStream
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap((searchText: string) => {
+        return this.spaceService.search(searchText);
+      })
+      .subscribe(values => {
+        this._spaces = values;
+      });
   }
 
   get spaces(): Space[] {
     return this._spaces;
+  }
+
+  searchSpaces(searchText: string) {
+    this.searchTermStream.next(searchText);
   }
 }
