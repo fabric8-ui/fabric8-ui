33179407ee064c71c0f27a8102df6a68573d4e7d
joshuawilson <javajoshw@gmail.com>
2017-03-03 22:39:32 -0500

    chore(build): add component to module export

--- src/app/work-item/work-item-list/planner-list.module.ts
+++ src/app/work-item/work-item-list/planner-list.module.ts
@@ -2,15 +2,21 @@ import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 import { HttpModule, Http } from '@angular/http';
 
-import { WorkItemListComponent }     from './work-item-list.component';
+import { WorkItemListComponent } from './work-item-list.component';
 import { PlannerListRoutingModule } from './planner-list-routing.module';
 
 import { WorkItemDetailModule } from '../work-item-detail/work-item-detail.module';
 
 
 @NgModule({
-  imports:      [ CommonModule, PlannerListRoutingModule, HttpModule, WorkItemDetailModule ],
+  imports: [
+    CommonModule,
+    PlannerListRoutingModule,
+    HttpModule,
+    WorkItemDetailModule
+  ],
   declarations: [ WorkItemListComponent ],
+  exports: [ WorkItemListComponent ],
 })
 export class PlannerListModule {
   constructor(http: Http) {}
