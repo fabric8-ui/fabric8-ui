9bc43371e8f4f75b287e989679ed6ed743145800
joshuawilson <javajoshw@gmail.com>
2017-03-03 22:40:33 -0500

    chore(build): add resolvers to routes for list and board

--- src/app/work-item/work-item-board/planner-board-routing.module.ts
+++ src/app/work-item/work-item-board/planner-board-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UsersResolve, AuthUserResolve } from '../common.resolver';
 import { WorkItemDetailComponent } from '../work-item-detail/work-item-detail.component';
 import { WorkItemBoardComponent } from './work-item-board.component';
 
@@ -8,8 +9,18 @@ const routes: Routes = [
   {
     path: '',
     component: WorkItemBoardComponent,
+    resolve: {
+      allusers: UsersResolve,
+      authuser: AuthUserResolve
+    }
     children: [
-      { path: 'detail/:id', component: WorkItemDetailComponent },
+      { path: 'detail/:id',
+        component: WorkItemDetailComponent,
+        resolve: {
+          allusers: UsersResolve,
+          authuser: AuthUserResolve
+        }
+      },
     ]
   }
 ];
--- src/app/work-item/work-item-list/planner-list-routing.module.ts
+++ src/app/work-item/work-item-list/planner-list-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UsersResolve, AuthUserResolve } from '../common.resolver';
 import { WorkItemDetailComponent } from '../work-item-detail/work-item-detail.component';
 import { WorkItemListComponent } from './work-item-list.component';
 
@@ -8,8 +9,18 @@ const routes: Routes = [
   {
     path: '',
     component: WorkItemListComponent,
+    resolve: {
+      allusers: UsersResolve,
+      authuser: AuthUserResolve
+    }
     children: [
-      { path: 'detail/:id', component: WorkItemDetailComponent },
+      { path: 'detail/:id',
+        component: WorkItemDetailComponent,
+        resolve: {
+          allusers: UsersResolve,
+          authuser: AuthUserResolve
+        }
+      }
     ]
   }
 ];
