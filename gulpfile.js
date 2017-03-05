b6041684f88f6b00b2897e4b04307ed7f20169e1
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-04 21:26:06 -0500

    chore(build): improve dev cycle
    * Move library build entirely to gulp
    * Add watch mode for library build
    * Add sass sourcemaps for library build

--- gulpfile.js
+++ gulpfile.js
@@ -1,177 +1,113 @@
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    runSequence = require('run-sequence'),
-    sourcemaps = require('gulp-sourcemaps'),
-    tsc = require('gulp-typescript'),
-    embedTemplates = require('gulp-inline-ng2-template'),
-    wait = require('gulp-wait'),
-    del = require('del'),
-    replace = require('gulp-string-replace'),
-    cleanCSS = require('gulp-clean-css'),
-    exec = require('child_process').exec;
+  sass = require('gulp-sass'),
+  runSequence = require('run-sequence'),
+  del = require('del'),
+  replace = require('gulp-string-replace'),
+  sourcemaps = require('gulp-sourcemaps'),
+  exec = require('child_process').exec,
+  ngc = require('gulp-ngc'),
+  changed = require('gulp-changed');
 
 var appSrc = 'src';
-var librarySrc = 'src-library';
-var libraryDist = 'dist-library';
+var libraryDist = 'dist';
+var watchDist = 'dist-watch';
 
-var tsProject = tsc.createProject('tsconfig.json');
+/**
+ * FUNCTION LIBRARY
+ */
 
-gulp.task('build:library', function(done) {
-  runSequence(
-    'clean',
-    'compile-sass',
-    'compile-typings',
-    'assets',
-    'copy-library-definitions',
-    done
-  );
-});
+function copyToDist(srcArr) {
+  return gulp.src(srcArr)
+    .pipe(gulp.dest(libraryDist + '/src'));
+}
 
-gulp.task('build:scss-css', function(done) {
-  runSequence(
-    'add-require-for-templates',
-    'replace-html',
-    'add-require-for-styles',
-    'replace-scss',
-    'compile-sass',
-    done
-  );
-});
+function updateWatchDist() {
+  return gulp
+    .src(libraryDist + '/**')
+    .pipe(changed(watchDist))
+    .pipe(gulp.dest(watchDist));
+}
 
-gulp.task('clean', function (done) {
-  return del([libraryDist], done);
-});
-
-gulp.task('add-require-for-templates', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/templateUrl:\s/g, 'template: require('))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
+function transpileSASS(src) {
+  return gulp.src(src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }).on('error', sass.logError)) // this will prevent our future watch-task from crashing on sass-errors
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(function (file) {
+      return libraryDist + file.base.slice(__dirname.length); // save directly to dist
     }));
-});
+}
 
-gulp.task('replace-html', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
+/**
+ * TASKS
+ */
+
+gulp.task('post-transpile', ['transpile'], function () {
+  return gulp.src(['dist/src/app/**/*.js'])
+    .pipe(replace(/templateUrl:\s/g, "template: require("))
     .pipe(replace(/\.html',/g, ".html'),"))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
-});
-
-gulp.task('add-require-for-styles', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/styleUrls: \[/g, 'styles: [require('))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
-});
-
-gulp.task('replace-scss', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
+    .pipe(replace(/styleUrls: \[/g, "styles: [require("))
     .pipe(replace(/\.scss']/g, ".css').toString()]"))
-    .pipe(gulp.dest(function(file) {
+    .pipe(gulp.dest(function (file) {
       return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
     }));
 });
 
 //Sass compilation and minifiction
-gulp.task('compile-sass', function () {
-  return gulp.src(appSrc + '/app/**/*.scss')
-    .pipe(sass().on('error', sass.logError)) // this will prevent our future watch-task from crashing on sass-errors
-    .pipe(cleanCSS({compatibility: 'ie8'})) // see the gulp-sass doc for more information on compatibility modes
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+gulp.task('transpile-sass', function () {
+  return transpileSASS(appSrc + '/app/**/*.scss');
 });
-
-//typescript compilation including sourcemaps and template embedding
-gulp.task('compile-typings', function() {
-
-  //loading typings file
-  return gulp.src([ appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'])
-    .pipe(embedTemplates({
-      base: appSrc + '/app',
-      useRelativePaths: true,
-      supportNonExistentFiles: true,
-      target: 'es5'
-    }))
-    .pipe(tsProject())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(libraryDist + '/lib'));
-});
-
-gulp.task('assets', function(done) {
-    return gulp.src(['src/assets/*/**', 'src/*.*'])
-        .pipe(gulp.dest(libraryDist));
-});
-
-gulp.task('copy-library-definitions', function(done) {
-  let files = [
-    'package.json',
-    'README.adoc',
-    'LICENSE',
-    librarySrc + '/components.d.ts',
-    librarySrc + '/components.js'
-  ];
-  return gulp.src(files).pipe(gulp.dest(libraryDist));
-});
-
-gulp.task('watch', function () {
-    gulp.watch([appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'], ['compile']).on('change', function (e) {
-        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
-    });
-    gulp.watch([appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'], ['resources']).on('change', function (e) {
-        console.log('Resource file ' + e.path + ' has been changed. Updating.');
-    });
-});
-
 
 // Put the SASS files back to normal
-gulp.task('build:css-scss', function(done) {
-  runSequence(
-    'remove-require-for-templates',
-    'replace-require-html',
-    'remove-require-for-styles',
-    'replace-require-scss',
-    'remove-css',
-    done
-  );
+gulp.task('build-library',
+  [
+    'transpile',
+    'post-transpile',
+    'transpile-sass',
+    'copy-html',
+    'copy-static-assets'
+  ]);
+
+gulp.task('transpile', /*['pre-transpile'],*/ function () {
+  return ngc('tsconfig.json')
 });
 
-gulp.task('remove-require-for-templates', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/template: require\(/g, 'templateUrl: '))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+gulp.task('copy-html', function () {
+  return copyToDist([
+    'src/**/*.html'
+  ]);
 });
 
-gulp.task('replace-require-html', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/\.html'\),/g, ".html',"))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+gulp.task('copy-static-assets', function () {
+  return gulp.src([
+    'LICENSE',
+    'README.adoc',
+    'package.json',
+  ])
+    .pipe(gulp.dest(libraryDist));
 });
 
-gulp.task('remove-require-for-styles', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/styles: \[require\(/g, 'styleUrls: ['))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+gulp.task('copy-watch', ['post-transpile'], function() {
+  return updateWatchDist();
 });
 
-gulp.task('replace-require-scss', function() {
-  gulp.src([appSrc + '/app/**/*.ts'])
-    .pipe(replace(/\.css'\)\.toString\(\)]/g, ".scss']"))
-    .pipe(gulp.dest(function(file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+gulp.task('copy-watch-all', ['build-library'], function() {
+  return updateWatchDist();
 });
 
-gulp.task('remove-css', function (done) {
-  return del([appSrc + '/app/**/*.css'], done);
+gulp.task('watch', ['build-library', 'copy-watch-all'], function () {
+  gulp.watch([appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'], ['transpile', 'post-transpile', 'copy-watch']).on('change', function (e) {
+    console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+  });
+  gulp.watch([appSrc + '/app/**/*.scss']).on('change', function (e) {
+    console.log(e.path + ' has been changed. Updating.');
+    transpileSASS(e.path)
+  });
+  gulp.watch([appSrc + '/app/**/*.html']).on('change', function (e) {
+    console.log(e.path + ' has been changed. Updating.');
+    copyToDist(e.path);
+  });
 });
