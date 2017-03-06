771b31771932130ac82fd91099fca4d685ce6d72
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-06 13:11:41 -0500

    chore(build): still testing semantic-release

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -50,9 +50,9 @@ if [ $? -eq 0 ]; then
     docker build -t fabric-ui-deploy -f Dockerfile.deploy . && \
     docker tag fabric-ui-deploy 8.43.84.245.xip.io/fabric8io/fabric8-ui:latest
     docker push 8.43.84.245.xip.io/fabric8io/fabric8-ui:latest
-    set +e
-    docker exec fabric8-ui-builder npm run semantic-release
     if [ $? -eq 0 ]; then
+      set +e
+      docker exec fabric8-ui-builder npm run semantic-release
       echo 'CICO: image pushed, npmjs published, ready to update deployed app'
       exit 0
     else
