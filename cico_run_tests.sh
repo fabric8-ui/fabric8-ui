f3aaf7cd339d62b7cb0ef6d466fb2aa0ad66074c
Michael Kleinhenz <kleinhenz@redhat.com>
2017-03-06 12:52:16 +0530

    fix(build): fixed directory bug for test build.

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -43,5 +43,5 @@ docker exec fabric8-planner-builder ./run_unit_tests.sh
 ## All ok, build prod version
 docker exec fabric8-planner-builder ./upload_to_codecov.sh
 docker exec fabric8-planner-builder npm run build:prod
-docker exec -u root fabric8-planner-builder cp -r /home/almighty/dist /
+docker exec -u root fabric8-planner-builder cp -r /home/fabric8/fabric8-planner/dist /
 
