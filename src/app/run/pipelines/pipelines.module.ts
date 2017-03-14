ce5bf5450d7ffab2fa010168bdd83b3dcdf05dfd
Dana Gutride <dgutride@redhat.com>
2017-03-13 23:53:43 -0400

    fix(modules): add space module dependencies

--- src/app/run/pipelines/pipelines.module.ts
+++ src/app/run/pipelines/pipelines.module.ts
@@ -5,9 +5,16 @@ import { HttpModule, Http } from '@angular/http';
 import { PipelinesComponent }     from './pipelines.component';
 import { PipelinesRoutingModule } from './pipelines-routing.module';
 import { SpaceModule } from 'fabric8-runtime-console';
+import { RestangularModule } from 'ng2-restangular';
+import { KubernetesRestangularModule } from 'fabric8-runtime-console/src/app/kubernetes/service/kubernetes.restangular';
 
 @NgModule({
-  imports:      [ CommonModule, PipelinesRoutingModule, HttpModule, SpaceModule ],
+  imports:      [ CommonModule,
+    PipelinesRoutingModule,
+    RestangularModule,
+    KubernetesRestangularModule,
+    HttpModule,
+    SpaceModule ],
   declarations: [ PipelinesComponent ],
 })
 export class PipelinesModule {
