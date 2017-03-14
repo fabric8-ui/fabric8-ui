80c57f6a8c67c0417667e28c284ee340cbb5e5e0
Sanborn <sanborn.sen@gmail.com>
2017-03-14 14:57:42 +0530

    fix(planner): Planne integration fixed

---
+++ src/app/plan/board/board.module.ts
@@ -0,0 +1,12 @@
+import { PlannerBoardModule } from 'fabric8-planner';
+import { NgModule }         from '@angular/core';
+import { CommonModule }     from '@angular/common';
+import { HttpModule, Http } from '@angular/http';
+
+
+@NgModule({
+  imports:      [ CommonModule, HttpModule, PlannerBoardModule ]
+})
+export class PlanModule {
+  constructor(http: Http) {}
+}
