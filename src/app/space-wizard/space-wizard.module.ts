b9472f57110a26ef70169a28f877d8e5a1fb4471
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-16 09:25:04 -0400

    fix(spaces): Use space name validators from -wit
    Fixes #296

--- src/app/space-wizard/space-wizard.module.ts
+++ src/app/space-wizard/space-wizard.module.ts
@@ -1,12 +1,14 @@
-import { UniqueSpaceNameValidatorDirective } from './unique-space-name.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { UniqueSpaceNameValidatorDirective, ValidSpaceNameValidatorDirective } from 'ngx-fabric8-wit';
+
 import { SpaceWizardComponent } from './space-wizard.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [SpaceWizardComponent, UniqueSpaceNameValidatorDirective],
+  declarations: [SpaceWizardComponent, UniqueSpaceNameValidatorDirective, ValidSpaceNameValidatorDirective],
   exports: [SpaceWizardComponent, UniqueSpaceNameValidatorDirective]
 })
 export class SpaceWizardModule {
