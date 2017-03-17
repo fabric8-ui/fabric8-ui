4d2cbd85933147ec83116444826dcfa331d316e1
Michael Kleinhenz <kleinhenz@redhat.com>
2017-03-17 22:57:07 +0530

    fix(tests): fixes areas for inmemory tests. (#1218)
    * fix(tests): fixed correct iteration behaviour in mock service.
    
    * fix(tests): Fix area module for inmemory.

--- src/app/work-item/work-item-detail/work-item-detail.module.ts
+++ src/app/work-item/work-item-detail/work-item-detail.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
+import { HttpModule, Http }    from '@angular/http';
 
 import { CollapseModule, TooltipModule } from 'ng2-bootstrap';
 import { Ng2CompleterModule } from 'ng2-completer';
 import { DropdownModule } from 'ng2-bootstrap';
+import { MockHttp } from './../../shared/mock-http';
 
 import { AlmUserName } from '../../pipes/alm-user-name.pipe';
 
@@ -23,9 +25,17 @@ import {
   WorkItemLinkTypeFilterByTypeName
 } from './work-item-detail-pipes/work-item-link-filters.pipe';
 
+let providers = [];
+
+if (process.env.ENV == 'inmemory') {
+  providers = [ AreaService, { provide: Http, useClass: MockHttp } ];
+} else {
+  providers = [ AreaService ];
+}
 
 @NgModule({
   imports: [
+    HttpModule,
     WidgetsModule,
     AlmIconModule,
     AlmEditableModule,
@@ -45,8 +55,6 @@ import {
     WorkItemLinkTypeFilterByTypeName
   ],
   exports: [WorkItemDetailComponent],
-  providers: [
-    AreaService
-  ]
+  providers: providers
 })
 export class WorkItemDetailModule { }
