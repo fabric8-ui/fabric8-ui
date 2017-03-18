67979d1f184140afce9b4d42b67a67ebebadb140
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-18 11:32:25 -0400

    fix(build): add env vars to build container

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -58,7 +58,11 @@ if [ $? -eq 0 ]; then
     echo 'CICO: build OK'
     # TODO HACK this needs to not be hard coded
     source environments/devshift-cluster.deploy.sh
-    docker build -t fabric8-ui-deploy -f Dockerfile.deploy . && \
+    docker build \
+      # TODO HACK this needs to be moved to the openshift template
+      -e WS_K8S_API_SERVER -e K8S_API_SERVER_PROTOCOL -e K8S_API_SERVER_BASE_PATH -e OAUTH_ISSUER -e OAUTH_CLIENT_ID -e OAUTH_SCOPE -e OAUTH_AUTHORIZE_URI -e OAUTH_LOGOUT_URI \
+      -t fabric8-ui-deploy \
+      -f Dockerfile.deploy . && \
     docker tag fabric8-ui-deploy registry.devshift.net/fabric8io/fabric8-ui:latest
     docker push registry.devshift.net/fabric8io/fabric8-ui:latest
     if [ $? -eq 0 ]; then
