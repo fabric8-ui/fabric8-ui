036c19c7002001c7689f8051622bc9aaa1904de9
Sanborn <sanborn.sen@gmail.com>
2017-03-20 11:00:58 +0530

    Wrapped HTTP service added

---
+++ src/app/shared/http-service.ts
@@ -0,0 +1,77 @@
+import { AuthenticationService } from 'ngx-login-client';
+import {
+  Injectable,
+  ReflectiveInjector
+} from '@angular/core';
+import {
+  Http,
+  XHRBackend,
+  RequestOptions,
+  Request,
+  RequestOptionsArgs,
+  Response,
+  Headers
+} from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+/**
+ * This class is an abstract of Angular2 HTTP
+ * Extended from Angular2 HTTP
+ *
+ * Purpose:
+ * Error handling on API services
+ * To implement a general retry logic
+ */
+
+@Injectable()
+export class HttpService extends Http {
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+  constructor (
+    backend: XHRBackend,
+    options: RequestOptions,
+    auth: AuthenticationService
+  ) {
+    super(backend, options);
+    if (auth.getToken() != null) {
+      options.headers.set('Authorization', `Bearer ${auth.getToken()}`);
+    }
+  }
+
+  get(url: string, options?: RequestOptionsArgs) {
+    console.log('GET request initiated');
+    console.log('URL - ', url);
+    console.log('Options - ', options);
+
+    return super.get(url, options);
+  }
+
+  post(url: string, body: any, options?: RequestOptionsArgs) {
+    console.log('POST request initiated');
+    console.log('URL - ', url);
+    console.log('Body - ', body);
+    console.log('Options - ', options);
+
+    return super.post(url, body, options);
+  }
+
+  put(url: string, body: any, options?: RequestOptionsArgs) {
+    console.log('PUT request initiated');
+    console.log('URL - ', url);
+    console.log('Body - ', body);
+    console.log('Options - ', options);
+
+    return super.post(url, body, options);
+  }
+
+  patch(url: string, body: any, options?: RequestOptionsArgs) {
+    console.log('PATCH request initiated');
+    console.log('URL - ', url);
+    console.log('Body - ', body);
+    console.log('Options - ', options);
+
+    return super.post(url, body, options);
+  }
+
+}
