ff076f62d99d06fd57f109b772d7fb06ef97378a
Michael Kleinhenz <kleinhenz@redhat.com>
2017-03-20 11:13:02 +0100

    fix(build): added integration launcher.

---
+++ scripts/run-integration.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# get the script's absolute path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+echo $SCRIPTPATH
+
+# defaults to script's parent dir and the common git name in parent
+PLANNER_HOME="$SCRIPTPATH/.."
+PLATFORM_HOME="$PLANNER_HOME/../fabric8-ui"
+REINSTALL=0
+NODE_ENV=production
+
+# fire up getopt
+TEMP=`getopt -o rp:f: --long reinstall,plannerhome:,platformhome: -n 'run-integration.sh' -- "$@"`
+eval set -- "$TEMP"
+
+# reinstall
+function reinstall {
+  echo "Reinstalling Planner in $PLANNER_HOME"
+  cd $PLANNER_HOME && npm run reinstall &
+  echo "Reinstalling Platform in $PLATFORM_HOME"
+  cd $PLATFORM_HOME && npm run reinstall &
+  wait
+} 
+
+# links planner to platform
+function linkPlanner {
+  echo "Linking Planner to Platform in $PLATFORM_HOME"
+  echo "cd $PLATFORM_HOME && npm link $PLANNER_HOME/dist-watch"
+} 
+
+# runs the platform
+function runPlatform {
+  echo "Running Platform in $PLATFORM_HOME"
+  cd $PLATFORM_HOME && npm start 
+} 
+
+# runs the planner in watch mode
+function runPlanner {
+  echo "Running Planner in $PLANNER_HOME"
+  cd $PLANNER_HOME && npm run watch:library &
+} 
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -r|--reinstall) REINSTALL=1 ; shift ;;
+        -p|--plannerhome)
+            case "$2" in
+                "") shift 2 ;;
+                *) PLANNER_HOME=$2 ; shift 2 ;;
+            esac ;;
+        -f|--platformhome)
+            case "$2" in
+                "") shift 2 ;;
+                *) PLATFORM_HOME=$2 ; shift 2 ;;
+            esac ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+if [ $REINSTALL -eq 1 ]
+  then
+    reinstall
+fi
+
+runPlanner
+linkPlanner
+runPlatform
+
+
