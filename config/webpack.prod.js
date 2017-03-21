20f9a72b3c24bfe369162a6eae00c3d11797b842
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-21 10:20:45 -0400

    fix(build): don't use inline sourcemaps in prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -66,7 +66,7 @@ module.exports = function (env) {
      * See: http://webpack.github.io/docs/configuration.html#devtool
      * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
      */
-    devtool: 'inline-source-map',
+    devtool: 'cheap-module-source-map',
 
     /**
      * Options affecting the output of the compilation.
