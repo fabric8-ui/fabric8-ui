fa98ae844127541aa0b3404586c324286151c355
mitchharpur <mitchharpur@metajunction.com>
2017-03-24 16:42:19 -0400

    fix(remove-default) :remove default forge url

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -29,7 +29,7 @@ const OfflinePlugin = require('offline-plugin');
  * Webpack Constants
  */
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
-const FABRIC8_FORGE_API_URL = process.env.FABRIC8_FORGE_API_URL || 'https://forge.api.prod-preview.openshift.io';
+const FABRIC8_FORGE_API_URL = process.env.FABRIC8_FORGE_API_URL;
 const FABRIC8_WIT_API_URL = process.env.FABRIC8_WIT_API_URL;
 const FABRIC8_RECOMMENDER_API_URL = process.env.FABRIC8_RECOMMENDER_API_URL || 'http://api-bayesian.dev.rdu2c.fabric8.io/api/v1/';
 const FABRIC8_SSO_API_URL = process.env.FABRIC8_SSO_API_URL;
