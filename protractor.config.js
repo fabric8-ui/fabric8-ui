a4b54a0f0c04e217c2540123133c78b7311926ce
Naina <naverma@redhat.com>
2017-03-27 18:35:28 +0530

    test(board-spec-remove): remove board spec temoparily (#1337)

--- protractor.config.js
+++ protractor.config.js
@@ -3,7 +3,7 @@ exports.config = {
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['src/tests/**/*.spec.js'],
-    exclude: ['src/tests/**/EE/*.spec.js', 'src/tests/**/TBD/*.spec.js', 'src/tests/**/orderOfExecution.spec.js', 'src/tests/**/work-item-timeStamp.spec.js', 'src/tests/**/testHelpers*.spec.js', 'src/tests/**/unauthorizeduser*.spec.js'],
+    exclude: ['src/tests/**/EE/*.spec.js', 'src/tests/**/TBD/*.spec.js', 'src/tests/**/board.spec.js', 'src/tests/**/orderOfExecution.spec.js', 'src/tests/**/work-item-timeStamp.spec.js', 'src/tests/**/testHelpers*.spec.js', 'src/tests/**/unauthorizeduser*.spec.js'],
     suites: {
     smokeTest: 'src/tests/**/smokeTest.spec.js'
     },
