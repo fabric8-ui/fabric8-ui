593f65efbdb732fb4d1f5330bfb0352970419b2e
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-27 14:42:57 -0400

    fix: add x-request-id to all http requests
    * Uses uuidv4 to genreate a unique id per request
    
    Fixes #348

--- src/app/shared/fabric8-ui-http.service.ts
+++ src/app/shared/fabric8-ui-http.service.ts
@@ -1,6 +1,3 @@
-import { Observable } from 'rxjs';
-import { WIT_API_URL } from 'ngx-fabric8-wit';
-import { HttpService, SSO_API_URL } from 'ngx-login-client';
 import { Injectable, Inject } from '@angular/core';
 import {
   Http,
@@ -8,8 +5,15 @@ import {
   RequestOptions,
   Request,
   RequestOptionsArgs,
-  XHRBackend
+  XHRBackend,
+  Headers
 } from '@angular/http';
+
+import { Observable } from 'rxjs';
+import { WIT_API_URL } from 'ngx-fabric8-wit';
+import { HttpService, SSO_API_URL } from 'ngx-login-client';
+
+import * as uuidV4 from 'uuid/v4';
 
 @Injectable()
 export class Fabric8UIHttpService extends Http {
@@ -23,10 +27,25 @@ export class Fabric8UIHttpService extends Http {
     super(backend, options);
   }
 
-  request(url: string|Request, options?: RequestOptionsArgs): Observable<Response> {
-    // Only use the HttpService from ngx-login-client for requests to certain endpoints
+  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
     let urlStr = (typeof url === 'string' ? url : url.url);
+    // Attach a X-Request-Id to all requests that don't have one
+    if (urlStr.startsWith(this.witApiUrl)) {
+      if (typeof url === 'string') {
+        if (!options) {
+          options = { headers: new Headers() };
+        }
+        if (!options.headers.has('X-Request-Id')) {
+          options.headers.set('X-Request-Id', uuidV4());
+        }
+      } else {
+        if (!url.headers.has('X-Request-Id')) {
+          url.headers.set('X-Request-Id', uuidV4());
+        }
+      }
+    }
     if (urlStr.startsWith(this.witApiUrl) || urlStr.startsWith(this.ssoApiUrl)) {
+      // Only use the HttpService from ngx-login-client for requests to certain endpoints
       return this.httpService.request(url, options);
     } else {
       return super.request(url, options);
