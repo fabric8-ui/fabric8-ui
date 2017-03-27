1b0591316bcfc1d0507085435bd2ee6792624ee4
rawlingsj <rawlingsj80@gmail.com>
2017-03-27 18:51:43 +0100

    add CI and CD pipelines which also update downstream projects to keep npm versions in sync

---
+++ Jenkinsfile
@@ -0,0 +1,39 @@
+#!/usr/bin/groovy
+@Library('github.com/fabric8io/fabric8-pipeline-library@master')
+def utils = new io.fabric8.Utils()
+def org = 'fabric8io'
+def repo = 'fabric8-planner'
+fabric8UINode{
+    ws {
+      git "https://github.com/${org}/${repo}.git"
+      readTrusted 'release.groovy'
+      sh "git remote set-url origin git@github.com:${org}/${repo}.git"
+      def pipeline = load 'release.groovy'
+
+      if (utils.isCI()){
+        container('ui'){
+          pipeline.ci()
+        }
+      } else if (utils.isCD()){
+        def branch
+        container('ui'){
+            branch = utils.getBranch()
+        }
+        
+        def published
+        container('ui'){
+          published = pipeline.cd(branch)
+        }
+
+        def releaseVersion
+        container('ui'){
+            releaseVersion = utils.getLatestVersionFromTag()
+        }
+
+        if (published){
+          pipeline.updateDownstreamProjects(releaseVersion)
+        }
+      }
+    }
+}
+
---
+++ release.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/groovy
+def ci (){
+    stage('build'){
+        sh 'npm install'
+    }
+    stage('unit test'){
+        sh './run_unit_tests.sh'
+    }
+    // stage('functional test'){
+    //     sh './run_functional_tests.sh'
+    // }
+}
+
+def cd (b){
+    stage('fix git repo'){
+        sh './fix-git-repo.sh'
+    }
+
+    stage('build'){
+        sh 'npm install'
+        sh 'npm run build'
+    }
+
+    stage('unit test'){
+        sh './run_unit_tests.sh'
+    }
+
+    // stage('functional test'){
+    //     sh './run_functional_tests.sh'
+    // }
+
+    stage('release'){
+        def published = npmRelease{
+            branch = b
+        }
+        return published
+    }
+}
+
+def updateDownstreamProjects(v){
+    echo 'we would Update Downstream Projects'
+    pushPackageJSONChangePR{
+        propertyName = 'fabric8-planner'
+        projects = [
+                'fabric8-ui/fabric8-npm-dependencies'
+        ]
+        version = v
+    }
+}
+return this
