567c164eadf5b2d1a4580168b65c3bf579897eb0
rawlingsj <rawlingsj80@gmail.com>
2017-03-28 22:23:50 +0100

    fix(release): update pipeline to use correct property name

--- release.groovy
+++ release.groovy
@@ -45,7 +45,7 @@ def updateDownstreamProjects(v){
                 'fabric8-ui/fabric8-npm-dependencies'
         ]
         version = v
-        container = 'ui'
+        containerName = 'ui'
     }
 }
 return this
