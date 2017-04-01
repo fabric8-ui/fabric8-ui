403c6a5d08f2d0cdd6e0a0e272a02034eeecf61a
Pete Muir <pmuir@bleepbleep.org.uk>
2017-04-01 09:11:30 -0400

    chore(components): add script to create components

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp'),
+  del = require('del'),
+  changeCase = require('change-case'),
+  replace = require('gulp-string-replace'),
+  rename = require('gulp-rename'),
+  exec = require('child_process').exec,
+  argv = require('yargs')
+    .usage('Usage: $0 [options]')
+    .command('create', 'Create a new component')
+    .example('$0 --component=example-widget --dest=src/app/')
+    .alias('d', 'dest')
+    .describe('d', 'The directory to place the component in.')
+    .alias('c', 'component')
+    .describe('c', 'The name of the component to create in snake case.')
+    .demandOption(['c', 'd'])
+    .argv;
+
+function copyToDest(srcArr) {
+  return gulp.src(srcArr)
+    .pipe(gulp.dest(function (file) {
+      return libraryDist + file.base.slice(__dirname.length); // save directly to dist
+    }));
+}
+
+gulp.task('create', function () {
+  let headerCase = argv.c;
+  let pascalCase = changeCase.pascalCase(headerCase);
+  let dest = argv.d;
+  let origHeaderCase = 'starter-widget';
+  let origPascalCase = changeCase.pascalCase(origHeaderCase);
+  return gulp.src([`src/templates/${origHeaderCase}/**`])
+    .pipe(rename(function (path) {
+      path.basename = path.basename.replace(new RegExp(origHeaderCase), headerCase);
+    }))
+    .pipe(replace(new RegExp(origHeaderCase, "g"), headerCase))
+    .pipe(replace(new RegExp(origPascalCase, "g"), pascalCase))
+    .pipe(gulp.dest(dest));
+});
--- src/app/dashboard-widgets/starter-widget/README.md
+++
@@ -1,1 +0,0 @@
-A starter widget you can copy
