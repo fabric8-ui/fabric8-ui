e348eb02289d6933a635293b34f7645ea4740841
Sanborn <sanborn.sen@gmail.com>
2017-04-04 15:10:12 +0530

    fix(test): Prepared a local functioanl test script that works for both mac and linux.

--- local_run_functional_tests.sh
+++ local_run_functional_tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 LOGFILE=$(pwd)/functional_tests.log
-echo Using logfile $LOGFILE 
+echo Using logfile $LOGFILE
 
 # For the functional tests, we are mocking the core
 export NODE_ENV=inmemory
+OS=$(uname -a | awk '{print $1;}')
 
 # Download dependencies
 echo -n Updating Webdriver and Selenium...
@@ -13,14 +14,25 @@ node_modules/protractor/bin/webdriver-manager update
 echo -n Starting Webdriver and Selenium...
 (node_modules/protractor/bin/webdriver-manager start >>$LOGFILE 2>&1 &)
 # Wait for port 4444 to be listening connections
-while ! (ncat -w 1 127.0.0.1 4444 </dev/null >/dev/null 2>&1); do sleep 1; done
+if [ $OS = 'Darwin' ]
+then
+  while ! (nc -w 1 127.0.0.1 4444 </dev/null >/dev/null 2>&1); do sleep 1; done
+else
+  while ! (ncat -w 1 127.0.0.1 4444 </dev/null >/dev/null 2>&1); do sleep 1; done
+fi
 echo done.
 
 # Start the web app
 echo -n Starting Almighty development server...
 (node_modules/webpack-dev-server/bin/webpack-dev-server.js --inline --progress --host 0.0.0.0 --port 8088 >>$LOGFILE 2>&1 &)
 # Wait for port 8088 to be listening connections
-while ! (ncat -w 1 127.0.0.1 8088 </dev/null >/dev/null 2>&1); do sleep 1; done
+if [ $OS = 'Darwin' ]
+then
+  while ! (nc -w 1 127.0.0.1 8088 </dev/null >/dev/null 2>&1); do sleep 1; done
+else
+  while ! (ncat -w 1 127.0.0.1 8088 </dev/null >/dev/null 2>&1); do sleep 1; done
+fi
+
 echo done.
 
 # Retrieve index.html to trigger webpack to build the source
@@ -31,17 +43,21 @@ echo done.
 
 # Finally run protractor
 echo Running tests...
-if [ -z "$1" ]
-  then
-    node_modules/protractor/bin/protractor protractor.config.js
-else
-    node_modules/protractor/bin/protractor protractor.config.js --suite $1
-fi
+#node_modules/protractor/bin/protractor protractor.config.js
+## Run functional tests on the bases of suite genere!  ##
+# This line has been commented for now!
+node_modules/protractor/bin/protractor protractor.config.js --suite $1
 TEST_RESULT=$?
 
 # Cleanup webdriver-manager and web app processes
-fuser -k -n tcp 4444
-fuser -k -n tcp 8088
+if [ $OS = 'Darwin' ]
+then
+  kill -9 $(lsof -ti tcp:4444)
+  kill -9 $(lsof -ti tcp:8088)
+else
+  fuser -k -n tcp 4444
+  fuser -k -n tcp 8088
+fi
 
 # Return test result
 if [ $TEST_RESULT -eq 0 ]; then
