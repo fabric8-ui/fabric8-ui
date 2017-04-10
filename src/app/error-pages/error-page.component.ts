866ad5087bbee6c31990853f80fbb3ef3bc56e00
SMahil <mahilsukh11@gmail.com>
2017-04-10 12:16:59 +0530

    feat(error-page):Add HTML/CSS for error page

---
+++ src/app/error-pages/error-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  host: {
+    'class': 'error-page'
+  },
+  selector: 'alm-errorpage',
+  templateUrl: 'error-page.component.html',
+  styleUrls: ['./error-page.component.scss']
+})
+export class ErrorPageComponent {
+
+  constructor(
+  ) { }
+
+}
