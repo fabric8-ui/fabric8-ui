f6dd1432867d54c10fb5d681208d2d310935e442
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-04-11 09:53:26 +0530

    fix(analytics_reports): Changes for the analytical reports portion.
    What does it have?
    
    1. Changes the analytical-report-widget.component.html
    	a. Comments the existing combobox hardcoded html temporarily.
    	b. Makes the select box dynamic by integrating it with the build configurations from the pipeline
    	c. Showing relevant builds on selection of the select box
    2. Changes the analytica-report-widget.component.ts
    	a. Introduces the pipeline interaction to get the buildconfig information
    	b. Brings in a few imports necessary for the same
    	c. Handles the select box change event to handle the UI
    3. Changes in the package.json to accomdate runtime version change and the stack-analysis version change as the changes have to go together

--- src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.component.ts
+++ src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.component.ts
@@ -1,8 +1,22 @@
+import { PipelinesService } from './../../shared/runtime-console/pipelines.service';
+import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Broadcaster } from 'ngx-base';
 import { Contexts } from 'ngx-fabric8-wit';
+
+import {
+  BuildConfig,
+  BuildConfigs,
+  combineBuildConfigAndBuilds,
+  filterPipelines,
+  BuildConfigStore,
+  BuildStore,
+  Build
+} from 'fabric8-runtime-console';
+
+import { Fabric8RuntimeConsoleService } from './../../shared/runtime-console/fabric8-runtime-console.service';
 
 import { DummyService } from './../shared/dummy.service';
 
@@ -18,14 +32,31 @@ export class AnalyticalReportWidgetComponent implements OnInit {
     uuid: 'ff59ea91cf264003bc6dc12621c91205'
   }];
 
+  buildConfigs: Observable<BuildConfigs>;
+  buildConfigsCount: Observable<number>;
+
+  currentPipeline: BuildConfig;
+  currentPipelineBuilds: Array<Build>;
+
   constructor(
     private context: Contexts,
     private broadcaster: Broadcaster,
-
+    private pipelinesService: PipelinesService
   ) { }
 
   ngOnInit() {
     this.context.current.subscribe(context => console.log('Context', context));
+
+    let bcs = this.pipelinesService.current
+      .publish();
+    this.buildConfigs = bcs;
+    this.buildConfigsCount = bcs.map(buildConfigs => buildConfigs.length);
+    bcs.connect();
+  }
+
+  selectedPipeline(): void {
+    let pipeline: BuildConfig = this.currentPipeline;
+    this.currentPipelineBuilds = pipeline.interestingBuilds;
   }
 
 }
