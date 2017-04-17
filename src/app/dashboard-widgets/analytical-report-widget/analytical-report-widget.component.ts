29da7f61a2dbc058e2e8f2b20f29ec582c838a04
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-04-18 00:58:42 +0530

    fix(analytics_report): change the look of stack report as per ux
    Changes:
    
    1. Introduces Changes in analytical-report-widget.component.html
    	a. Select boxes for selecting pipelines and builds if present
    	b. On selecting, show the stack analysis information if any
    2. Uses exposed bayesian functions to get recommendations while hitting its API - analytical-report-widget.component.ts
    3. Changes in styles to support the HTMLs - analytical-report-widget.component.scss
    
    Fixes #814.

--- src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.component.ts
+++ src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.component.ts
@@ -12,10 +12,15 @@ import {
   Build
 } from 'fabric8-runtime-console';
 
+import {StackAnalysesService, getStackRecommendations} from 'fabric8-stack-analysis-ui';
+
 @Component({
   selector: 'fabric8-analytical-report-widget',
   templateUrl: './analytical-report-widget.component.html',
-  styleUrls: ['./analytical-report-widget.component.scss']
+  styleUrls: ['./analytical-report-widget.component.scss'],
+  providers: [
+    StackAnalysesService
+  ]
 })
 export class AnalyticalReportWidgetComponent implements OnInit {
 
@@ -25,10 +30,16 @@ export class AnalyticalReportWidgetComponent implements OnInit {
   currentPipeline: BuildConfig;
   currentPipelineBuilds: Array<Build>;
 
+  currentBuild: Build;
+  stackAnalysisInformation: any = {
+    recommendationsLimit: 5
+  };
+
   constructor(
     private context: Contexts,
     private broadcaster: Broadcaster,
-    private pipelinesService: PipelinesService
+    private pipelinesService: PipelinesService,
+    private stackAnalysisService: StackAnalysesService
   ) { }
 
   ngOnInit() {
@@ -43,7 +54,52 @@ export class AnalyticalReportWidgetComponent implements OnInit {
 
   selectedPipeline(): void {
     let pipeline: BuildConfig = this.currentPipeline;
-    this.currentPipelineBuilds = pipeline.interestingBuilds;
+    this.currentPipelineBuilds = pipeline.builds;
+  }
+
+  selectedBuild(): void {
+    let build: Build = this.currentBuild;
+    if (build.annotations['fabric8.io/bayesian.analysisUrl']) {
+      let url: string = build.annotations['fabric8.io/bayesian.analysisUrl'];
+      this.stackAnalysisService
+          .getStackAnalyses(url)
+          .subscribe((data) => {
+            let recommendationsObservable = getStackRecommendations(data);
+            if (recommendationsObservable) {
+              let recommendations: Array<any> = [];
+              recommendationsObservable.subscribe((result) => {
+                let missing: Array<any> = result.missing || [];
+                let version: Array<any> = result.version || [];
+
+
+                for (let i in missing) {
+                  if (missing.hasOwnProperty(i)) {
+                    let keys: Array<string> = Object.keys(missing[i]);
+                    recommendations.push({
+                      suggestion: 'Recommendation',
+                      action: 'Add',
+                      message: keys[0] + ' : ' + missing[i][keys[0]]
+                    });
+                  }
+                }
+                for (let i in version) {
+                  if (version.hasOwnProperty(i)) {
+                    let keys: Array<string> = Object.keys(version[i]);
+                    recommendations.push({
+                      suggestion: 'Recommendation',
+                      action: 'Update',
+                      message: keys[0] + ' : ' + version[i][keys[0]]
+                    });
+                  }
+                }
+
+                this.stackAnalysisInformation['recommendations'] = recommendations;
+                // Restrict the recommendations to a particular limit as specified in UX
+                this.stackAnalysisInformation['recommendations'].splice(this.stackAnalysisInformation['recommendationsLimit']);
+              });
+            }
+        });
+      }
   }
 
 }
--- src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.module.ts
+++ src/app/dashboard-widgets/analytical-report-widget/analytical-report-widget.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-
 @NgModule({
   imports: [CommonModule, FormsModule, StackDetailsModule],
   declarations: [AnalyticalReportWidgetComponent],
