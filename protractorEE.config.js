54fad40b5e6158d350bdb5bab9a4cd8e5425eba1
Len DiMaggio <ldimaggi@redhat.com>
2017-04-18 14:32:01 -0400

    fix(test): update EE test to generate screenshots for failing tests to aid in debugging

--- protractorEE.config.js
+++ protractorEE.config.js
@@ -1,3 +1,12 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html',
+  reportOnlyFailedSpecs: false,
+  captureOnlyFailedSpecs: true
+});
+
 exports.config = {
     useAllAngular2AppRoots: true,
     getPageTimeout: 30000,
@@ -11,5 +20,25 @@ exports.config = {
       'chromeOptions': {
         'args': [ '--no-sandbox']
       }
-    }
+    },
+
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  }
+
 };
