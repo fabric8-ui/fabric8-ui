7969f05ab74606a5db2c083c9b9fb22dd6ce6b75
Dan <dlabrecq@redhat.com>
2017-04-22 12:40:51 -0400

    fix(getting-started): Added AuthGuard to enforce login

--- src/app/getting-started/getting-started-routing.module.ts
+++ src/app/getting-started/getting-started-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }  from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './../shared/auth-guard.service';
 import { GettingStartedComponent } from './getting-started.component';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     component: GettingStartedComponent
   }
 ];
