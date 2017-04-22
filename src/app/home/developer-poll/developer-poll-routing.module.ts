daa3609180db1e1c78faff410f48459b0b0faac0
Dan <dlabrecq@redhat.com>
2017-04-22 13:16:03 -0400

    fix(home): Fix for issue #983
    Home page was inadvertently routed to the poll component because its child route path was empty.

--- src/app/home/developer-poll/developer-poll-routing.module.ts
+++ src/app/home/developer-poll/developer-poll-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DeveloperPollComponent } from './developer-poll.component';
 
-const routes: Routes = [{
-  path: '',
-  component: DeveloperPollComponent
-}];
+export const routes: Routes = [
+  {
+    path: 'alm-developer-poll',
+    component: DeveloperPollComponent
+  }
+];
 
 @NgModule({
   imports: [ RouterModule.forChild(routes) ],
