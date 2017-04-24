f23e995f700d292e7fc797955a984b1b1db04ae4
James Strachan <james.strachan@gmail.com>
2017-04-24 16:57:21 +0100

    improve the production environment script so that the console works against the production cluster

--- environments/openshift-prod-cluster.sh
+++ environments/openshift-prod-cluster.sh
@@ -21,9 +21,6 @@ export K8S_API_SERVER="localhost:3000"
 
 export OPENSHIFT_CONSOLE_URL="https://${OPENSHIFT_CONSOLE_HOST}:${OPENSHIFT_CONSOLE_PORT}/console"
 
-if [ -z "${OAUTH_ISSUER}" ]; then
-  export OAUTH_ISSUER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}"
-fi
 if [ -z "${OAUTH_SCOPE}" ]; then
   export OAUTH_SCOPE="user:full"
 fi
@@ -36,12 +33,19 @@ fi
 if [ -z "${K8S_API_SERVER_BASE_PATH}" ]; then
   export K8S_API_SERVER_BASE_PATH="/_p/oso"
 fi
-if [ -z "${WS_K8S_API_SERVER}" ]; then
-  export WS_K8S_API_SERVER=${PROXIED_K8S_API_SERVER}
-fi
 if [ -z "${FABRIC8_PIPELINES_NAMESPACE}" ]; then
   export FABRIC8_PIPELINES_NAMESPACE=""
 fi
+
+
+export OAUTH_ISSUER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}"
+export WS_K8S_API_SERVER="${PROXIED_K8S_API_SERVER}"
+
+export FABRIC8_SSO_API_URL=https://sso.openshift.io/
+export FABRIC8_WIT_API_URL=https://api.openshift.io/api/
+
+# TODO use prod preview for now
+export FABRIC8_FORGE_API_URL="https://forge.api.prod-preview.openshift.io'"
 
 echo "Configured to connect to kubernetes cluster at https://${PROXIED_K8S_API_SERVER}/"
 
@@ -56,4 +60,8 @@ echo "OAUTH_AUTHORIZE_URI            ${OAUTH_AUTHORIZE_URI}"
 echo "OAUTH_LOGOUT_URI               ${OAUTH_LOGOUT_URI}"
 echo "FABRIC8_PIPELINES_NAMESPACE    ${FABRIC8_PIPELINES_NAMESPACE}"
 echo "OPENSHIFT_CONSOLE_URL          ${OPENSHIFT_CONSOLE_URL}"
+echo "FABRIC8_SSO_API_URL            ${KEYCLOAK_SAAS_URL}"
+echo "FABRIC8_WIT_API_URL            ${FABRIC8_WIT_API_URL}"
+echo "FABRIC8_FORGE_API_URL          ${FABRIC8_FORGE_API_URL}"
+echo "WS_K8S_API_SERVER              ${WS_K8S_API_SERVER}"
 echo ""
