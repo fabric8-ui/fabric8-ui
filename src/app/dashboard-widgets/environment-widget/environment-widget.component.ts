7d0ee0ac76fef49e739ec5f3a140f50e55edaa79
Dana Gutride <dgutride@redhat.com>
2017-04-25 16:02:07 -0400

    feat(environments): add environments info to space dashboard

--- src/app/dashboard-widgets/environment-widget/environment-widget.component.ts
+++ src/app/dashboard-widgets/environment-widget/environment-widget.component.ts
@@ -1,24 +1,65 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, OnDestroy, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subscription } from 'rxjs/Rx';
 
-import { Contexts } from 'ngx-fabric8-wit';
+import { Contexts, Context } from 'ngx-fabric8-wit';
+import { PipelinesService } from '../../shared/runtime-console/pipelines.service';
+
+import {
+  BuildConfigs
+} from 'fabric8-runtime-console';
+
+import { appInfos } from 'fabric8-runtime-console/src/app/kubernetes/model/buildconfig.model';
+
 
 @Component({
   selector: 'fabric8-environment-widget',
   templateUrl: './environment-widget.component.html',
   styleUrls: ['./environment-widget.component.scss']
 })
-export class EnvironmentWidgetComponent implements OnInit{
+export class EnvironmentWidgetComponent implements OnInit, OnDestroy {
 
   contextPath: Observable<string>;
+  buildConfigs: Observable<BuildConfigs>;
+  appsCount: number = 0;
+  appSubscription: Subscription;
+  contextSubscription: Subscription;
+  currentContext: Context;
+  loadedEnvironments = [];
 
-  constructor(private context: Contexts) {
+  constructor(private context: Contexts,
+              private pipelinesService: PipelinesService) {
   }
 
   ngOnInit() {
+    this.contextSubscription = this.context.current.subscribe((context) => {
+      this.currentContext = context;
+    });
     this.contextPath = this.context.current.map(context => context.path);
+    let bcs = this.pipelinesService.current
+      .publish();
+    this.buildConfigs = bcs;
+    let environmentAppInfo = this.buildConfigs.map(appInfos);
+    this.appSubscription = environmentAppInfo.subscribe((environments) => {
+      let keys = Object.keys(environments);
+      // reset environments array
+      this.loadedEnvironments = [];
+      keys.forEach((key) => {
+        if(environments[key].apps[this.currentContext.space.attributes.name]) {
+          this.loadedEnvironments.push({
+            "name": key,
+            "apps": environments[key].apps[this.currentContext.space.attributes.name]
+          });
+        }
+      });
+      this.appsCount = this.loadedEnvironments.length;
+    });
+    bcs.connect();
   }
 
+  ngOnDestroy() {
+    this.appSubscription.unsubscribe();
+    this.contextSubscription.unsubscribe();
+  }
 }
