f0815f0384a84e4e6d3006ae91872b6e617d0df0
joshuawilson <javajoshw@gmail.com>
2017-04-26 17:08:27 -0400

    fix(forge): change forge api URL to use process.env

--- src/app/shared/forge-api.provider.ts
+++ src/app/shared/forge-api.provider.ts
@@ -1,14 +1,15 @@
 import { OpaqueToken } from '@angular/core'
+
 import { ApiLocatorService } from './api-locator.service';
 
-let forgeApiUrlLocatorToken = new OpaqueToken("fabric8.forge.api.url");
+import { FABRIC8_FORGE_API_URL } from 'fabric8-runtime-console';
 
 let forgeApiUrlFactory = (api: ApiLocatorService) => {
   return api.forgeApiUrl;
 };
 
 export let forgeApiUrlProvider = {
-  provide: forgeApiUrlLocatorToken,
+  provide: FABRIC8_FORGE_API_URL,
   useFactory: forgeApiUrlFactory,
   deps: [ApiLocatorService]
 };
