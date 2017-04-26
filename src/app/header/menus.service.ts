6f77fe2712490f658c1de4b53b0a94bdaf169a91
James Strachan <james.strachan@gmail.com>
2017-04-26 17:27:56 +0100

    fix(environments): bring it back
    lets add back the environments page now it seems to be working again

--- src/app/header/menus.service.ts
+++ src/app/header/menus.service.ts
@@ -56,6 +56,10 @@ export class MenusService {
               {
                 name: 'Pipelines',
                 path: 'pipelines'
+              },
+              {
+                name: 'Environments',
+                path: 'environments'
               }
             ]
           }
