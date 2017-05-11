031af5bd34560c4ff9fb40a41171ab88784e960a
Len DiMaggio <ldimaggi@redhat.com>
2017-05-11 05:20:58 -0400

    Remove obsolete ee tests (#1869)
    * fix(user-icon): use correct patternfly icon for user-icon
    
    * fix (test): removed old, obsolete EE POC test (current test is in fabric8-ui repo)

--- protractor.config.js
+++ protractor.config.js
@@ -3,7 +3,7 @@ exports.config = {
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['src/tests/**/*.spec.js'],    
-    exclude: ['src/tests/**/*work-item-dynamic-fields.spec.js','src/tests/**/EXCLUDED/*.spec.js','src/tests/**/EE/*.spec.js'],
+    exclude: ['src/tests/**/*work-item-dynamic-fields.spec.js','src/tests/**/EXCLUDED/*.spec.js'],
     suites: {
     smokeTest: 'src/tests/**/smokeTest.spec.js',
     fullTest:  'src/tests/**/*.spec.js'
