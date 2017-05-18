0d04c70f25bbb3fa76503ee37f938faa4c66c054
Michael Kleinhenz <kleinhenz@redhat.com>
2017-05-18 11:14:16 +0200

    fix(integration): fixes watch library build.

--- index.ts
+++ index.ts
@@ -6,19 +6,13 @@ export { WorkItemService } from './src/app/services/work-item.service';
 export { WorkItem } from './src/app/models/work-item';
 export { PlannerModule } from './planner.module';
 
-export { WorkItemQuickAddComponent } from './src/app/work-item/work-item-quick-add/work-item-quick-add.component';
-export { WorkItemQuickAddModule } from './src/app/work-item/work-item-quick-add/work-item-quick-add.module';
-export { MockHttp } from './src/app/shared/mock-http';
-export { MockDataService } from './src/app/shared/mock-data.service';
-export { HttpService } from './src/app/shared/http-service';
+export { WorkItemQuickAddComponent } from './src/app/components/work-item-quick-add/work-item-quick-add.component';
+export { WorkItemQuickAddModule } from './src/app/components/work-item-quick-add/work-item-quick-add.module';
+export { MockHttp } from './src/app/mock/mock-http';
+export { MockDataService } from './src/app/mock/mock-data.service';
+export { HttpService } from './src/app/services/http-service';
 
 export { PlannerBoardComponent } from './src/app/components/planner-board/planner-board.component';
 export { PlannerBoardModule } from './src/app/components/planner-board/planner-board.module';
 export { PlannerListComponent } from './src/app/components/planner-list/planner-list.component';
 export { PlannerListModule } from './src/app/components/planner-list/planner-list.module';
-/* REMOVE ME WHEN FINISHED
-export { WorkItemBoardComponent } from './src/app/work-item/work-item-board/work-item-board.component';
-export { PlannerBoardModule } from './src/app/work-item/work-item-board/planner-board.module';
-export { WorkItemListComponent } from './src/app/work-item/work-item-list/work-item-list.component';
-export { PlannerListModule } from './src/app/work-item/work-item-list/planner-list.module';
-*/
