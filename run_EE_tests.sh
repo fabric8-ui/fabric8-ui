96781b09c9658e6aa18981527856dd6178ef8186
ldimaggi <ldimaggi@redhat.com>
2017-05-19 08:24:25 -0400

    fix(test): adding missing shell script parameter for target URL

--- run_EE_tests.sh
+++ run_EE_tests.sh
@@ -18,7 +18,7 @@ echo Running tests...
 set +x
 cat ~/payload/jenkins-env | grep EE_TEST > ~/.ee_test_params
 . ~/.ee_test_params
-node_modules/protractor/bin/protractor protractorEE.config.js --params.login.user=$EE_TEST_USERNAME --params.login.password=$EE_TEST_PASSWORD
+node_modules/protractor/bin/protractor protractorEE.config.js --params.login.user=$EE_TEST_USERNAME --params.login.password=$EE_TEST_PASSWORD --params.target.url=$1
 TEST_RESULT=$?
 set -x
 
