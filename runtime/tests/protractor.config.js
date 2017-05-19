bb82a1e1d97607f5041e28ff5e6d37a97c907f21
Sanborn <sanborn.sen@gmail.com>
2017-05-19 17:36:48 +0530

    fix(func_test): File paths are fixed for functional tests

--- runtime/tests/protractor.config.js
+++ runtime/tests/protractor.config.js
@@ -2,13 +2,13 @@ exports.config = {
     useAllAngular2AppRoots: true,
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['src/tests/**/*.spec.js'],    
-    exclude: ['src/tests/**/*work-item-dynamic-fields.spec.js','src/tests/**/EXCLUDED/*.spec.js'],
+    specs: ['./../src/tests/**/*.spec.js'],
+    exclude: ['./../src/tests/**/*work-item-dynamic-fields.spec.js','src/tests/**/EXCLUDED/*.spec.js'],
     suites: {
-    smokeTest: 'src/tests/**/smokeTest.spec.js',
-    fullTest:  'src/tests/**/*.spec.js'
+      smokeTest: './../src/tests/**/smokeTest.spec.js',
+      fullTest:  './../src/tests/**/*.spec.js'
     },
-    
+
     jasmineNodeOpts: {
         isVerbose: true,
         showColors: true,
