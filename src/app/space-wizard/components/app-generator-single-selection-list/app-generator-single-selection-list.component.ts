f1e7c5676183ac17bbe3c5fb52802bf26af22edd
mitchharpur <mitchharpur@metajunction.com>
2017-05-31 13:57:13 -0400

    chore(space-wizard):refactor single select into component

---
+++ src/app/space-wizard/components/app-generator-single-selection-list/app-generator-single-selection-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+//
+import { ILoggerDelegate, LoggerFactory } from '../../common/logger';
+
+import { ForgeAppGenerator } from './forge-app-generator';
+
+import {
+  IField,
+  IFieldChoice
+} from '../../services/app-generator.service';
+
+import { ForgeAppGeneratorServiceClient } from '../forge-app-generator/forge-app-generator-service-client';
+import { FieldWidgetClassificationOptions } from '../../models/contracts/field-classification';
+
+@Component({
+  selector: 'app-generator-single-selection-list',
+  templateUrl: './app-generator-single-selection-list.component.html',
+  styleUrls: [ './app-generator-single-selection-list.component.scss' ]
+})
+export class AppGeneratorSingleSelectionListComponent implements OnInit, OnDestroy {
+
+  // keep track of the number of instances
+  static instanceCount: number = 1;
+
+  @Input() field: IField = <IField>{ name:'', value:'', display:{ choices:[] }};
+  @Input() appGenerator: ForgeAppGeneratorServiceClient;
+
+  constructor(
+    loggerFactory: LoggerFactory) {
+    let logger = loggerFactory.createLoggerDelegate(this.constructor.name, AppGeneratorSingleSelectionListComponent.instanceCount++);
+    if ( logger ) {
+      this.log = logger;
+    }
+    this.log(`New instance ...`);
+  }
+
+  ngOnInit() {
+    this.log(`ngOnInit ...`);
+  }
+
+  ngOnDestroy() {
+    this.log(`ngOnDestroy ...`);
+  }
+  /** logger delegate delegates logging to a logger */
+  private log: ILoggerDelegate = () => {};
+
+}
+
--- src/app/space-wizard/space-wizard.module.ts
+++ src/app/space-wizard/space-wizard.module.ts
@@ -7,6 +7,7 @@ import { runtimeConsoleImports } from './../shared/runtime-console/runtime-conso
 import { LoggerFactory } from './common/logger';
 import { ForgeAppGeneratorComponent } from './components/forge-app-generator/forge-app-generator.component';
 import { ForgePipelineViewComponent } from './components/forge-pipeline-view/forge-pipeline-view.component';
+import { AppGeneratorSingleSelectionListComponent } from './components/app-generator-single-selection-list/app-generator-single-selection-list.component';
 import { ForgeStepViewComponent } from './components/forge-step-view/forge-step-view.component';
 import { ForgePanelComponent } from './components/forge-panel/forge-panel.component';
 import { CreateSpaceComponent } from './components/create-space/create-space.component';
@@ -32,6 +33,7 @@ import { CodebasesService } from '../create/codebases/services/codebases.service
     ForgePipelineViewComponent,
     ForgeStepViewComponent,
     ForgePanelComponent,
+    AppGeneratorSingleSelectionListComponent,
     CreateSpaceComponent,
     UniqueSpaceNameValidatorDirective,
     ValidSpaceNameValidatorDirective,
