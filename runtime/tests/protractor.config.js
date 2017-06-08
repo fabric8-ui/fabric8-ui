87476022ce01c04238fe0d44608382201ea72211
Ruchir <rgarg@redhat.com>
2017-06-08 15:47:56 +0000

    fix(test): path to spec files fixed (#1961)

--- runtime/tests/protractor.config.js
+++ runtime/tests/protractor.config.js
@@ -3,7 +3,7 @@ exports.config = {
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./../src/tests/**/*.spec.js'],
-    exclude: ['./../src/tests/**/*work-item-dynamic-fields.spec.js','src/tests/**/EXCLUDED/*.spec.js'],
+    exclude: ['./../src/tests/**/*work-item-dynamic-fields.spec.js','./../src/tests/**/EXCLUDED/*.spec.js'],
     suites: {
       smokeTest: './../src/tests/**/smokeTest.spec.js',
       fullTest:  './../src/tests/**/*.spec.js'
@@ -28,7 +28,7 @@ exports.config = {
       'browser': 'INFO'
       },
       'chromeOptions': {
-        'args': [ '--no-sandbox']
+      'args': [ '--no-sandbox']
       }
     }
 };
