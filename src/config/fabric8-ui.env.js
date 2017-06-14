dbc11a84f9eeaa8581eb1c5c3273d06dbedbf09e
James Strachan <james.strachan@gmail.com>
2017-06-14 16:22:07 +0100

    remove duplicate vlaue

--- src/config/fabric8-ui.env.js
+++ src/config/fabric8-ui.env.js
@@ -5,7 +5,6 @@ window.Fabric8UIEnv = {
   "openshiftConsoleUrl": "{{ .Env.OPENSHIFT_CONSOLE_URL }}",
   "witApiUrl": "{{ .Env.FABRIC8_WIT_API_URL }}",
   "ssoApiUrl": "{{ .Env.FABRIC8_SSO_API_URL }}",
-  "forgeApiUrl": "{{ .Env.FABRIC8_FORGE_API_URL }}",
   "recommenderApiUrl": "{{ .Env.FABRIC8_RECOMMENDER_API_URL }}",
   "statusApiUrl": "{{ .Env.FABRIC8_STATUS_API_URL }}"
 };
