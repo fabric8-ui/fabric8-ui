c8846e07d4d664f21f3f9fe30a14371a8821749e
James Strachan <james.strachan@gmail.com>
2017-06-14 17:41:03 +0100

    fix(envvars): add a proxy api server
    lets add a property to load the proxied k8s API server host

--- src/config/fabric8-ui.env.js
+++ src/config/fabric8-ui.env.js
@@ -3,6 +3,7 @@ window.Fabric8UIEnv = {
   "analyticsWriteKey": "{{ .Env.ANALYTICS_WRITE_KEY }}",
   "forgeApiUrl": "{{ .Env.FABRIC8_FORGE_API_URL }}",
   "openshiftConsoleUrl": "{{ .Env.OPENSHIFT_CONSOLE_URL }}",
+  "openshiftProxiedApiServer": "{{ .Env.PROXIED_K8S_API_SERVER }}",
   "witApiUrl": "{{ .Env.FABRIC8_WIT_API_URL }}",
   "ssoApiUrl": "{{ .Env.FABRIC8_SSO_API_URL }}",
   "recommenderApiUrl": "{{ .Env.FABRIC8_RECOMMENDER_API_URL }}",
