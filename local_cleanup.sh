9886e77835356d3c68259a8a46c1ebf1e44606d5
ldimaggi <ldimaggi@redhat.com>
2017-06-19 15:16:39 -0400

    fix(test): added code to support browser restart to enable us to run multiple tests

--- local_cleanup.sh
+++ local_cleanup.sh
@@ -6,6 +6,7 @@ oc delete bc --all -n almusertest1
 oc delete build --all -n almusertest1
 oc delete build --all -n almusertest1-test
 oc delete build --all -n almusertest1-stage
+oc delete service --all -n almusertest1-stage
 oc delete build --all -n almusertest1-run
 oc delete build --all -n almusertest1-jenkins
 oc delete bc --all -n almusertest1-jenkins
--- src/tests/work-item/work-item-list/EE/openshift-io-login.spec.js
+++ src/tests/work-item/work-item-list/EE/openshift-io-login.spec.js
@@ -39,11 +39,13 @@ describe('openshift.io End-to-End POC test - Scenario - Existing user: ', functi
   /* Tests must reset the browser so that the test can logout/login cleanly */
   afterEach(function () { 
     browser.restart();
+    /* Protractor must recreate its ExpectedConditions */
+    until = protractor.ExpectedConditions;
   });
 
   /* Simple test for registered user */
   it('should enable a registered user create a space, project, etc. ', function() {
-   
+    
     /* Protractor must recreate its ExpectedConditions if the browser is restarted */
     until = protractor.ExpectedConditions;
     
@@ -78,74 +80,6 @@ describe('openshift.io End-to-End POC test - Scenario - Existing user: ', functi
       } else {
         //do nothing
       }
-    });
-
-    /* Step 3 - on home page - create new space - embed time in space name to ensure unique space name */
-    OpenShiftIoDashboardPage.clickHeaderDropDownToggle();
-    OpenShiftIoDashboardPage.clickCreateSpaceUnderLeftNavigationBar();  
-
-    var spaceTime = returnTime();
-    OpenShiftIoDashboardPage.typeNewSpaceName((spaceTime));
-    OpenShiftIoDashboardPage.typeDevProcess("Scenario Driven Planning");
-    OpenShiftIoDashboardPage.clickCreateSpaceButton();   
-
-    /* For the purposes of this test - ignore all 'toast' popup warnings */
-    OpenShiftIoDashboardPage.waitForToastToClose();
-    OpenShiftIoSpaceHomePage = OpenShiftIoDashboardPage.clickNoThanksButton();
-
-    /* Step - in the space home page, verify URL and end the test */
-    browser.wait(until.urlContains('https://openshift.io/almusertest1/'+ spaceTime), constants.WAIT);
-    browser.wait(until.urlIs('https://openshift.io/almusertest1/'+ spaceTime), constants.WAIT); 
-    expect(browser.getCurrentUrl()).toEqual('https://openshift.io/almusertest1/'+ spaceTime);
-
-    browser.getCurrentUrl().then(function (text) { 
-       console.log ('EE POC test - new space URL = ' + text);
-    });
-
-    /* Add a project to the space */
-    OpenShiftIoDashboardPage.waitForToastToClose();
-    OpenShiftIoSpaceHomePage.clickPrimaryAddToSpaceButton();  
-    OpenShiftIoSpaceHomePage.clickTechnologyStack();
-    OpenShiftIoSpaceHomePage.clickQuickStartFinishButton2();
-    OpenShiftIoSpaceHomePage.clickOkButton();
-
-    /* TODO - Trap 'Application Generation Error' here - if found, fail test and exit */
-    expect(OpenShiftIoDashboardPage.appGenerationError.isPresent()).toBe(false);
-    OpenShiftIoDashboardPage.waitForToastToClose();
-
-    /* TODO - Create a workspace */
-
-    /* Navigating thru the Plan/Create/Analyze tabs is not working in the UI - due to 
-       Angular bug with Protractor? Navigate directly to the URL instead */
-     //OpenShiftIoSpaceHomePage.clickHeaderAnalyze();
-
-    /* Go to the Create page - https://openshift.io/almusertest1/testmay91494369460731/create  */
-    browser.get("https://openshift.io/almusertest1/" + spaceTime + "/create");
-    
-    /* Locate the first codebase */
-    OpenShiftIoSpaceHomePage.clickFirstCodebase();
-
-    /* TODO - Verify the workspace in Che - TODO - Create a page object modelk for the Che dashboard */
-    browser.get("https://che-almusertest1-che.8a09.starter-us-east-2.openshiftapps.com/dashboard/#/");
-
-    /* Navigating thru the Plan/Create/Analyze tabs is not working in the UI - due to 
-       Angular bug with Protractor? Navigate directly to the URL instead */
-    // OpenShiftIoSpaceHomePage.clickHeaderAnalyze();
-    browser.get("https://openshift.io/almusertest1/" + spaceTime);
-
-    OpenShiftIoSpaceHomePage.clickPipelinesSectionTitle();
-    OpenShiftIoSpaceHomePage.pipelinesPage.getText().then(function(text){
-    console.log("Pipelines page = " + text);
-
-      /* May 9, 2017 - clicking on a pipeline fails due to this error:
-      https://openshift.io/kleinhenz-1/osio-planner/plan/detail/682    *
-      /* Example of expected text:
-         testmay91494354476064 created a few seconds ago
-         Source Repository: https://github.com/almightytest/testmay91494354476064.git
-         -or- 
-         No pipeline builds have run for testmay91494354476064.   */
-//      expect(text).toContain("No pipeline builds have run for " + spaceTime);
-      expect(text).toContain("Source Repository: https://github.com/almightytest/" + spaceTime + ".git");
     });
 
     /* Step 5 - log out */
