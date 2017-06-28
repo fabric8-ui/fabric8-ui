b724dc6c10a4ae7ec99061952ff0c3acd10ca7a6
Corinne Krych <corinnekrych@gmail.com>
2017-06-28 16:14:24 +0200

    fix(wizard): import github organisation with space in the name

--- src/app/space-wizard/services/fabric8-app-generator-configurator.service.ts
+++ src/app/space-wizard/services/fabric8-app-generator-configurator.service.ts
@@ -124,19 +124,6 @@ export class AppGeneratorConfiguratorService {
         }
         break;
       }
-      case 'gitorganisation': {
-        if ( !Array.isArray(input.value) ) {
-          let value: string = input.value || '';
-          if ( value ) {
-            // convert to lower case
-            value = value.trim().toLowerCase();
-            // remove white space
-            value = value.replace(/\s+/g, '');
-            input.value = value;
-          }
-        }
-        break;
-      }
       default: {
         break;
       }
