d466ec540549c3e58c7107fc7e14e8ffe06a7aa7
joshuawilson <javajoshw@gmail.com>
2017-06-28 13:31:31 +0200

    fix(branding): add env var for branding

--- src/config/fabric8-ui.env.js
+++ src/config/fabric8-ui.env.js
@@ -1,11 +1,12 @@
 window.Fabric8UIEnv = {
-  "pipelinesNamespace": "{{ .Env.FABRIC8_PIPELINES_NAMESPACE }}",
   "analyticsWriteKey": "{{ .Env.ANALYTICS_WRITE_KEY }}",
+  "branding": "{{ .Env.BRANDING }}",
   "forgeApiUrl": "{{ .Env.FABRIC8_FORGE_API_URL }}",
   "openshiftConsoleUrl": "{{ .Env.OPENSHIFT_CONSOLE_URL }}",
   "openshiftProxiedApiServer": "{{ .Env.PROXIED_K8S_API_SERVER }}",
-  "witApiUrl": "{{ .Env.FABRIC8_WIT_API_URL }}",
-  "ssoApiUrl": "{{ .Env.FABRIC8_SSO_API_URL }}",
+  "pipelinesNamespace": "{{ .Env.FABRIC8_PIPELINES_NAMESPACE }}",
   "recommenderApiUrl": "{{ .Env.FABRIC8_RECOMMENDER_API_URL }}",
-  "statusApiUrl": "{{ .Env.FABRIC8_STATUS_API_URL }}"
+  "ssoApiUrl": "{{ .Env.FABRIC8_SSO_API_URL }}",
+  "statusApiUrl": "{{ .Env.FABRIC8_STATUS_API_URL }}",
+  "witApiUrl": "{{ .Env.FABRIC8_WIT_API_URL }}"
 };
