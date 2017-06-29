8fb15d2958d5f1937c3a3468f1109f8d7cf9cef0
Hector Fernandez <hfernand@redhat.com>
2017-06-29 12:15:14 +0200

    Removed docker commented code and added some echo cmds

--- scripts/run-functests.sh
+++ scripts/run-functests.sh
@@ -3,28 +3,22 @@
 # Show command before executing
 set -x
 
-#set -e
-
 echo -n Running Xvfb...
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 &
 
-# Build builder image
-#cp /tmp/jenkins-env .
-#docker build -t fabric8-planner-builder -f deploy/Dockerfile.builder .
-# User root is required to run webdriver-manager update. This shouldn't be a problem for CI containers
-#mkdir -p dist && docker run --detach=true --name=fabric8-planner-builder --user=root --cap-add=SYS_ADMIN -e "API_URL=https://api.prod-preview.openshift.io/api/" -e "CI=true" -t -v $(pwd)/dist:/dist:Z fabric8-planner-builder
 export API_URL=https://api.prod-preview.openshift.io/api/
+# We need to set inmemory to use the mock data
 export NODE_ENV=inmemory
 
-#docker exec fabric8-planner-builder npm install
+echo "Building fabric8-planner..."
 npm install
 
-#docker exec fabric8-planner-builder npm run build
+echo "Run build fabric8-planner"
 mkdir -p dist && cp package.json dist/ && npm run build
 
-#docker exec  -i fabric8-planner-builder bash -c "cd runtime ; npm install"
+echo "Building fabric8-planner/runtime..."
 cd runtime
 npm install
 
-#docker exec fabric8-planner-builder bash -c "cd runtime ; npm run test:funcsmoke"
+echo "Running smoke functional test..."
 npm run test:funcsmoke
