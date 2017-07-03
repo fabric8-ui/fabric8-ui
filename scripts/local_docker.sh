c6123196cb5e90f74918c1971309cd0bde5d9db7
Hector Fernandez <hfernand@redhat.com>
2017-07-03 15:45:59 +0200

    Changed local_docker to run functional tests locally using docker

--- scripts/local_docker.sh
+++ scripts/local_docker.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 docker build -t almighty-ui-builder -f ./deploy/Dockerfile.builder .
 mkdir -p runtime/dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd)/runtime/dist:/dist:Z almighty-ui-builder
 docker exec almighty-ui-builder npm install
 docker exec almighty-ui-builder ./run_unit_tests.sh
+
+if [[ "$1" == "functionalTests" ]]; then
+  echo "Running functional tests..."
+  #docker exec almighty-ui-builder ./scripts/run-functests.sh
+fi
+
 docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
 docker build -t almighty-ui-deploy -f ./deploy/Dockerfile.deploy .
