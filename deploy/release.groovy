1e918ae58700d8375ba4bf584ad39632bb9a95af
Hector Fernandez <hfernand@redhat.com>
2017-07-03 14:19:05 +0200

    Run xvfb and npm setup in the groovy file

--- deploy/release.groovy
+++ deploy/release.groovy
@@ -14,8 +14,16 @@ def ci (){
     }
 
     stage('func test'){
-        container('ui'){
-            sh './scripts/run-functests.sh'
+        dir('runtime'){
+            container('ui'){
+                sh '''
+        /usr/bin/Xvfb :99 -screen 0 1024x768x24 &
+        export API_URL=https://api.prod-preview.openshift.io/api/
+        export NODE_ENV=inmemory
+        npm install
+        ./tests/run_functional_tests.sh smokeTest
+'''
+            }
         }
     }
 }
