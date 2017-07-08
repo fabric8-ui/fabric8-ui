cbd710a98ae3b202b719d2c698c774a1f8cb8d9d
Kyle Buchanan <kylebuch8@gmail.com>
2017-07-08 08:39:34 -0400

    feat(dynamic page titles): Adding private variables to the branding service
    Moving conditional logic to the constructor and returning private variables in the getters
    
    1377

--- src/app/shared/branding.service.ts
+++ src/app/shared/branding.service.ts
@@ -7,32 +7,52 @@ import fabric8Logo from '../../assets/images/fabric8_logo.png';
 @Injectable()
 export class BrandingService {
   private isFabric8: boolean;
+  private _logo: string;
+  private _backgroundClass: string;
+  private _description: string;
+  private _name: string;
+  private _moreInfoLink: string;
 
   constructor(
     private fabric8UIConfig: Fabric8UIConfig,
   ) {
     this.isFabric8 = this.fabric8UIConfig.branding && this.fabric8UIConfig.branding === "fabric8";
-    console.log('isFabric8', this.isFabric8);
+
+    if (this.isFabric8) {
+      this._logo = fabric8Logo;
+      // replace background image with fabric8 background once available
+      this._backgroundClass = "home-fabric8-background-image";
+      this._description = "A free, end-to-end, cloud-native development experience.";
+      this._name = "fabric8.io";
+      this._moreInfoLink = "https://fabric8.io/";
+    } else {
+      // default openshift.io branding
+      this._logo = openshiftLogo;
+      this._backgroundClass = "home-header-background-image";
+      this._description = "A free, end-to-end, cloud-native development experience.";
+      this._name = "OpenShift.io";
+      this._moreInfoLink = "https://openshift.io";
+    }
   }
 
   get name(): string {
-    return this.isFabric8 ? "fabric8.io" : "OpenShift.io";
+    return this._name;
   }
 
   get logo(): string {
-    return this.isFabric8 ? fabric8Logo : openshiftLogo;
+    return this._logo;
   }
 
   get backgroundClass(): string {
-    return this.isFabric8 ? "home-fabric8-background-image" : "home-header-background-image";
+    return this._backgroundClass;
   }
 
   get description(): string {
-    return this.isFabric8 ? "A free, end-to-end, cloud-native development experience." : "A free, end-to-end, cloud-native development experience.";
+    return this._description;
   }
 
   get moreInfoLink(): string {
-    return this.isFabric8 ? "https://fabric8.io/" : "https://openshift.io";
+    return this._moreInfoLink;
   }
 
 }
