bfc5a791a6ccc3387d408f3fdca8066daa1ff3b8
Dana Gutride <dgutride@gmail.com>
2017-07-13 14:46:02 -0400

    fix(button): fix context button event handling (#1731)

--- src/app/shared/experimental-feature.resolver.ts
+++ src/app/shared/experimental-feature.resolver.ts
@@ -14,37 +14,38 @@ import { FeatureFlagConfig } from '../models/feature-flag-config';
 @Injectable()
 export class ExperimentalFeatureResolver implements Resolve<FeatureFlagConfig> {
 
-  private _loggedInUser: User;
-
   constructor(private router: Router, private userService: UserService, private authService: AuthenticationService) {
-    if(authService.isLoggedIn()) {
-      userService.loggedInUser.subscribe((user) => {
-        this._loggedInUser = user;
-      });
-    }
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FeatureFlagConfig> {
-    // Resolve the context
     let featureName = route.data["featureName"];
-    let experimentalFeaturesEnabled = false;
-    if (this._loggedInUser && this._loggedInUser.attributes) {
-      let contextInformation = this._loggedInUser.attributes["contextInformation"];
-      if (contextInformation && contextInformation.experimentalFeatures ) {
-        experimentalFeaturesEnabled =  contextInformation.experimentalFeatures["enabled"];
-      }
-    }
-
-    if (!this.authService.isLoggedIn()) {
+    let experimentalFeaturesEnabled = true;
+    if(this.authService.isLoggedIn()) {
+      return this.userService.loggedInUser.map((user) => {
+        let loggedInUser = user;
+        // Resolve the context
+        let experimentalFeaturesEnabled = false;
+        if (loggedInUser && loggedInUser.attributes) {
+          let contextInformation = loggedInUser.attributes["contextInformation"];
+          if (contextInformation && contextInformation.experimentalFeatures ) {
+            experimentalFeaturesEnabled =  contextInformation.experimentalFeatures["enabled"];
+          }
+        }
+        return {
+          name: featureName,
+          showBanner: true,
+          enabled: experimentalFeaturesEnabled
+        } as FeatureFlagConfig;
+      }).take(1);
+    } else  {
       // enable experimental features for all non-logged in users
       experimentalFeaturesEnabled = true;
+      return Observable.of({
+        name: featureName,
+        showBanner: true,
+        enabled: experimentalFeaturesEnabled
+      } as FeatureFlagConfig);
     }
-
-    return Observable.of({
-      name: featureName,
-      showBanner: true,
-      enabled: experimentalFeaturesEnabled
-    } as FeatureFlagConfig);
   }
 
 }
