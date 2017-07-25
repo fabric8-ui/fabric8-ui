7dedc90506f7ab0cd5a547ee3fb394637ab9d332
Dana Gutride <dgutride@redhat.com>
2017-07-25 14:10:51 -0400

    fix(widget): only call codebase api one time

--- src/app/dashboard-widgets/add-codebase-widget/add-codebase-widget.component.ts
+++ src/app/dashboard-widgets/add-codebase-widget/add-codebase-widget.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
-import { Broadcaster } from 'ngx-base';
-import { Contexts, Spaces, Space } from 'ngx-fabric8-wit';
+import { Contexts } from 'ngx-fabric8-wit';
 
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 import { CodebasesService } from './../../create/codebases/services/codebases.service';
 import { Codebase } from './../../create/codebases/services/codebase';
-import { DummyService } from './../shared/dummy.service';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -16,30 +13,32 @@ import { DummyService } from './../shared/dummy.service';
   templateUrl: './add-codebase-widget.component.html',
   styleUrls: ['./add-codebase-widget.component.less']
 })
-export class AddCodebaseWidgetComponent implements OnInit {
+export class AddCodebaseWidgetComponent implements OnInit, OnDestroy {
 
-  codebases: Observable<Codebase[]>;
-  codebaseCount: Observable<number>;
-  contextPath: Observable<string>;
+  codebases: Codebase[];
+  codebaseCount: number;
+  contextPath: string;
+  contextSubscription: Subscription;
   @Output() addToSpace = new EventEmitter();
 
   constructor(
     private context: Contexts,
-    private broadcaster: Broadcaster,
     private codebaseService: CodebasesService,
-    private spaces: Spaces
   ) { }
 
   ngOnInit() {
-    this.contextPath = this.context.current.map(context => context.path);
-
-    this.codebases = this.spaces.current
-      .filter(space => space !== null)
-      .switchMap(space => this.codebaseService.getCodebases(space.id));
-    this.codebaseCount = this.spaces.current
-      .filter(space => space !== null)
-      .switchMap(space => this.codebaseService.getCodebases(space.id))
-      .map(codebases => codebases.length);
+    this.contextSubscription = this.context.current.subscribe(context => {
+      this.contextPath = context.path;
+      if (context.space) {
+        this.codebaseService.getCodebases(context.space.id).subscribe((codebases) => {
+          this.codebases = codebases;
+          this.codebaseCount = codebases.length;
+        })
+      }
+    });
   }
 
+  ngOnDestroy() {
+    this.contextSubscription.unsubscribe();
+  }
 }
