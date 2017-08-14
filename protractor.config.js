31cfda5be2682a97c34d0660fbe4aff2dc603039
Hector Fernandez <hfernand@redhat.com>
2017-08-14 17:39:46 +0200

    Use chromedriver instead of phantomjs

--- protractor.config.js
+++ protractor.config.js
@@ -10,15 +10,28 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['src/tests/**/*.spec.js'],
     exclude: ['src/tests/**/EE/*.spec.js'],
+
     jasmineNodeOpts: {
-        defaultTimeoutInterval: 30000
+        isVerbose: true,
+        showColors: true,
+        includeStackTrace: true,
+        defaultTimeoutInterval: 60000
     },
 
-    capabilities: {
-         'browserName': 'phantomjs',
-         'phantomjs.binary.path': require('phantomjs-prebuilt').path,
-         'phantomjs.cli.args': ['--webdriver-loglevel=ERROR', '--local-storage-path=/tmp/phantom_' + Math.random()]
+    troubleshoot: true,
 
+    capabilities: {
+      'browserName': 'chrome',
+      'maxInstances': 2,
+      'shardTestFiles': true,
+      'loggingPrefs': {
+      'driver': 'WARNING',
+      'server': 'WARNING',
+      'browser': 'INFO'
+      },
+      'chromeOptions': {
+      'args': [ '--no-sandbox', '--window-workspace=1']
+      }
     }
 };
 
