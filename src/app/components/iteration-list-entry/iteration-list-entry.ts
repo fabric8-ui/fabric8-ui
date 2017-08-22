8d37fe33e731e5185eb98275da383f50635ee15f
Nimisha Mukherjee <nimishamukerjee@gmail.com>
2017-08-22 14:41:11 +0530

    feat(iteration entry): Add the iteration entry component.

---
+++ src/app/components/iteration-list-entry/iteration-list-entry.ts
@@ -0,0 +1,91 @@
+import { IterationModel } from '../../models/iteration.model';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, OnDestroy } from '@angular/core';
+import {
+  Router,
+  ActivatedRoute,
+  Event as NavigationEvent,
+  NavigationStart,
+  NavigationEnd
+} from '@angular/router';
+
+import { Broadcaster, Logger, Notification, NotificationType, Notifications } from 'ngx-base';
+import { AuthenticationService } from 'ngx-login-client';
+import { Dialog } from 'ngx-widgets';
+import { IterationService } from '../../services/iteration.service';
+import { TreeListItemComponent } from 'ngx-widgets';
+
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: 'iteration-list-entry',
+  templateUrl: './iteration-list-entry.component.html',
+  styleUrls: ['./iteration-list-entry.component.less'],
+})
+export class IterationListEntryComponent implements OnInit, OnDestroy {
+  @Input() listItem: TreeListItemComponent;
+  @Input() iterations: IterationModel[];
+  @Input() selected: boolean = false;
+
+  @Output() selectEvent: EventEmitter<IterationListEntryComponent> = new EventEmitter<IterationListEntryComponent>();
+
+  loggedIn: Boolean = false;
+  queryParams: Object = {};
+  eventListeners: any[] = [];
+  selectedItemId: string | number = 0;
+
+  constructor(private auth: AuthenticationService,
+              private broadcaster: Broadcaster,
+              private route: ActivatedRoute,
+              private iterationService: IterationService,
+              private notifications: Notifications,
+              private router: Router,
+              private logger: Logger) {}
+
+  ngOnInit(): void {
+    this.listenToEvents();
+    this.loggedIn = this.auth.isLoggedIn();
+  }
+
+  ngOnDestroy() {
+    this.eventListeners.forEach(subscriber => subscriber.unsubscribe());
+  }
+
+  select(): void {
+    this.listItem.setSelected(true);
+  }
+
+  deselect(): void {
+    this.listItem.setSelected(false);
+  }
+
+
+
+  selectEntry(): void {
+    this.selectEvent.emit(this);
+  }
+
+  onSelect(event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectEvent.emit(this);
+  }
+
+
+
+  listenToEvents() {
+    this.eventListeners.push(
+      this.broadcaster.on<string>('logout')
+        .subscribe(message => {
+          this.loggedIn = false;
+      })
+    )
+
+    this.eventListeners.push(
+      this.route.queryParams.subscribe((params) => {
+        this.queryParams = params;
+      })
+    );
+
+
+  }
+
+
+}
