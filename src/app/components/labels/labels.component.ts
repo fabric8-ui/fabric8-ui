d14e1a1a1bb80ad3c8b499dfe1d776d379ab96cb
Vikram Raj <vikramraj1991@gmail.com>
2017-09-08 15:21:37 +0530

    add functions

--- src/app/components/labels/labels.component.ts
+++ src/app/components/labels/labels.component.ts
@@ -16,14 +16,54 @@ import { User } from 'ngx-login-client';
 export class LabelsComponent implements OnInit {
 
   @Input() labels: any[];
+  @Input() truncateAfter: number;
   @Output() onLabelClick = new EventEmitter();
-  @Output() onDeleteLabel = new EventEmitter();
+  @Output() onRemoveLabel = new EventEmitter();
 
   ngOnInit() {
-
+    this.labels = [{
+      name: 'frontend',
+      color: '#f7bd7f'
+    },
+    {
+      name: 'backend',
+      color: '#c8eb79'
+    },
+    {
+      name: 'bug',
+      color: '#9ecf99'
+    },
+    {
+      name: 'iteration',
+      color: '#ededed'
+    },
+    {
+      name: 'board view',
+      color: '#7bdbc3'
+    },
+    {
+      name: 'list view',
+      color: '#7cdbf3'
+    },
+    {
+      name: 'user story',
+      color: '#a18fff'
+    }];
   }
 
   moreClick(event) {
     event.stopPropagation();
+  }
+
+  clickLabel(label, event) {
+    event.stopPropagation();
+    this.onLabelClick.emit(label);
+    console.log(label, 'Label Click');
+  }
+
+  removeLabel(label, event) {
+    event.stopPropagation();
+    this.onRemoveLabel.emit(label);
+    console.log(label, 'Remove Label');
   }
 }
