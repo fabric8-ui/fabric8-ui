4658e1548f2b56761010029fbd479a8d7b25ba31
SMahil <mahilsukh11@gmail.com>
2017-09-08 12:55:47 +0530

    fix(less): remove generic css from main.ts

--- runtime/src/main.ts
+++ runtime/src/main.ts
@@ -3,7 +3,6 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import './assets/stylesheets/_planner-osio.less';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
