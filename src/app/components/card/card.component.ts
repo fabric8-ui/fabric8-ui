82072830177784983c32fafb6d004f40985087d9
Vikram Raj <vikramraj1991@gmail.com>
2017-09-18 12:03:56 +0530

    fix(card): remove console log

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -36,7 +36,6 @@ export class CardComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    console.log(this.cardValue, '##$$##$$##$$');
     if (this.urlListener === null) {
      this.urlListener =
       this.route.queryParams.subscribe((params) => {
