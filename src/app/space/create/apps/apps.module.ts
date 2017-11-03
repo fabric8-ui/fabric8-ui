e6fb37d6d34f031e4b520ece33b4fddcbd61b9f6
Andrew Azores <me@andrewazor.es>
2017-11-03 13:28:52 -0400

    Fix dropdown menu kebabs

--- src/app/space/create/apps/apps.module.ts
+++ src/app/space/create/apps/apps.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Http } from '@angular/http';
 
+import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppsComponent } from './apps.component';
 import { AppsRoutingModule } from './apps-routing.module';
 
@@ -9,11 +11,15 @@ import { AppsService } from './services/apps.service';
 
 @NgModule({
   imports: [
+    BsDropdownModule.forRoot(),
     CommonModule,
     AppsRoutingModule
   ],
   declarations: [AppsComponent],
-  providers: [AppsService]
+  providers: [
+    BsDropdownConfig,
+    AppsService
+  ]
 })
 export class AppsModule {
   constructor(http: Http) { }
