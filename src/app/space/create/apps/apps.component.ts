324b8cae219a619e00ca5c3b89647d916d30f51e
Andrew Azores <me@andrewazor.es>
2017-11-07 11:27:43 -0500

    Unsubscribe on destroy

--- src/app/space/create/apps/apps.component.ts
+++ src/app/space/create/apps/apps.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 import {
   AppsService,
   Environment,
 } from './services/apps.service';
+import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'alm-apps',
   templateUrl: 'apps.component.html'
 })
-export class AppsComponent implements OnInit {
+export class AppsComponent implements OnDestroy, OnInit {
   spaceId: string;
   environments: Environment[];
   applications: string[];
+
+  private envSubscription: ISubscription = null;
+  private appSubscription: ISubscription = null;
 
   constructor(
     private router: Router,
@@ -23,13 +27,22 @@ export class AppsComponent implements OnInit {
     this.spaceId = 'placeholder-space';
   }
 
+  ngOnDestroy(): void {
+    if (this.envSubscription) {
+      this.envSubscription.unsubscribe();
+    }
+    if (this.appSubscription) {
+      this.appSubscription.unsubscribe();
+    }
+  }
+
   ngOnInit(): void {
     this.updateResources();
   }
 
   private updateResources(): void {
-    this.appsService.getEnvironments(this.spaceId).subscribe(val => this.environments = val);
-    this.appsService.getApplications(this.spaceId).subscribe(val => this.applications = val);
+    this.envSubscription = this.appsService.getEnvironments(this.spaceId).subscribe(val => this.environments = val);
+    this.appSubscription = this.appsService.getApplications(this.spaceId).subscribe(val => this.applications = val);
   }
 
 }
