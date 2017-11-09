ab1d616cc61275b8603e2bd8748b856ceb278bda
Andrew Azores <me@andrewazor.es>
2017-11-09 12:11:04 -0500

    Conditionally use new Apps module
    New Apps module is used when NODE_ENV is "development". Otherwise,
    the previous runtime-console modules are used.
    
    Fixes #10

--- src/app/space/create/apps/apps.module.ts
+++ src/app/space/create/apps/apps.module.ts
@@ -7,28 +7,38 @@ import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
+import { AppModule as RuntimeConsoleModule } from '../../../../a-runtime-console/index';
+
 import { AppsComponent } from './apps.component';
 import { AppCardComponent } from './components/app-card.component';
 import { AppsRoutingModule } from './apps-routing.module';
 
 import { AppsService } from './services/apps.service';
 
-@NgModule({
-  imports: [
+const USE_RUNTIME_CONSOLE = ENV !== 'development';
+
+const imports = USE_RUNTIME_CONSOLE ?
+  [CommonModule, RuntimeConsoleModule] :
+  [
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     AccordionModule.forRoot(),
     CommonModule,
     AppsRoutingModule
-  ],
-  declarations: [
-    AppsComponent,
-    AppCardComponent
-  ],
-  providers: [
-    BsDropdownConfig,
-    AppsService
-  ]
+  ];
+
+const declarations = USE_RUNTIME_CONSOLE ?
+  [] :
+  [AppsComponent, AppCardComponent];
+
+const providers = USE_RUNTIME_CONSOLE ?
+  [] :
+  [BsDropdownConfig, AppsService];
+
+@NgModule({
+  imports: imports,
+  declarations: declarations,
+  providers: providers
 })
 export class AppsModule {
   constructor(http: Http) { }
