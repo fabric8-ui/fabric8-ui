d9bfcc70901172cb30acd1b1d31919a7e1d7b5e6
Andrew Azores <me@andrewazor.es>
2017-11-20 10:49:30 -0500

    fix(deployments): resource-card unsubscribes on destroy
    fixes openshiftio/openshift.io#1412

--- src/app/space/create/apps/components/resource-card.component.ts
+++ src/app/space/create/apps/components/resource-card.component.ts
@@ -9,7 +9,10 @@ import { DeploymentsService } from '../services/deployments.service';
 import { Environment } from '../models/environment';
 import { Stat } from '../models/stat';
 
-import { Observable } from 'rxjs';
+import {
+  Observable,
+  Subscription
+} from 'rxjs';
 
 @Component({
   selector: 'resource-card',
@@ -25,14 +28,21 @@ export class ResourceCardComponent implements OnInit {
   usedPercent: number;
   unusedPercent: number;
 
+  private statSubscription: Subscription;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.stat.subscribe(val => {
+    this.statSubscription = this.stat.subscribe(val => {
       this.used = val.used;
       this.total = val.total;
       this.usedPercent = (this.total !== 0) ? Math.floor(this.used / this.total * 100) : 0;
       this.unusedPercent = 100 - this.usedPercent;
     });
   }
+
+  ngOnDestroy(): void {
+    this.statSubscription.unsubscribe();
+  }
+
 }
