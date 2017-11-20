9ac90c4e065827e50424bfc88463e8baf66f37c7
Andrew Azores <me@andrewazor.es>
2017-11-20 13:53:50 -0500

    fix(menus): display Deployments only in dev
    fixes openshiftio/openshift.io#1397

--- src/app/layout/header/menus.service.ts
+++ src/app/layout/header/menus.service.ts
@@ -45,36 +45,11 @@ export class MenusService {
                 path: 'board'
               }
             ]
-          }, {
-            name: 'Create',
-            path: 'create',
-            menus: [
-              {
-                name: 'Codebases',
-                path: ''
-              },
-              {
-                name: 'Pipelines',
-                path: 'pipelines'
-              },
-              {
-                name: 'Applications',
-                path: 'apps'
-              },
-              {
-                name: 'Deployments',
-                path: 'deployments'
-              },
-              {
-                name: 'Environments',
-                path: 'environments'
-              }
-            ]
-          }
+          },
+          this.getCreateMenuItems()
         ]
       ]
     ]);
-
   }
 
   public attach(context: Context) {
@@ -110,5 +85,38 @@ export class MenusService {
       res = res.replace(/\/*$/, '');
     }
     return res;
+  }
+
+  private getCreateMenuItems(): MenuItem {
+    const displayDeployments = (ENV === 'development');
+    let menus = [
+      {
+        name: 'Codebases',
+        path: ''
+      },
+      {
+        name: 'Pipelines',
+        path: 'pipelines'
+      },
+      {
+        name: 'Applications',
+        path: 'apps'
+      },
+      {
+        name: 'Environments',
+        path: 'environments'
+      }
+    ];
+    if (displayDeployments) {
+      menus.push({
+        name: 'Deployments',
+        path: 'deployments'
+      });
+    }
+    return {
+      name: 'Create',
+      path: 'create',
+      menus: menus
+    };
   }
 }
