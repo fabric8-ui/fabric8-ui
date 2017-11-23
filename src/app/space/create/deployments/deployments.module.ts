683097c9d4016a7a24ad0f26d128c2ae362b45ee
Christopher Koehler <chkoehle@redhat.com>
2017-11-23 15:22:57 -0500

    style(deployments): add tooltip to checkmark in deployment card

--- src/app/space/create/deployments/deployments.module.ts
+++ src/app/space/create/deployments/deployments.module.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { DeploymentsComponent } from '../deployments/deployments.component';
 import { DeploymentCardComponent } from '../deployments/components/deployment-card/deployment-card.component';
@@ -20,6 +21,7 @@ import { ChartModule } from 'patternfly-ng';
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     AccordionModule.forRoot(),
+    TooltipModule.forRoot(),
     CommonModule,
     ChartModule,
     DeploymentsRoutingModule
