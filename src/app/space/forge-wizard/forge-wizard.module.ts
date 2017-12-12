9b454e1d5af186e3981a2d79edca741441154296
Arunkumar S <arunkumars08@users.noreply.github.com>
2017-12-12 20:39:08 +0530

    fix(forge): delete forge components and import from ngx-forge (#2298)
    deletes the following:
    1. forge-errors
    2. forge-exception
    3. spinner
    4. multiple-selection-list
    5. single-selection-dropdown
    6. pipeline-view
    from fabric8-ui and imports from ngx-forge
    
    update ngx-forge version to 0.0.33-development.
    Tracks: https://github.com/openshiftio/openshift.io/issues/1186

--- src/app/space/forge-wizard/forge-wizard.module.ts
+++ src/app/space/forge-wizard/forge-wizard.module.ts
@@ -1,34 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ForgeImportWizardComponent } from './import-wizard.component';
 import { ForgeQuickstartComponent } from './quickstart-wizard.component';
 import { WizardModule, FilterModule } from 'patternfly-ng';
 import { OrganisationComponent } from './import-pages/step1/organisation-step.component';
-import { PipelineViewComponent } from './components/pipeline-view/pipeline-view.component';
-import { SingleSelectionDropDownComponent } from
-  './components/single-selection-dropdown/single-selection-dropdown.component';
 import { RepositoriesComponent } from './import-pages/step2/repositories-step.component';
-import { MultipleSelectionListComponent } from './components/multiple-selection-list/multiple-selection-list.component';
-import { SelectedItemsPipe } from './components/multiple-selection-list/selected-items.pipe';
-import { VisibleItemsPipe } from './components/multiple-selection-list/visible-items.pipe';
 import { PipelineStepComponent } from './import-pages/step3/pipeline-step.component';
 import { BuildConfigStepComponent } from './import-pages/step4/build-config-step.component';
 import { SingleInputComponent } from './components/single-input/single-input.component';
 import { ReviewStepComponent } from './import-pages/step5/review-step.component';
-import { ForgeErrorsComponent } from './components/forge-errors/forge-errors.component';
 import { FlowSelectorComponent } from './components/flow-selector/flow-selector.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ChooseQuickstartComponent } from './quickstart-pages/step1/choose-quickstart.component';
 import { ProjectInfoStepComponent } from './quickstart-pages/step2/project-info-step.component';
 import { PipelineQuickstartStepComponent } from './quickstart-pages/step3/pipeline-quickstart-step.component';
 import { NgxForgeModule, Config } from 'ngx-forge';
-import { ForgeExceptionComponent } from './components/forge-exception/forge-exception.component';
 import { ForgeConfig } from './service/forge-config';
 import { TokenProvider } from 'ngx-forge';
 import { AuthenticationService } from 'ngx-login-client';
 import { KeycloakTokenProvider } from './service/token-provider';
 import { FormatNameValidationPipe } from './quickstart-pages/step2/project-name-validation.pipe';
+import {
+  ForgeErrorsComponent,
+  ForgeExceptionComponent,
+  MultipleSelectionListComponent,
+  PipelineViewComponent,
+  SingleSelectionDropDownComponent,
+  SpinnerComponent,
+  SelectedItemsPipe,
+  VisibleItemsPipe
+} from 'ngx-forge';
 
 @NgModule({
   imports: [
