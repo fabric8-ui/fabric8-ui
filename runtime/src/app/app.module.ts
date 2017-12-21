f45b6d42c48a2aaf860e2e059b7b96f0b6095bec
Vikram Raj <vikramraj1991@gmail.com>
2017-12-21 13:09:36 +0530

    setup ngrx/store devtools

--- runtime/src/app/app.module.ts
+++ runtime/src/app/app.module.ts
@@ -46,6 +46,7 @@ import { AppRoutingModule } from './app-routing.module';
 import {
   StoreModule
 } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 // conditionally import the inmemory resource module
 let serviceImports: Array<any[] | any | ModuleWithProviders>;
@@ -138,7 +139,11 @@ if (process.env.ENV == 'inmemory') {
     HeaderModule,
     FooterModule,
     StoreModule.forRoot({}),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25 //  Retains last 25 states
+    })
   ],
   declarations: [
     AppComponent,
