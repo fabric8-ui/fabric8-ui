6add572a0028c0a964d0ac17f0977708bd20f915
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-12-28 16:22:11 +0530

    fix(CI/CD): Include tests/*.log files in jenkins artifacts

--- deploy/release.groovy
+++ deploy/release.groovy
@@ -17,10 +17,8 @@ def ci (){
     stage('func test'){
         dir('runtime'){
             container('ui'){
-                sh '''
-        npm install
-        HEADLESS_MODE=true ./tests/run_functional_tests.sh smokeTest
-'''
+                sh 'npm install && HEADLESS_MODE=true ./tests/run_functional_tests.sh smokeTest'
+                archiveArtifacts artifacts: '**/tests/*.log'
             }
         }
     }
@@ -61,10 +59,8 @@ def cd (b){
     stage('func test'){
         dir('runtime'){
             container('ui'){
-                sh '''
-        npm install
-        HEADLESS_MODE=true ./tests/run_functional_tests.sh smokeTest
-    '''
+                sh 'npm install && HEADLESS_MODE=true ./tests/run_functional_tests.sh smokeTest'
+                archiveArtifacts artifacts: '**/tests/*.log'
             }
         }
     }
