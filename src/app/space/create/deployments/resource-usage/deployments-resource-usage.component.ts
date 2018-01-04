dada57ba49790fa79f525e4055829a57bbda376f
Andrew Azores <me@andrewazor.es>
2018-01-04 15:37:00 -0500

    fix(deployments): do not underline Resource Usage title on hover
    fixes https://github.com/openshiftio/openshift.io/issues/1779

--- src/app/space/create/deployments/resource-usage/deployments-resource-usage.component.ts
+++ src/app/space/create/deployments/resource-usage/deployments-resource-usage.component.ts
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'deployments-resource-usage',
+  styleUrls: ['./deployments-resource-usage.component.less'],
   templateUrl: 'deployments-resource-usage.component.html'
 })
 export class DeploymentsResourceUsageComponent {
