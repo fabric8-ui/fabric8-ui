6f465615416aae10afaba8b3476f81f35af679ee
joshuawilson <javajoshw@gmail.com>
2018-01-04 16:30:55 -0500

    fix(build): add updated tslint-loader config to webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -150,6 +150,78 @@ module.exports = function (options) {
           exclude: [/\.(spec|e2e)\.ts$/]
         },
 
+        /**
+         * Static analysis linter for TypeScript advanced options configuration
+         * Description: An extensible linter for the TypeScript language.
+         *
+         * See: https://github.com/wbuchwalter/tslint-loader
+         */
+        {
+          test: /\.ts$/,
+          enforce: 'pre',
+          loader: 'tslint-loader',
+          // exclude: [/node_modules/,/src\/a-runtime-console/],
+          options: {
+          //   configuration: {
+          //     rules: {
+          //       quotemark: [true, 'double']
+          //     }
+          //   },
+          //
+            // can specify a custom config file relative to current directory or with absolute path
+            // 'tslint-custom.json'
+            configFile: "tslint.json",
+
+            // tslint errors are displayed by default as warnings
+            // set emitErrors to true to display them as errors
+            emitErrors: true,
+
+            // tslint does not interrupt the compilation by default
+            // if you want any file with tslint errors to fail
+            // set failOnHint to true
+            failOnHint: true,
+
+            // enables type checked rules like 'for-in-array'
+            // uses tsconfig.json from current working directory
+            typeCheck: true,
+
+            // automatically fix linting errors
+            fix: false,
+
+            // can specify a custom tsconfig file relative to current directory or with absolute path
+            // to be used with type checked rules
+            tsConfigFile: 'tsconfig.json',
+
+            // path to directory containing formatter (optional)
+            formattersDirectory: 'node_modules/tslint-loader/formatters/',
+
+            // name of your formatter (optional)
+            formatter: 'custom',
+
+            // These options are useful if you want to save output to files
+            // for your continuous integration server
+            fileOutput: {
+              // The directory where each file's report is saved
+              dir: './lint/',
+
+              // The extension to use for each report's filename. Defaults to 'txt'
+              ext: 'xml',
+
+              // If true, all files are removed from the report directory at the beginning of run
+              clean: true,
+
+              // A string to include at the top of every report file.
+              // Useful for some report formats.
+              header: '<?xml version="1.0" encoding="utf-8"?>\n<checkstyle version="5.7">',
+
+              // A string to include at the bottom of every report file.
+              // Useful for some report formats.
+              footer: '</checkstyle>'
+            }
+          }
+        },
+
+
         /*
          * Json loader support for *.json files.
          *
