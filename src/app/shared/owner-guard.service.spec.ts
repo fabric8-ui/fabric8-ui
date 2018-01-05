b9c24ef7336bc24dff195f0c0dfb1d6126ad9b0b
Christopher Koehler <chkoehle@redhat.com>
2018-01-05 18:18:23 -0500

    fix(profile): use ContextService capabilities for profile redirection

--- src/app/shared/owner-guard.service.spec.ts
+++ src/app/shared/owner-guard.service.spec.ts
@@ -1,22 +1,21 @@
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 
-import { Observable } from 'rxjs';
+import { AuthenticationService } from 'ngx-login-client';
 
-import { AuthenticationService, UserService, User } from 'ngx-login-client';
-
+import { ContextService } from './context.service';
 import { LoginService } from './login.service';
 import { OwnerGuard } from './owner-guard.service';
 
 describe('OwnerGuard', () => {
   describe('should not let in users not logged in', () => {
+    const FAKE_URL = 'fakeUrl';
     let ownerGuard: OwnerGuard;
     let mockAuthService = { isLoggedIn: () => { return false; } };
+    let mockContext = { };
     let mockLoginService = { redirectToLogin: () => { } };
-    let mockRouter = { };
-    let mockUserService = { };
     let mockRoute = { } as ActivatedRouteSnapshot;
-    let mockState = { url: 'fakeUrl' } as RouterStateSnapshot;
+    let mockState = { url: FAKE_URL } as RouterStateSnapshot;
 
     beforeEach(() => {
       spyOn(mockAuthService, 'isLoggedIn').and.callThrough();
@@ -26,97 +25,89 @@ describe('OwnerGuard', () => {
         providers: [
           OwnerGuard,
           { provide: AuthenticationService, useValue: mockAuthService },
-          { provide: LoginService, useValue: mockLoginService },
-          { provide: Router, useValue: mockRouter },
-          { provide: UserService, useValue: mockUserService },
+          { provide: ContextService, useValue: mockContext },
+          { provide: LoginService, useValue: mockLoginService }
         ]
       });
       ownerGuard = TestBed.get(OwnerGuard);
     });
 
     it('via root path', () => {
-      ownerGuard.canActivate(mockRoute, mockState).subscribe(val => { expect(val).toBe(false); });
+      expect(ownerGuard.canActivateChild(mockRoute, mockState)).toBeFalsy();
       expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
-      expect(mockLoginService.redirectToLogin).toHaveBeenCalledWith('fakeUrl');
+      expect(mockLoginService.redirectToLogin).toHaveBeenCalledWith(FAKE_URL);
     });
 
     it('via child path', () => {
-      ownerGuard.canActivateChild(mockRoute, mockState).subscribe(val => { expect(val).toBe(false); });
+      expect(ownerGuard.canActivateChild(mockRoute, mockState)).toBeFalsy();
       expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
-      expect(mockLoginService.redirectToLogin).toHaveBeenCalledWith('fakeUrl');
+      expect(mockLoginService.redirectToLogin).toHaveBeenCalledWith(FAKE_URL);
     });
   });
 
-  describe('should handle logged in users', () => {
-    const DEFAULT_USERNAME = 'user';
-    const DIFFERENT_USERNAME = 'differentUser';
+  describe('should handle logged in users who are not viewing their own context', () => {
     let ownerGuard: OwnerGuard;
+    let mockAuthService = { isLoggedIn: () => { return true; } };
+    let mockContextService = { viewingOwnContext: () => { return false; } };
+    let mockLoginService = { redirectToLogin: () => { } };
     let mockRoute = { } as ActivatedRouteSnapshot;
     let mockState = { } as RouterStateSnapshot;
-    let mockUser = { attributes: { username: DEFAULT_USERNAME } } as User;
-    let mockAuthService = { isLoggedIn: () => { return true; } };
-    let mockUserService = { loggedInUser: Observable.of(mockUser) };
-    let mockLoginService = { redirectToLogin: () => { } };
 
-    let createMockRouterWithEntity = (username: string) => {
-      return {
-        routerState: {
-          snapshot: {
-            root: {
-              firstChild: {
-                params: {
-                  entity: username
-                }
-              }
-            }
-          }
-        }
-      };
-    };
+    beforeEach(() => {
+      spyOn(mockAuthService, 'isLoggedIn').and.callThrough();
 
-    let firstTestConfig = {
-      title: 'who are not the same and deny them access',
-      username: DIFFERENT_USERNAME,
-      expectedObserverResult: false
-    };
-    let secondTestConfig = {
-      title: 'who are the same and provide them access',
-      username: DEFAULT_USERNAME,
-      expectedObserverResult: true
-    };
-    [firstTestConfig, secondTestConfig].forEach(config => {
-      describe(config.title, () => {
-        let mockRouter = createMockRouterWithEntity(config.username);
-
-        beforeEach(() => {
-          spyOn(mockAuthService, 'isLoggedIn').and.callThrough();
-
-          TestBed.configureTestingModule({
-            providers: [
-              { provide: AuthenticationService, useValue: mockAuthService },
-              { provide: LoginService, useValue: mockLoginService },
-              { provide: Router, useValue: mockRouter },
-              { provide: UserService, useValue: mockUserService },
-              OwnerGuard
-            ]
-          });
-          ownerGuard = TestBed.get(OwnerGuard);
-        });
-
-        it('via root path', () => {
-          ownerGuard.canActivate(mockRoute, mockState).subscribe(val => {
-            expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
-            expect(val).toBe(config.expectedObserverResult);
-          });
-        });
-
-        it('via child path', () => {
-          ownerGuard.canActivateChild(mockRoute, mockState).subscribe(val => {
-            expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
-            expect(val).toBe(config.expectedObserverResult);
-          });
-        });
+      TestBed.configureTestingModule({
+        providers: [
+          OwnerGuard,
+          { provide: AuthenticationService, useValue: mockAuthService },
+          { provide: ContextService, useValue: mockContextService },
+          { provide: LoginService, useValue: mockLoginService }
+        ]
       });
+      ownerGuard = TestBed.get(OwnerGuard);
+    });
+
+    it('via root path', () => {
+      expect(ownerGuard.canActivate(mockRoute, mockState)).toBeFalsy();
+      expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
+    });
+
+    it('via child path', () => {
+      expect(ownerGuard.canActivate(mockRoute, mockState)).toBeFalsy();
+      expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
+    });
+  });
+
+  describe('should handle logged in users who are viewing their own context', () => {
+    let ownerGuard: OwnerGuard;
+    let mockAuthService = { isLoggedIn: () => { return true; } };
+    let mockContextService = { viewingOwnContext: () => { return true; } };
+    let mockLoginService = { redirectToLogin: () => { } };
+    let mockRoute = {} as ActivatedRouteSnapshot;
+    let mockState = {} as RouterStateSnapshot;
+
+    beforeEach(() => {
+      spyOn(mockAuthService, 'isLoggedIn').and.callThrough();
+
+      TestBed.configureTestingModule({
+        providers: [
+          OwnerGuard,
+          { provide: AuthenticationService, useValue: mockAuthService },
+          { provide: ContextService, useValue: mockContextService },
+          { provide: LoginService, useValue: mockLoginService }
+        ]
+      });
+      ownerGuard = TestBed.get(OwnerGuard);
+    });
+
+    it('via root path', () => {
+      expect(ownerGuard.canActivate(mockRoute, mockState)).toBeTruthy();
+      expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
+    });
+
+    it('via child path', () => {
+      expect(ownerGuard.canActivate(mockRoute, mockState)).toBeTruthy();
+      expect(mockAuthService.isLoggedIn).toHaveBeenCalled();
     });
   });
 });
--- src/app/shared/owner-guard.service.ts
+++ src/app/shared/owner-guard.service.ts
@@ -1,82 +1,39 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   CanActivate,
   CanActivateChild,
-  Router,
   ActivatedRouteSnapshot,
   RouterStateSnapshot
 } from '@angular/router';
 
-import { Observable, Subscription } from 'rxjs';
-
-import { AuthenticationService, UserService, User } from 'ngx-login-client';
+import { AuthenticationService } from 'ngx-login-client';
 
 import { LoginService } from './login.service';
+import { ContextService } from './context.service';
 
 /**
  * A guard which checks that the owner of the resource is the same as the one
- * who is logged in. This will also fail if the user is not logged in, making
- * it a superset of the AuthGuard service.
+ * who is logged in. This will also fail if the user is not logged in.
  */
 @Injectable()
-export class OwnerGuard implements OnDestroy {
-  private subscription: Subscription;
+export class OwnerGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthenticationService,
-    private router: Router,
-    private loginService: LoginService,
-    private userService: UserService
+    private contextService: ContextService,
+    private loginService: LoginService
   ) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.authService.isLoggedIn()) {
-      return Observable.create(observer => {
-        this.subscription = this.userService.loggedInUser.subscribe(user => {
-          observer.next(this.userMatchesOwnerFromRoute(user));
-        });
-      });
+      return this.contextService.viewingOwnContext();
     } else {
       this.loginService.redirectToLogin(state.url);
-      return Observable.of(false);
+      return false;
     }
   }
 
-  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.canActivate(route, state);
-  }
-
-  private getRouteParams(): any {
-    if (
-      this.router &&
-      this.router.routerState &&
-      this.router.routerState.snapshot &&
-      this.router.routerState.snapshot.root &&
-      this.router.routerState.snapshot.root.firstChild
-    ) {
-      return this.router.routerState.snapshot.root.firstChild.params;
-    }
-    return null;
-  }
-
-  private extractUserFromRoute(): string {
-    let params = this.getRouteParams();
-    if (params && params['entity']) {
-      return decodeURIComponent(params['entity']);
-    }
-    return null;
-  }
-
-  private userMatchesOwnerFromRoute(user: User): boolean {
-    return user &&
-           user.attributes &&
-           user.attributes.username &&
-           user.attributes.username === this.extractUserFromRoute();
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
   }
 }
