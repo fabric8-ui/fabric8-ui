83808bedfb7925757acd995ffa93390aa2865368
Sanborn <sanborn.sen@gmail.com>
2018-01-09 09:23:24 +0530

    fix(list): started migration of planner list component

--- src/app/components_ngrx/planner-list/planner-list.component.ts
+++ src/app/components_ngrx/planner-list/planner-list.component.ts
@@ -1,8 +1,13 @@
 import {
   Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
   ViewEncapsulation,
   ViewChild
 } from '@angular/core';
+
+import { PlannerLayoutComponent } from './../../widgets/planner-layout/planner-layout.component';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -13,13 +18,33 @@ import {
   templateUrl: './planner-list.component.html',
   styleUrls: ['./planner-list.component.less']
 })
-export class PlannerListComponent {
+
+export class PlannerListComponent implements OnInit {
   private uiLockedAll: boolean = false;
   private sidePanelOpen: boolean = true;
 
-  @ViewChild('plannerLayout') plannerLayout: any;
+  @ViewChild('plannerLayout') plannerLayout: PlannerLayoutComponent;
+  @ViewChild('containerHeight') containerHeight: ElementRef;
 
-  constructor() {}
+  constructor(
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.resizeHeight();
+  }
+
+  resizeHeight() {
+    setTimeout(() => {
+      const navElemnts = document.getElementsByTagName('nav');
+      const navHeight = navElemnts[0].offsetHeight;
+      const totalHeight = window.innerHeight;
+      this.renderer.setStyle(
+        this.containerHeight.nativeElement,
+        'height',
+        (totalHeight - navHeight) + "px");
+    }, 200)
+  }
 
   togglePanelState(event) {
     console.log(event);
