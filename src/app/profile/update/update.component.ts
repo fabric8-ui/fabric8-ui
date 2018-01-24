7378a8f4fa5cee473cda0b0776a4bbcdcd2c5738
Corinne Krych <1395710+corinnekrych@users.noreply.github.com>
2018-01-24 14:19:04 +0100

    clean(feature-flag): remove unused  user consent enum (#2437)

--- src/app/profile/update/update.component.ts
+++ src/app/profile/update/update.component.ts
@@ -418,11 +418,6 @@ export class UpdateComponent implements AfterViewInit, OnInit {
         this.featureLevel = 'internal';
         break;
       }
-      // to be deleted once auth-service and DB have been migrated
-      case 'nopreproduction': {
-        this.featureLevel = 'released';
-        break;
-      }
       default: {
         this.featureLevel = 'released';
         break;
