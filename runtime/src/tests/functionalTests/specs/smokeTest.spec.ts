81de38334c86845baf922cc6f5445b2969fa88a2
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-01-26 23:50:54 +0530

    fix(tests): Rename constants

--- runtime/src/tests/functionalTests/specs/smokeTest.spec.ts
+++ runtime/src/tests/functionalTests/specs/smokeTest.spec.ts
@@ -21,41 +21,40 @@ describe('Planner Smoke Tests:', () => {
   });
 
   it('create a work item and add/remove assignee', async () => {
-    await planner.createWorkItem(c.newWorkItem);
-    expect(await planner.workItemList.hasWorkItem(c.newWorkItem.title)).toBeTruthy();
-    await planner.workItemList.clickWorkItem(c.newWorkItem.title);
+    await planner.createWorkItem(c.newWorkItem1);
+    expect(await planner.workItemList.hasWorkItem(c.newWorkItem1.title)).toBeTruthy();
+    await planner.workItemList.clickWorkItem(c.newWorkItem1.title);
 
-    await planner.quickPreview.addAssignee(c.user1);
-    expect(await planner.quickPreview.hasAssignee(c.user1)).toBeTruthy();
+    await planner.quickPreview.addAssignee(c.user2);
+    expect(await planner.quickPreview.hasAssignee(c.user2)).toBeTruthy();
     expect(await planner.quickPreview.hasCreationTime('Creating now!')).toBeTruthy();
     await planner.quickPreview.close();
 
-    await planner.workItemList.clickWorkItem(c.newWorkItem.title);
-    await planner.quickPreview.addAssignee(c.user1);
-    expect(await planner.quickPreview.hasAssignee(c.user1)).toBeFalsy();
+    await planner.workItemList.clickWorkItem(c.newWorkItem1.title);
+    await planner.quickPreview.addAssignee(c.user2);
+    expect(await planner.quickPreview.hasAssignee(c.user2)).toBeFalsy();
     await planner.quickPreview.close();
   });
 
   it('update workitem title/description', async () => {
-    await planner.createWorkItem(c.newWorkItem1);
+    await planner.createWorkItem(c.newWorkItem2);
 
-    expect(await planner.workItemList.hasWorkItem(c.newWorkItem1.title)).toBeTruthy();
-    await planner.workItemList.clickWorkItem(c.newWorkItem1.title);
+    expect(await planner.workItemList.hasWorkItem(c.newWorkItem2.title)).toBeTruthy();
+    await planner.workItemList.clickWorkItem(c.newWorkItem2.title);
     await planner.quickPreview.updateTitle(c.updatedWorkItem.title);
     await planner.quickPreview.updateDescription(c.updatedWorkItem.description);
-    // Mock data doesn't support saving description
-    // expect(await planner.quickPreview.hasDescription(c.updatedWorkItem.description)).toBeTruthy();
+    expect(await planner.quickPreview.hasDescription(c.updatedWorkItem.description)).toBeTruthy();
     await planner.quickPreview.close();
-    expect(await planner.workItemList.hasWorkItem(c.newWorkItem1.title)).toBeFalsy();
+    expect(await planner.workItemList.hasWorkItem(c.newWorkItem2.title)).toBeFalsy();
     expect(await planner.workItemList.hasWorkItem(c.updatedWorkItem.title)).toBeTruthy();
   });
 
   it('Edit and check WorkItem, creator name and image is reflected', async () => {
     await planner.workItemList.clickWorkItem(c.workItemTitle1);
-    expect(await planner.quickPreview.hasCreator(c.user)).toBeTruthy();
+    expect(await planner.quickPreview.hasCreator(c.user1)).toBeTruthy();
 
     await planner.quickPreview.updateTitle(c.updatedWorkItem.title);
-    expect(await planner.quickPreview.hasCreator(c.user)).toBeTruthy();
+    expect(await planner.quickPreview.hasCreator(c.user1)).toBeTruthy();
     expect(await planner.quickPreview.hasCreatorAvatar(c.user_avatar)).toBeTruthy()
     await planner.quickPreview.close();
 
@@ -63,22 +62,23 @@ describe('Planner Smoke Tests:', () => {
     expect(await planner.workItemList.hasWorkItem(c.updatedWorkItem.title)).toBeTruthy();
 
     await planner.workItemList.clickWorkItem(c.updatedWorkItem.title);
-    expect(await planner.quickPreview.hasCreator(c.user)).toBeTruthy();
+    expect(await planner.quickPreview.hasCreator(c.user1)).toBeTruthy();
     expect(await planner.quickPreview.hasCreatorAvatar(c.user_avatar)).toBeTruthy()
   });
 
-  it('Assign workitem to an Area', async () => {
-    await planner.workItemList.clickWorkItem(c.workItemTitle1);
+  it('Associate workitem with an Area', async () => {
+    await planner.workItemList.clickWorkItem(c.workItemTitle2);
     await planner.quickPreview.addArea(c.areaTitle1);
     expect(await planner.quickPreview.hasArea(c.areaTitle1)).toBeTruthy();
     await planner.quickPreview.close();
 
-    await planner.workItemList.clickWorkItem(c.workItemTitle1);
+    await planner.workItemList.clickWorkItem(c.workItemTitle2);
     expect(await planner.quickPreview.hasArea(c.areaTitle1)).toBeTruthy();
     await planner.quickPreview.addArea(c.areaTitle2);
     expect(await planner.quickPreview.hasArea(c.areaTitle1)).toBeFalsy();
     expect(await planner.quickPreview.hasArea(c.areaTitle2)).toBeTruthy();
     await planner.quickPreview.close();
-  })
+  });
+
 });
 
--- runtime/src/tests/functionalTests/support/constants.ts
+++ runtime/src/tests/functionalTests/support/constants.ts
@@ -1,11 +1,11 @@
 export class Constants {
   areaTitle1 = '/Root Area/Area 0';
   areaTitle2 = '/Root Area/Area 1';
-  newWorkItem = {
+  newWorkItem1 = {
     title: "Workitem Title",
     description: "Describes the work item"
   };
-  newWorkItem1 = {
+  newWorkItem2 = {
     title: "Workitem Title 1"
   };
   updatedWorkItem = {
@@ -13,8 +13,8 @@ export class Constants {
     description: 'New WorkItem Description'
   };
   workItemTitle1 = 'Title Text 0';
-  workItemTitle2 = 'Title Text 0';
-  user = "Example User 0";
-  user1 = "Example User 1";
+  workItemTitle2 = 'Title Text 1';
+  user1 = "Example User 0";
+  user2 = "Example User 1";
   user_avatar = 'https://avatars.githubusercontent.com/u/2410471?v=3&s=20';
 }
