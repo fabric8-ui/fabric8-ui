16a36b7f92d53aadd86317850faadd8ffe4b3d6d
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-01-29 17:55:54 +0530

    fix(tests): Run TS smoketests on each PR instead of JS tests

--- deploy/release.groovy
+++ deploy/release.groovy
@@ -23,7 +23,8 @@ def ci (){
                 sh '''
         npm cache clean --force
         npm install
-        HEADLESS_MODE=true ./tests/run_js_functional_tests.sh smokeTest
+        cd src/tests/functionalTests
+        HEADLESS_MODE=true ./run_ts_functional_tests.sh smokeTest
 '''
             }
         }
