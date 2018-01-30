19f999990a56f33de62a571f3fd9d5b03a5291e9
Corinne Krych <corinnekrych@gmail.com>
2018-01-30 12:13:53 +0100

    fix(feature-flag): fix typescript shouting in sepc file

--- src/app/feature-flag/resolver/feature-flag.resolver.spec.ts
+++ src/app/feature-flag/resolver/feature-flag.resolver.spec.ts
@@ -3,7 +3,7 @@ import { Logger } from 'ngx-base';
 import { AuthenticationService } from 'ngx-login-client';
 import { Feature, FeatureTogglesService } from '../service/feature-toggles.service';
 
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { FeatureFlagConfig } from '../../models/feature-flag-config';
 import { FeatureFlagResolver } from './feature-flag.resolver';
@@ -14,12 +14,14 @@ describe('FeatureFlag resolver: it', () => {
   let mockRouter: any;
   let mockTogglesService: any;
   let resolver: FeatureFlagResolver;
+  let mockActivatedRoute: any;
   beforeEach(() => {
     mockLog = jasmine.createSpyObj('Logger', ['log']);
     mockAuthService = jasmine.createSpyObj('AuthenticationService', ['isLoggedIn']);
     mockAuthService.isLoggedIn.and.returnValue(true);
     mockTogglesService = jasmine.createSpyObj('FeatureTogglesService', ['getFeature']);
     mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+    mockActivatedRoute = jasmine.createSpy('ActivatedRouteSnapshot');
     TestBed.configureTestingModule({
       providers: [
         {
@@ -50,9 +52,8 @@ describe('FeatureFlag resolver: it', () => {
 
   it('should route to requested feature', () => {
     // given
-    const route = {data: {
-      featureName: 'Deployment'
-    }};
+    let route = new ActivatedRouteSnapshot();
+    route.data = {featureName: 'Deployment'};
     let feature = {id: 'Deployment', attributes: {
       enabled: true,
       'user-enabled': true,
@@ -65,7 +66,7 @@ describe('FeatureFlag resolver: it', () => {
     } as FeatureFlagConfig;
     mockTogglesService.getFeature.and.returnValue(Observable.of(feature));
     // when
-    resolver.resolve(route, null).subscribe(val => {
+    resolver.resolve(route as ActivatedRouteSnapshot, null).subscribe(val => {
       // then
       expect(val.name).toEqual(expected.name);
       expect(val.showBanner).toEqual(expected.showBanner);
@@ -75,9 +76,8 @@ describe('FeatureFlag resolver: it', () => {
 
   it('should route to error when requested feature is not-enabled', () => {
     // given
-    const route = {data: {
-      featureName: 'Deployment'
-    }};
+    let route = new ActivatedRouteSnapshot();
+    route.data = {featureName: 'Deployment'};
     let feature = {id: 'Deployment', attributes: {
       enabled: false,
       'user-enabled': true,
@@ -94,9 +94,8 @@ describe('FeatureFlag resolver: it', () => {
 
   it('should route to error when requested feature is not-user-enabled', () => {
     // given
-    const route = {data: {
-      featureName: 'Deployment'
-    }};
+    let route = new ActivatedRouteSnapshot();
+    route.data = {featureName: 'Deployment'};
     let feature = {id: 'Deployment', attributes: {
       enabled: true,
       'user-enabled': false
