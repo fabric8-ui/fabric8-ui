948932eaf19a9f34182cd4d3b3428d7e70877b4c
Dana Gutride <dgutride@redhat.com>
2018-02-05 14:00:10 -0500

    fix(exception): add exception handler

--- src/app/shared/exception.handler.ts
+++ src/app/shared/exception.handler.ts
@@ -2,8 +2,20 @@ import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { UserService } from 'ngx-login-client';
 import Raven from 'raven-js';
 
+const isPreview = window.location.href.indexOf('prod-preview') > -1;
+const isLocal = window.location.href.indexOf('localhost') > -1;
+let environment = 'production';
+if (isPreview) {
+  environment = 'prod-preview';
+} else if (isLocal) {
+  environment = 'development';
+}
+
 // TODO - replace with configuration variable
-Raven.config('https://e71023d2bd794b708ea5a4f43e914b11@errortracking.prod-preview.openshift.io/8').install();
+Raven.config('https://e71023d2bd794b708ea5a4f43e914b11@errortracking.prod-preview.openshift.io/8',
+{
+  environment: environment
+}).install();
 
 @Injectable()
 export class RavenExceptionHandler implements ErrorHandler {
