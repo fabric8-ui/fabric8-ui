038752bb6d9dc111e5e921b47a8e39ac6f3cc0e3
joshuawilson <javajoshw@gmail.com>
2018-02-11 20:53:49 -0500

    fix(build): update sourcemap so that we have them in prod for Sentry

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -83,7 +83,7 @@ module.exports = function (env) {
      */
 
     // PROD VALUE
-    devtool: 'hidden-source-map',
+    devtool: 'source-map',
 
     // DEBUG VALUE
     //devtool: 'inline-source-map',
