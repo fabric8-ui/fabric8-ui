1055f7341594d5275e4986b64a0d8b01adefc1fd
chris lessard <chris.lessard.96@gmail.com>
2018-02-16 13:28:26 -0500

    fix(areas): select correct area when creating child area
    This patch ascertains that the correct area is set by default in the
    create area dialog modal, depending on whether the user is creating a
    new area or a new child area.

--- src/app/space/settings/areas/areas.component.ts
+++ src/app/space/settings/areas/areas.component.ts
@@ -24,6 +24,7 @@ export class AreasComponent implements OnInit, OnDestroy {
   private listConfig: ListConfig;
   private areaSubscription: Subscription;
   private selectedAreaId: string;
+  private defaultArea: string;
 
   constructor(
     private contexts: ContextService,
@@ -47,6 +48,7 @@ export class AreasComponent implements OnInit, OnDestroy {
       areas.forEach((area) => {
         if (area.attributes.parent_path == '/') {
           this.selectedAreaId = area.id;
+          this.defaultArea = area.id;
         }
       });
       this.areas = areas;
@@ -62,6 +64,7 @@ export class AreasComponent implements OnInit, OnDestroy {
   }
 
   addChildArea(id: string) {
+    this.selectedAreaId = this.defaultArea;
     if (id) {
       this.selectedAreaId = id;
     }
