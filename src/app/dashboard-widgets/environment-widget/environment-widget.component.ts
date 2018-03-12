21c5861f5b7812ab0b1ebe233758d2c9ee72c691
Corinne Krych <corinnekrych@gmail.com>
2018-03-12 09:58:10 +0100

    fix(env-widget): put back code from master

--- src/app/dashboard-widgets/environment-widget/environment-widget.component.ts
+++ src/app/dashboard-widgets/environment-widget/environment-widget.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Contexts, Spaces } from 'ngx-fabric8-wit';
-import { Observable } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+
+import { BehaviorSubject, ConnectableObservable, Observable, Subject, Subscription } from 'rxjs/Rx';
+
+import { Context, Contexts, Spaces } from 'ngx-fabric8-wit';
+
+import { Environment as ModelEnvironment } from '../../../app/space/create/deployments/models/environment';
+
 import {
   ApplicationAttributesOverview,
-  DeploymentsService
+  DeploymentsService,
+  Space
 } from '../../../app/space/create/deployments/services/deployments.service';
 
 @Component({
@@ -23,7 +30,6 @@ export class EnvironmentWidgetComponent implements OnInit {
               private spaces: Spaces,
               private deploymentsService: DeploymentsService) {
     this.spaceId = this.spaces.current.first().map(space => space.id);
-    // console.log('::LOADING....::');
   }
 
   ngOnInit() {
