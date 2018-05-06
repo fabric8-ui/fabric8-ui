85206ec2a71ab8ebe209cceef4741379ce0aca16
Sudipta Sen <sanborn.sen@gmail.com>
2018-05-06 12:14:59 +0530

    fix(cookie): added version to table config cookie (#2620)
    - fixes openshiftio/openshift.io#3472

--- src/app/services/cookie.service.ts
+++ src/app/services/cookie.service.ts
@@ -2,22 +2,32 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CookieService {
+  // Increase this by one everytime
+  // a change id made in the table columns
+  datatableColumnVersion = 1;
+
   constructor() { }
 
   setCookie(cName: string, cValue: Array<any>) {
-    document.cookie = cName + "=" + JSON.stringify(cValue);
+
+    document.cookie = cName + "=" + JSON.stringify({
+      version: this.datatableColumnVersion, value: cValue
+    });
   }
 
   getCookie(itemLength: Number) {
     let cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       if (cookies[i].includes('datatableColumn')) {
-        let array = cookies[i].split('=');
-        if(array.length > 1 && Array.isArray(JSON.parse(array[1])) && JSON.parse(array[1]).length === itemLength){
-                return {status: true, array: JSON.parse(array[1])};
+        const data = JSON.parse(cookies[i].split('=')[1]);
+        if (Object.keys(data).length === 2) {
+          if (data['version'] === this.datatableColumnVersion &&
+            data['value'].length === itemLength) {
+            return {status: true, array: data['value']};
           }
         }
       }
-      return {status: false, array: []};
     }
+    return {status: false, array: []};
+  }
 }
