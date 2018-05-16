6a7797b7c49339c25f16b2ec9369d0f787ead9fc
Sanborn <sanborn.sen@gmail.com>
2018-05-16 15:20:47 +0530

    fix(drodown): common dropdowns are fixed with header text and truncate

--- src/app/components_ngrx/common-selector/common-selector.module.ts
+++ src/app/components_ngrx/common-selector/common-selector.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TruncateModule } from 'ng2-truncate';
 import { WidgetsModule } from 'ngx-widgets';
 
 import { SelectDropdownModule } from './../../widgets/select-dropdown/select-dropdown.module';
@@ -9,6 +10,7 @@ import { CommonSelectorComponent } from './common-selector.component';
   imports: [
     CommonModule,
     SelectDropdownModule,
+    TruncateModule,
     WidgetsModule
   ],
   declarations: [
