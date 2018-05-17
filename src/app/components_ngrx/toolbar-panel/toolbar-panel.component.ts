7df558e6efd5b8b710fe5d6bc199f86397b09798
SMahil <mahilsukh11@gmail.com>
2018-05-17 12:25:46 +0530

    fix(filters): fix save filter dropdown in closed sidepanel state

--- src/app/components_ngrx/toolbar-panel/toolbar-panel.component.ts
+++ src/app/components_ngrx/toolbar-panel/toolbar-panel.component.ts
@@ -57,7 +57,7 @@ export class ToolbarPanelComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @Input() context: string;
   @Input() loggedInUser: User | Object = {};
-
+  @Input() dropdownPlacement: any = 'right'; // value is right or left
   @Output() onCreateNewWorkItemSelected: EventEmitter<any | null> = new EventEmitter();
 
   loggedIn: boolean = false;
