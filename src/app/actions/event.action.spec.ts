e7f33236bdfb5caa8dc876f82f16b4785303adb0
sahil143 <sahilbudhwar143@gmail.com>
2018-05-28 14:30:31 +0530

    fix(events): add unit tests for events action

---
+++ src/app/actions/event.action.spec.ts
@@ -0,0 +1,40 @@
+import { EventUI } from './../models/event.model';
+import {
+  Get, GET,
+  GetSuccess, GET_SUCCESS,
+  GetError, GET_ERROR
+} from './event.action';
+
+fdescribe('Unit Test :: Event Actions', () => {
+  it('GetEvent :: should create get action', () => {
+    const url = 'https://mock';
+    const action = new Get(url);
+    expect({ ...action }).toEqual({
+      payload: url,
+      type: GET
+    })
+  })
+
+  it("GetEventSuccess :: should create get success action", () => {
+    const event = [{
+      name: "system.title",
+      newValue: 'a',
+      oldValue: 'b',
+      timestamp: "2018-05-27T08:54:44.63509Z",
+      modifierId: "20694424-0841-4d6c-bfb5-bbbb0391b8db",
+      newValueRelationships: null,
+      oldValueRelationships: null,
+      type: null,
+    }] as EventUI[];
+    const action = new GetSuccess(event);
+    expect({ ...action }).toEqual({
+      type: GET_SUCCESS,
+      payload: event
+    });
+  })
+
+  it("GetEventError :: should create get error action", () => {
+    const action = new GetError();
+    expect({ ...action }).toEqual({ type: GET_ERROR });
+  })
+})
