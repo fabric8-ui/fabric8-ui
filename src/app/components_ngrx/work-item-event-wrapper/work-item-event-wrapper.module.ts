9b3197762b983d7927dafd5b8651d1ff7a15ff3b
sahil143 <sahilbudhwar143@gmail.com>
2018-07-03 11:52:16 +0530

    chore(event): remove unused imports

--- src/app/components_ngrx/work-item-event-wrapper/work-item-event-wrapper.module.ts
+++ src/app/components_ngrx/work-item-event-wrapper/work-item-event-wrapper.module.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Http, HttpModule, RequestOptions, XHRBackend } from '@angular/http';
 import { CollapseModule } from 'ngx-bootstrap';
-import { AuthenticationService } from 'ngx-login-client';
 import { EmptyStateModule } from 'patternfly-ng/empty-state';
 import { EventQuery } from '../../models/event.model';
-import { GlobalSettings } from '../../shared/globals';
 import { WorkItemEventModule } from '../work-item-event/work-item-event.module';
-import { HttpService } from './../../services/http-service';
 import { WorkItemEventWrapperComponent } from './work-item-event-wrapper.component';
 
 @NgModule({
