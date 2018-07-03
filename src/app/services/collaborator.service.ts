a63034381e92333daa2a395d51794f8753ba6e21
sahil143 <sahilbudhwar143@gmail.com>
2018-07-03 11:55:26 +0530

    fix(http.service): use HttpClient service in collaborators - use AuthInterceptor for AuthorizationToken - create get method for HttpClient

--- src/app/services/collaborator.service.ts
+++ src/app/services/collaborator.service.ts
@@ -1,16 +1,15 @@
 import { Inject, Injectable } from '@angular/core';
 import { User } from 'ngx-login-client';
 import { Observable } from 'rxjs/Observable';
-import { HttpService } from './http-service';
+import { HttpClientService } from './http.service';
 
 
 @Injectable()
 export class CollaboratorService {
-  constructor(private http: HttpService) {
-  }
+  constructor(private http: HttpClientService) {}
 
   getCollaborators(url: string): Observable<User[]> {
     return this.http.get(url)
-      .map(resp => resp.json().data as User[]);
+      .map(resp => resp.data);
   }
 }
---
+++ src/app/services/interceptors.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthenticationService } from 'ngx-login-client';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthenticationService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authreq = req.clone(
+      {
+        setHeaders: {
+          'Authorization' : `Bearer ${this.auth.getToken()}`,
+          'Content-Type': `application/json`
+        }
+      }
+    );
+    return next.handle(authreq);
+  }
+}
