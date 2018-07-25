e8f44e5c4e823c33480fd7c7e6d6fc982e729e4e
Raunak1203 <34271052+Raunak1203@users.noreply.github.com>
2018-07-25 14:01:37 +0530

    fix(tests) : Added workitem-naughty-string-spec.ts and updated AgileTemplate spec (#2733)

--- src/tests/specs/agileTemplate.spec.ts
+++ src/tests/specs/agileTemplate.spec.ts
@@ -4,28 +4,26 @@ import * as support from '../support';
 
 
 describe('Agile template tests: ', () => {
-  let planner: PlannerPage;
-  let planner1: PlannerPage;
+  let plannerAgile: PlannerPage;
+  let c = new support.Constants();
+  let URL;
 
   beforeAll(async () => {
     await support.desktopTestSetup();
-    planner = new PlannerPage(browser.baseUrl);
-    await planner.openInBrowser();
-    let url = await browser.getCurrentUrl();
-    let urlPathName: any = await browser.executeScript('return document.location.pathname');
-    let URL = url.replace(urlPathName, '/' + process.env.USER_NAME + '/' + process.env.SPACE_NAME_SCRUM + '/plan');
-    planner1 = new PlannerPage(URL);
+    URL = process.env.BASE_URL + '/' + process.env.USER_NAME + '/' + process.env.SPACE_NAME_SCRUM + '/plan';
+    plannerAgile = new PlannerPage(URL);
+    plannerAgile.openInBrowser();
     await browser.get(URL);
-    await planner1.waitUntilUrlContains('typegroup');
+    await plannerAgile.waitUntilUrlContains('typegroup');
   });
 
   beforeEach(async () => {
-    await planner1.workItemList.overlay.untilHidden();
-    await planner1.sidePanel.workItemsGroupAgile.clickWhenReady();
+    await plannerAgile.workItemList.overlay.untilHidden();
+    await plannerAgile.sidePanel.workItemsGroupAgile.clickWhenReady();
   });
 
   it('should have workitem types', async () => {
-    let wiTypes = await planner1.quickAdd.workItemTypes();
+    let wiTypes = await plannerAgile.quickAdd.workItemTypes();
     expect(wiTypes.length).toBe(6);
     expect(wiTypes[0]).toBe('Theme');
     expect(wiTypes[1]).toBe('Epic');
@@ -37,49 +35,49 @@ describe('Agile template tests: ', () => {
 
   it('should create a workitem of type defect and update Effort', async () => {
     let newWorkItem = { title: 'Workitem of type Defect', type : 'Defect'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     /* Update Effort */
-    await planner1.workItemList.clickWorkItem(newWorkItem.title);
-    await planner1.quickPreview.updateEffort('3');
-    await planner1.quickPreview.effortTextArea.untilTextIsPresentInValue('3');
-    expect(await planner1.quickPreview.effortTextArea.getAttribute('value')).toBe('3');
-    await planner1.quickPreview.close();
+    await plannerAgile.workItemList.clickWorkItem(newWorkItem.title);
+    await plannerAgile.quickPreview.updateEffort('3');
+    await plannerAgile.quickPreview.effortTextArea.untilTextIsPresentInValue('3');
+    expect(await plannerAgile.quickPreview.effortTextArea.getAttribute('value')).toBe('3');
+    await plannerAgile.quickPreview.close();
   });
 
   it('should create a workitem of type Theme and update Business value', async () => {
     let newWorkItem = { title: 'Workitem of type Theme', type : 'Theme'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     /* Update Business Value */
-    await planner1.workItemList.clickWorkItem(newWorkItem.title);
-    await planner1.quickPreview.updateBusinessValue('Business value for this Theme');
-    await planner1.quickPreview.businessValue.untilTextIsPresentInValue('Business value for this Theme');
-    expect(await planner1.quickPreview.businessValue.getAttribute('value')).toBe('Business value for this Theme');
-    await planner1.quickPreview.close();
+    await plannerAgile.workItemList.clickWorkItem(newWorkItem.title);
+    await plannerAgile.quickPreview.updateBusinessValue('Business value for this Theme');
+    await plannerAgile.quickPreview.businessValue.untilTextIsPresentInValue('Business value for this Theme');
+    expect(await plannerAgile.quickPreview.businessValue.getAttribute('value')).toBe('Business value for this Theme');
+    await plannerAgile.quickPreview.close();
   });
 
   it('Dynamic fields should not get closed on outside click', async () => {
     let newWorkItem = { title: 'Workitem of type Story', type : 'Story'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     /* Edit Story Points */
-    await planner1.workItemList.clickWorkItem(newWorkItem.title);
-    await planner1.quickPreview.storyPoints.clickWhenReady();
-    expect(await planner1.quickPreview.isDynamicFieldSaveButtonDisplayed()).toBeTruthy();
-    await planner1.quickPreview.labelDropdown.clickWhenReady();
-    expect(await planner1.quickPreview.isDynamicFieldSaveButtonDisplayed()).toBeTruthy();
-    await planner1.quickPreview.close();
+    await plannerAgile.workItemList.clickWorkItem(newWorkItem.title);
+    await plannerAgile.quickPreview.storyPoints.clickWhenReady();
+    expect(await plannerAgile.quickPreview.isDynamicFieldSaveButtonDisplayed()).toBeTruthy();
+    await plannerAgile.quickPreview.labelDropdown.clickWhenReady();
+    expect(await plannerAgile.quickPreview.isDynamicFieldSaveButtonDisplayed()).toBeTruthy();
+    await plannerAgile.quickPreview.close();
   });
 
   it('should create a workitem of type THEME and check for the order of child types in dropdown', async () => {
     let newWorkItem = { title: 'Theme 1', type : 'Theme'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     // open the inline quick add for newly created WorkItem
-    await planner1.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
+    await plannerAgile.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
     // get the workItem types for inline quick add
-    let wiTypes = await planner1.inlineQuickAdd.workItemTypes();
+    let wiTypes = await plannerAgile.inlineQuickAdd.workItemTypes();
     expect(wiTypes.length).toBe(3);
     expect(wiTypes[0]).toBe('Defect');
     expect(wiTypes[1]).toBe('Epic');
@@ -88,12 +86,12 @@ describe('Agile template tests: ', () => {
 
   it('should create a workitem of type EPIC and check for the order of child types in dropdown', async () => {
     let newWorkItem = { title: 'Epic 1', type : 'Epic'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     // open the inline quick add for newly created WorkItem
-    await planner1.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
+    await plannerAgile.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
     // get the workItem types for inline quick add
-    let wiTypes = await planner1.inlineQuickAdd.workItemTypes();
+    let wiTypes = await plannerAgile.inlineQuickAdd.workItemTypes();
     expect(wiTypes.length).toBe(3);
     expect(wiTypes[0]).toBe('Defect');
     expect(wiTypes[1]).toBe('Story');
@@ -102,12 +100,12 @@ describe('Agile template tests: ', () => {
 
   it('should create a workitem of type STORY and check for the order of child types in dropdown', async () => {
     let newWorkItem = { title: 'Story 1', type : 'Story'};
-    await planner1.createWorkItem(newWorkItem);
-    expect(planner1.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
+    await plannerAgile.createWorkItem(newWorkItem);
+    expect(plannerAgile.workItemList.hasWorkItem(newWorkItem.title)).toBeTruthy();
     // open the inline quick add for newly created WorkItem
-    await planner1.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
+    await plannerAgile.workItemList.workItem(newWorkItem.title).clickInlineQuickAdd();
     // get the workItem types for inline quick add
-    let wiTypes = await planner1.inlineQuickAdd.workItemTypes();
+    let wiTypes = await plannerAgile.inlineQuickAdd.workItemTypes();
     expect(wiTypes.length).toBe(3);
     expect(wiTypes[0]).toBe('Task');
     expect(wiTypes[1]).toBe('Defect');
