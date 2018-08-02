ed432e0e05a128f76ed960321df14ecbceda625a
Andrew Azores <me@andrewazor.es>
2018-08-02 10:56:23 -0400

    fix(error): remove unused HttpModule dependency
    related to https://openshift.io/openshiftio/Openshift_io/plan/detail/348

--- src/app/layout/error/error.module.ts
+++ src/app/layout/error/error.module.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Http, HttpModule } from '@angular/http';
 
 import { ErrorRoutingModule } from './error-routing.module';
 import { ErrorComponent } from './error.component';
 
 @NgModule({
-  imports: [CommonModule, ErrorRoutingModule, HttpModule],
+  imports: [CommonModule, ErrorRoutingModule],
   declarations: [ErrorComponent]
 })
-export class ErrorModule {
-  constructor(http: Http) {}
-}
+export class ErrorModule {}
