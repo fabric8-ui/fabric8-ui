c994d29adffa66e59fde6a8429118f7de0e38b5c
Andrew Azores <me@andrewazor.es>
2018-08-02 13:49:05 -0400

    fix(plan): remove unused HttpModule dependency
    related to https://openshift.io/openshiftio/Openshift_io/plan/detail/348

--- src/app/space/plan/plan.module.ts
+++ src/app/space/plan/plan.module.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Http } from '@angular/http';
 
 import { PlannerModule } from 'fabric8-planner';
 
-
 @NgModule({
-  imports:      [ CommonModule, PlannerModule ]
+  imports: [ CommonModule, PlannerModule ]
 })
-export class PlanModule {
-  constructor(http: Http) {}
-}
+export class PlanModule { }
