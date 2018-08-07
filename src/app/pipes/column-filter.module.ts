d5a9fd4b16a76f3473394b28b903db7b697cf0bf
sahil143 <sahilbudhwar143@gmail.com>
2018-08-07 11:59:35 +0530

    fix(columnFilterPipe): add module.ts for column filter

---
+++ src/app/pipes/column-filter.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FilterColumn } from './column-filter.pipe';
+@NgModule({
+  declarations: [FilterColumn],
+  exports: [FilterColumn]
+})
+
+export class FilterColumnModule { }
