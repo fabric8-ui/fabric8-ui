666a124c9c8f015f45c0a179e5816c181d7febfe
sanbornsen <sanborn.sen@gmail.com>
2018-08-26 12:41:49 +0530

    fix(infotip-service): info tip service upgraded with new http client

--- src/app/services/infotip.service.ts
+++ src/app/services/infotip.service.ts
@@ -1,24 +1,18 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { HttpBackendClient } from './../shared/http-module/http.service';
 import { InfotipState } from './../states/infotip.state';
-import { HttpService } from './http-service';
 
 const infotipsUrl: string = 'https://docs.openshift.io/json/infotips.json';
 
 @Injectable()
 export class InfotipService {
   constructor(
-      private http: HttpService
+      private http: HttpBackendClient
     ) {}
 
   getInfotips(): Observable<InfotipState> {
     return this.http
-      .get(infotipsUrl, {'no-header': null})
-      .map(response => {
-        if (/^[5, 4][0-9]/.test(response.status.toString())) {
-          throw new Error('API error occured');
-        }
-        return response.json() as InfotipState;
-      });
+      .get<{data: InfotipState}>(infotipsUrl);
   }
 }
