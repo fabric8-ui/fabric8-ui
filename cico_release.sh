743450f77d524272da2019fbef8c6c311b54cc36
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-08-30 11:53:47 +0530

    fix(CI/CD): Fix failing master build

--- cico_release.sh
+++ cico_release.sh
@@ -16,9 +16,11 @@ function main() {
     # Build and Release Planner (It will update the tag on github and push fabric8-planner to npmjs.org)
     npm run semantic-release
 
-    create_merge_PR
 }
 
+#
+#   This function no longer works.
+#
 # This function raises a PR against fabric8-npm-dependencies
 function create_merge_PR {
     # Fetch latest tags
