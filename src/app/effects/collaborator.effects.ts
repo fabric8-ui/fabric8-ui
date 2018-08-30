6b0795bd63111e18e6442b7fb06658a8f3dae4ac
sahil143 <sahilbudhwar143@gmail.com>
2018-08-30 13:08:32 +0530

    fix(effects): update collaborator effect to use pipeable operators

--- src/app/effects/collaborator.effects.ts
+++ src/app/effects/collaborator.effects.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import {
-  Notification,
-  Notifications,
-  NotificationType
-} from 'ngx-base';
 import { Observable } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { normalizeArray } from '../models/common.model';
+import { SpaceQuery } from '../models/space';
 import * as CollaboratorActions from './../actions/collaborator.actions';
 import * as UserActions from './../actions/user.actions';
-import { AppState } from './../states/app.state';
-
-import { normalizeArray } from '../models/common.model';
 import {
   UserMapper,
   UserService as UserServiceModel,
@@ -20,6 +14,8 @@ import {
 import {
   CollaboratorService as CollabService
 } from './../services/collaborator.service';
+import { ErrorHandler, filterTypeWithSpace } from './work-item-utils';
+
 
 export type Action = CollaboratorActions.All | UserActions.All;
 
@@ -28,38 +24,31 @@ export class CollaboratorEffects {
   constructor(
     private actions$: Actions,
     private collaboratorService: CollabService,
-    private notifications: Notifications,
-    private store: Store<AppState>
+    private spaceQuery: SpaceQuery,
+    private errHandler: ErrorHandler
   ) {}
 
   @Effect() getCollaborators$: Observable<Action> = this.actions$
-    .ofType(CollaboratorActions.GET)
-    .withLatestFrom(this.store.select('planner').select('space'))
-    .switchMap(([action, space]) => {
-      return this.collaboratorService.getCollaborators(
-        space.links.self + '/collaborators?page[offset]=0&page[limit]=1000'
-      );
-    })
-    .map((collaborators: UserServiceModel[]) => {
-      const collabM = new UserMapper();
-      return collaborators.map(c => collabM.toUIModel(c)).
-      sort((c1, c2) => (c1.name > c2.name ? 1 : 0));
-    })
-    .switchMap(collaborators => {
-      return [
-        new CollaboratorActions.GetSuccess(collaborators.map(c => c.id)),
-        new UserActions.Set(normalizeArray<UserUI>(collaborators))
-      ];
-    })
-    .catch(e => {
-      try {
-        this.notifications.message({
-          message: `Problem in fetching collaborators`,
-          type: NotificationType.DANGER
-        } as Notification);
-      } catch (e) {
-        console.log('Problem in fetching collaborators');
-      }
-      return Observable.of(new CollaboratorActions.GetError());
-    });
+    .pipe(
+      filterTypeWithSpace(CollaboratorActions.GET, this.spaceQuery.getCurrentSpace),
+      switchMap(([action, space]) => {
+        return this.collaboratorService.getCollaborators(
+          space.links.self + '/collaborators?page[offset]=0&page[limit]=1000'
+        );
+      }),
+      map((collaborators: UserServiceModel[]) => {
+        const collabM = new UserMapper();
+        return collaborators.map(c => collabM.toUIModel(c)).
+        sort((c1, c2) => (c1.name > c2.name ? 1 : 0));
+      }),
+      switchMap(collaborators => {
+        return [
+          new CollaboratorActions.GetSuccess(collaborators.map(c => c.id)),
+          new UserActions.Set(normalizeArray<UserUI>(collaborators))
+        ];
+      }),
+      catchError(err => this.errHandler.handleError<Action>(err,
+           'Problem in fetching collaborators',
+            new CollaboratorActions.GetError()))
+    );
 }
