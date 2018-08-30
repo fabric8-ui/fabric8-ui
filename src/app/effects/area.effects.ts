0579f87386fa84b18860837e45d3c0117da24da9
sahil143 <sahilbudhwar143@gmail.com>
2018-08-30 13:07:28 +0530

    fix(effects): update area effect to use pipeable operators

--- src/app/effects/area.effects.ts
+++ src/app/effects/area.effects.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import { Notification, Notifications, NotificationType } from 'ngx-base';
-import { Observable, pipe } from 'rxjs';
-import { catchError, map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { Actions, Effect } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { normalizeArray } from '../models/common.model';
-import { spaceSelector } from '../models/space';
+import { SpaceQuery } from '../models/space';
 import * as AreaActions from './../actions/area.actions';
 import {
   AreaMapper,
   AreaService
 } from './../models/area.model';
 import { AreaService as AService } from './../services/area.service';
-import { AppState } from './../states/app.state';
-import { nameless } from './work-item-utils';
+import { ErrorHandler, filterTypeWithSpace } from './work-item-utils';
 
 export type Action = AreaActions.All;
 
@@ -22,13 +19,13 @@ export class AreaEffects {
   constructor(
     private actions$: Actions,
     private areaService: AService,
-    private store: Store<AppState>,
-    private notifications: Notifications
+    private errHandler: ErrorHandler,
+    private spaceQuery: SpaceQuery
   ) {}
 
   @Effect() getAreas$: Observable<Action> = this.actions$
-    .let(nameless(AreaActions.GET, this.store.select(spaceSelector)))
     .pipe(
+      filterTypeWithSpace(AreaActions.GET, this.spaceQuery.getCurrentSpace),
       switchMap(([action, space]) => {
         return this.areaService.getAreas2(
             space.relationships.areas.links.related
@@ -40,19 +37,7 @@ export class AreaEffects {
               sort((a1, a2) => (a1.name.toLowerCase() > a2.name.toLowerCase() ? 1 : 0));
             }),
             map(areas => new AreaActions.GetSuccess(normalizeArray(areas))),
-            catchError((e) => {
-              try {
-                this.notifications.message({
-                  message: `Problem in fetching Areas.`,
-                  type: NotificationType.DANGER
-                } as Notification);
-              } catch (e) {
-                console.log('Problem in fetching Areas.');
-              }
-              return Observable.of(
-                new AreaActions.GetError()
-              );
-            })
+            catchError((err) => this.errHandler.handleError<Action>(err, 'Problem in fetching Areas.', new AreaActions.GetError()))
           );
       })
     );
