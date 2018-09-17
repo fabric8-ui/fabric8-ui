0fe50ee67bd02e1d8f0c1b32ca4a41b9b032f40a
Raunak Bajpai <bajpairaunak@gmail.com>
2018-09-17 21:10:52 +0530

    update typescript version, remove unused import and package.json for planner page objects

--- src/tests/ui/planner/workitem-list.ts
+++ src/tests/ui/planner/workitem-list.ts
@@ -80,7 +80,6 @@ export class WorkItemList extends BaseElement {
   }
 
   async getUnassignedWorkItemCount(assigneeName: string) {
-    let assignee: any;
     let assignees: any = await this.$$('f8-assignee').getAttribute('innerText');
     let unassigned: any = assignees.filter((assignee: any) => assignee === assigneeName);
     return unassigned.length;
