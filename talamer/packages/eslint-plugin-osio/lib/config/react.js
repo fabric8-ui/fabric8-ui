cebb53f56ca74f49778d455a8b8cc5b3fe59f10e
christianvogt <cvogt@redhat.com>
2018-10-24 17:29:57 -0400

    fix(monorepo): add env `browser` to react eslint config

--- talamer/packages/eslint-plugin-osio/lib/config/react.js
+++ talamer/packages/eslint-plugin-osio/lib/config/react.js
@@ -3,6 +3,10 @@ const merge = require('merge');
 module.exports = {
   extends: ['plugin:osio/base', 'airbnb'],
 
+  env: {
+    browser: true,
+  },
+
   rules: merge(
     require('./rules/react'),
     require('./rules/jsx-a11y'),
