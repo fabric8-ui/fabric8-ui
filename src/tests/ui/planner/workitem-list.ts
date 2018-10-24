b2e0a8684e4191df68209ebd7c9244432d6e092a
Raunak Bajpai <bajpairaunak@gmail.com>
2018-10-24 13:08:57 +0530

    temporary change

--- src/tests/ui/planner/workitem-list.ts
+++ src/tests/ui/planner/workitem-list.ts
@@ -81,6 +81,7 @@ export class WorkItemList extends BaseElement {
 
   async getUnassignedWorkItemCount(assigneeName: string) {
     let assignees: any = await this.$$('f8-assignee').getAttribute('innerText');
+    console.log(assignees);
     let unassigned: any = assignees.filter((assignee: any) => assignee === assigneeName);
     return unassigned.length;
   }
