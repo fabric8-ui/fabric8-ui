f86e62dc326663d16cad35ce74d56771c0f5ff11
Rohit Kumar Rai <rohitkrai03@gmail.com>
2018-10-25 13:07:17 +0530

    Revert "feat(interceptors): add interceptor for `X-App` headers in launcher API calls" (#3391)
    * Revert "cleanup(add-space-overlay): remove all code related to template selection (#3371)"
    
    This reverts commit 7c2c03f66f6a8a69825d3c8c3e6083de3df5f50b.
    
    * Revert " feat(my-spaces): Format My Spaces list as table (#3379)"
    
    This reverts commit dcc6b07635dfbbf06a3a70a09c4cf729332edc3a.
    
    * Revert "fix(interceptor): add more test and merge two ifs"
    
    This reverts commit 0ca3b25695bc2deacebade96794cffa0b919225d.
    
    * Revert "feat(interceptors): add interceptor for `X-App` headers in launcher API calls"
    
    This reverts commit c3ac664ee20aa0a48559dd020f1f7250f4afa313.

--- src/app/shared/interceptors/index.ts
+++ src/app/shared/interceptors/index.ts
@@ -3,12 +3,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from 'ngx-login-client';
 import { CacheInterceptor } from './cache.interceptor';
 import { RequestIdInterceptor } from './request-id.interceptor';
-import { XAppInterceptor } from './x-app.interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const HttpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: RequestIdInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: XAppInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }
 ];
