1fdd849ec83c23249c52644fdb661d654aa4400f
joshuawilson <javajoshw@gmail.com>
2018-10-26 13:15:19 -0400

    fix(build): set git to v2 in running container calling the commands directly

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -34,8 +34,6 @@ eval "$(./env-toolkit load -f jenkins-env.json \
 export BUILD_TIMESTAMP=`date -u +%Y-%m-%dT%H:%M:%S`+00:00
 set -x
 
-. cico_setup.sh
-
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0 || :
 
@@ -57,7 +55,10 @@ if [ ! -d dist ]; then
   docker run --detach=true --name="${BUILDER_CONT}" -t -v $(pwd)/dist:/dist:Z -e BUILD_NUMBER -e BUILD_URL -e BUILD_TIMESTAMP -e JENKINS_URL -e GIT_BRANCH -e "CI=true" -e GH_TOKEN -e NPM_TOKEN -e FABRIC8_BRANDING=openshiftio -e FABRIC8_REALM=fabric8 "${BUILDER_CONT}"
 
   # Semantic release needs git v2 and default is v1. This will update to v2.
-  docker exec install_dependencies
+  docker exec yum -y install centos-release-scl
+  docker exec yum -y install sclo-git212.x86_64
+  docker exec export PATH=${PATH}:/opt/rh/sclo-git212/root/usr/bin/
+
 
   # In order to run semantic-release we need a non detached HEAD, see https://github.com/semantic-release/semantic-release/issues/329
   docker exec "${BUILDER_CONT}" git checkout master
