de32893eb03907cb49f58bd8afc76bdbc1ff2522
Joshua Wilson <javajoshw@gmail.com>
2018-11-09 17:04:22 -0500

    fix(build): remove console statements from the production build via the uglify plugin in webpack (#3414)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -171,6 +171,7 @@ module.exports = function (env) {
             },
             compress: {
               ecma: 5,
+              drop_console: true,
               warnings: false,
               // Disabled because of an issue with Uglify breaking seemingly valid code:
               // https://github.com/facebook/create-react-app/issues/2376
