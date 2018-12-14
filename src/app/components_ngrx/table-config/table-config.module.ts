175951bfcc975615ac50b56720ba0e6f3f00a1f9
Abhi <abhinandan13jan@gmail.com>
2018-12-14 13:47:03 +0530

    fix(table-config): Tooltip not displaying in attributes table (#2818)

--- src/app/components_ngrx/table-config/table-config.module.ts
+++ src/app/components_ngrx/table-config/table-config.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap';
+import { TooltipConfig, TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ClickOutModule } from '../../widgets/clickout/clickout.module';
 import { FilterColumnModule } from './../../pipes/column-filter.module';
 import { TableConfigComponent } from './table-config.component';
-
 
 @NgModule({
   imports: [
@@ -12,11 +12,12 @@ import { TableConfigComponent } from './table-config.component';
     ClickOutModule,
     ClickOutModule,
     FilterColumnModule,
-    BsDropdownModule
+    BsDropdownModule,
+    TooltipModule
   ],
   declarations: [TableConfigComponent],
   exports: [TableConfigComponent],
-  providers: [BsDropdownConfig]
+  providers: [BsDropdownConfig, TooltipConfig]
 })
 
 export class TableConfigModule {}
