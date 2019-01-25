dd75ecd45d2bdbb1eddfa9524bcc414c678195ba
christianvogt <cvogt@redhat.com>
2019-01-25 00:03:14 -0500

    fix(middleware): install localStorage and redirect middlewares

--- packages/toolchain/src/app/redux/store.tsx
+++ packages/toolchain/src/app/redux/store.tsx
@@ -4,13 +4,17 @@ import { History } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from './reducers';
 import { AppState } from './appState';
+import { localStorageMiddleware } from './middleware/localStorage';
+import { redirectMiddleware } from './middleware/redirect';
 
 export function configureStore(history: History): Store<AppState, AnyAction> {
   // eslint-disable-next-line no-underscore-dangle
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     createRootReducer(history),
-    composeEnhancers(applyMiddleware(routerMiddleware(history), thunk)),
+    composeEnhancers(
+      applyMiddleware(routerMiddleware(history), thunk, localStorageMiddleware, redirectMiddleware),
+    ),
   );
   return store;
 }
