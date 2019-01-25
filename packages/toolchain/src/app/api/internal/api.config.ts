24744f345de0d755c910c30b56273a7d99a38315
christianvogt <cvogt@redhat.com>
2019-01-24 23:55:28 -0500

    fix(apiurl): use Fabric8UIEnv for api urls w/ a fallback to process.env

--- packages/toolchain/src/app/api/internal/api.config.ts
+++ packages/toolchain/src/app/api/internal/api.config.ts
@@ -1,7 +1,7 @@
 const { FABRIC8_AUTH_API_URL, FABRIC8_FEATURE_TOGGLES_API_URL, FABRIC8_WIT_API_URL } = process.env;
 
-const DEFAULT_URL = '/api/';
-
-export const AUTH_API_URL = FABRIC8_AUTH_API_URL || DEFAULT_URL;
-export const FEATURE_TOGGLES_API_URL = FABRIC8_FEATURE_TOGGLES_API_URL || DEFAULT_URL;
-export const WIT_API_URL = FABRIC8_WIT_API_URL || DEFAULT_URL;
+const config: { [key: string]: string } = (window as any).Fabric8UIEnv || {};
+export const AUTH_API_URL = config.authApiUrl || FABRIC8_AUTH_API_URL;
+export const FEATURE_TOGGLES_API_URL =
+  config.featureTogglesApiUrl || FABRIC8_FEATURE_TOGGLES_API_URL;
+export const WIT_API_URL = config.witApiUrl || FABRIC8_WIT_API_URL;
